2d525ddabfd122a07999b8f743b34c85be8c06be
bellmich <bellmich@95d9436f-6502-0410-902c-bd9569d1a17e>
2006-04-12 12:09:23 +0000

    added a script which checks the requirements before starting the configuration
    git-svn-id: https://openxpki.svn.sourceforge.net/svnroot/openxpki@200 95d9436f-6502-0410-902c-bd9569d1a17e

---
+++ trunk/deployment/check_modules.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/perl
+
+# perl 5.6.1 - Solaris Perl understand this too ;)
+use 5.006_001;
+
+# find all files
+
+my $result = `find . -type f -print`;
+my @list = grep !/^(\.|\.\.)$/, split /\n/, $result;
+
+our @modules = ("OpenXPKI::Server");
+foreach my $filename (@list)
+{
+    # extract use statements
+    my $file = "";
+    open FD, $filename or die "Cannot open file $filename.\n";
+    while (<FD>) {$file .= $_;};
+    close FD;
+    my @use = grep /^\s*use /, split /\n/, $file;
+    foreach my $module (@use)
+    {
+        $module =~ s/^\s*use\s*([a-zA-Z0-9:\._]+);$/$1/;
+        push @modules, $module;
+    }
+}
+
+# checking modules
+my $last = "";
+my @missing = ();
+foreach my $module (sort @modules)
+{
+    next if ($module =~ /\$/);  # no dynamic includes
+    next if ($last eq $module); # no double checks
+    $last = $module;
+
+    if (not eval "use $module;" and $@)
+    {
+        push @missing, $module;
+    }
+}
+
+if (not @missing)
+{
+    print STDOUT "All modules are available.\n";
+} else {
+    print STDOUT "There are some modules missing.\n";
+    foreach my $module (@missing)
+    {
+        print STDERR "    $module\n";
+    }
+    exit 1;
+}
+
+1;
