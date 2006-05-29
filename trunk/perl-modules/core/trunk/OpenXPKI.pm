34aa4f62b8f89fef1181b6aeb6f11e724b9c22da
bellmich <bellmich@95d9436f-6502-0410-902c-bd9569d1a17e>
2006-05-29 09:11:21 +0000

    moved all i18n stuff into an own module to make it easily available for all clients (with own translations)
    git-svn-id: https://openxpki.svn.sourceforge.net/svnroot/openxpki@300 95d9436f-6502-0410-902c-bd9569d1a17e

--- trunk/perl-modules/core/trunk/OpenXPKI.pm
+++ trunk/perl-modules/core/trunk/OpenXPKI.pm
@@ -21,116 +21,17 @@ use OpenXPKI::Exception;
 use Date::Parse;
 use DateTime;
 use Scalar::Util qw( blessed );
-use Locale::Messages qw (:locale_h :libintl_h nl_putenv);
-use POSIX qw (setlocale);
 use Fcntl qw (:DEFAULT);
+
+use OpenXPKI::i18n qw (i18nGettext set_locale_prefix set_language);
 
 use File::Spec;
 use File::Temp;
-
-our $language = "";
-our $locale_prefix = "";
 
 use vars qw (@ISA @EXPORT_OK);
 require Exporter;
 @ISA = qw (Exporter);
 @EXPORT_OK = qw (i18nGettext set_locale_prefix set_language read_file write_file convert_date get_safe_tmpfile);
-
-sub set_locale_prefix
-{
-    $locale_prefix = shift;
-    if (not -e $locale_prefix)
-    {
-        OpenXPKI::Exception->throw (
-            message => "I18N_OPENXPKI_SET_LOCALE_PREFIX_DIR_DOES_NOT_EXIST",
-            params  => {"DIR" => $locale_prefix});
-    }
-}
-
-
-sub i18nGettext {
-    my $text = shift;
-
-    my $arg_ref;
-    my $ref_of_first_argument = ref($_[0]);
-
-    # coerce arguments into a hashref
-    if ($ref_of_first_argument eq "") {
-	# first argument is a scalar
-	my %arguments = @_;
-	$arg_ref = \%arguments;
-    } 
-    elsif ($ref_of_first_argument eq "HASH") {
-	$arg_ref = $_[0];
-    }
-    elsif ($ref_of_first_argument eq "REF") {
-	$arg_ref = ${$_[0]};
-    }
-
-    ## we need this for utf8
-    #it's too slow, I try to use "use utf8;"
-    #my $i18n_string = pack "U0C*", unpack "C*", gettext ($text);
-    my $i18n_string = gettext ($text);
-
-    if ($i18n_string ne $text)
-    {
-	## there is a translation for this, so replace the parameters 
-	## in the resulting string
-
-	for my $parameter (keys %{$arg_ref}) {
-	    warn if ($parameter !~ m{\A __\w+__ \z}xm);
-            $i18n_string =~ s/$parameter/$arg_ref->{$parameter}/g;
-        }
-    } else {
-        ## no translation found, output original string followed
-	## by all parameters (and values) passed to the function
-
-	## append arguments passed to the function
-	my $untranslated .= join ("; ", 
-				  $text,
-				  map { $_ . " => " . $arg_ref->{$_}  } 
-				      keys %{$arg_ref});
-	
-        #it's too slow, I try to use "use utf8;"
-        #$i18n_string = pack "U0C*", unpack "C*", $untranslated;
-    }
-
-    return $i18n_string;
-}
-
-
-
-sub set_language
-{
-    ## global scope intended
-    $language = shift;
-    if (! defined $language) {
-	$language = "";
-    }
-
-    ## erase environment to block libc's automatic environment detection
-    ## and enforcement
-    #delete $ENV{LC_MESSAGES};
-    #delete $ENV{LC_TIME};
-    delete $ENV{LANGUAGE};    ## known from Debian
-
-    if ($language eq "C" or $language eq "")
-    {
-        setlocale(LC_MESSAGES, "C");
-        setlocale(LC_TIME,     "C");
-        nl_putenv("LC_MESSAGES=C");
-        nl_putenv("LC_TIME=C");
-    } else {
-        my $loc = "${language}.UTF-8";
-        setlocale(LC_MESSAGES, $loc);
-        setlocale(LC_TIME,     $loc);
-        nl_putenv("LC_MESSAGES=$loc");
-        nl_putenv("LC_TIME=$loc");
-    }
-    textdomain("openxpki");
-    bindtextdomain("openxpki", $locale_prefix);
-    bind_textdomain_codeset("openxpki", "UTF-8");
-}
 
 sub read_file
 {
@@ -296,54 +197,22 @@ All functions work in static mode (static member functions).
 This means that they are to be invoked directly and not via an object
 instance.
 
+=head1 i18n Functions
+
+Please see OpenXPKI::i18n for more detailed description of these
+functions:
+
+=over
+
+=item * set_locale_prefix
+
+=item * i18nGettext
+
+=item * set_language
+
+=back
+
 =head1 Functions
-
-=head2 set_locale_prefix
-
-The only parameter is a directory in the filesystem. The function is used
-to set the path to the directory with the mo databases.
-
-
-=head2 i18nGettext
-
-The first parameter is the i18n code string that should be looked up
-in the translation table. Usually this identifier should look like
-C<I18N_OPENCA_MODULE_FUNCTION_SPECIFIC_STUFF>. 
-Optionally there may follow a hash or a hash reference that maps parameter
-keywords to values that should be replaced in the original string.
-A parameter should have the format C<__NAME__>, but in fact every
-keyword is possible.
-
-The function obtains the translation for the code string (if available)
-and then replaces each parameter keyword in the code string
-with the corresponding replacement value.
-
-The function always returns an UTF8 string.
-
-Examples:
-
-    my $text;
-    $text = i18nGettext("I18N_OPENCA_FOO_BAR");
-    $text = i18nGettext("I18N_OPENCA_FOO_BAR", 
-                        "__COUNT__" => 1,
-                        "__ORDER__" => "descending",
-                        );
-
-    %translation = ( "__COUNT__" => 1,
-                     "__ORDER__" => "descending" );
-    $text = i18nGettext("I18N_OPENCA_FOO_BAR", %translation);
-
-    $translation_ref = { "__COUNT__" => 1,
-                         "__ORDER__" => "descending" };
-    $text = i18nGettext("I18N_OPENCA_FOO_BAR", $translation_ref);
-
-
-
-=head2 set_language
-
-Switch complete language setup to the specified language. If no
-language is specified then the default language C is activated. This
-deactivates all translation databases.
 
 =head2 read_file
 
