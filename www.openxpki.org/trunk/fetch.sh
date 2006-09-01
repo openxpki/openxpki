acc17d506d942605ca408d2903992c66494ea273
djulia <djulia@95d9436f-6502-0410-902c-bd9569d1a17e>
2006-09-01 15:22:05 +0000

    Script fetsh.sh is improved
    git-svn-id: https://openxpki.svn.sourceforge.net/svnroot/openxpki@491 95d9436f-6502-0410-902c-bd9569d1a17e

--- www.openxpki.org/trunk/fetch.sh
+++ www.openxpki.org/trunk/fetch.sh
@@ -18,15 +18,18 @@ ${WGET} -nH -m -I lastmidnight http://openxpki.sourceforge.net/lastmidnight/inde
 # not referenced by new index.html any more:
 
 cd ${THIS_DIR}/lastmidnight
-FILES_LIST=`ls`
- 
+# Find files which names begin with a letter and contain only appropriate 
+# characters (letters, digits, '-' and '.')
+FILES_LIST=`find ./ -type f -maxdepth 1 -regex "\./[a-zA-Z][a-zA-Z0-9\.-]*"`
+
 for file in ${FILES_LIST}; do
+    file=`echo $file | sed -e 's/\.\///'`
     if [ ! $file = 'index.html' ]; then
-        RES=`grep -o -e $file index.html`
+        RES=`grep -e href=\"$file\" index.html`
         if [ ! "${RES}" ]; then
             echo "File $file will be removed"
-            rm -f $file
-        fi
-    fi
+            rm -f ${THIS_DIR}/lastmidnight/$file
+        fi # if a file is absent from index.html
+    fi # if a file is not index.html
 done
 
