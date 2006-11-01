b26e8e9d7e4fa49c918a2b1aef66d4b41cdb5649
djulia <djulia@95d9436f-6502-0410-902c-bd9569d1a17e>
2006-11-01 13:17:57 +0000

    Old p5-openxpki port has been split into 8 new ports. Added new tools to update OpenXPKI ports.
    git-svn-id: https://openxpki.svn.sourceforge.net/svnroot/openxpki@583 95d9436f-6502-0410-902c-bd9569d1a17e

---
+++ trunk/package/freebsd/build-plist.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+##
+## Written 2006 by Julia Dubenskaya and Sergei Vyshenski
+## for the OpenXPKI project
+## (C) Copyright 2006 by The OpenXPKI Project
+## Based on the example of pkg-plist building 
+## from the FreeBSD Porter's Handbook:
+## http://www.freebsd.org/doc/en_US.ISO8859-1/books/porters-handbook/
+##
+## Prepare preliminary version of pkg-plist for port.
+## cd to the port directory before starting this script.
+
+set -e
+WD=`pwd`
+PORT_NAME=`make -V PORTNAME`
+TMP=${HOME}/tmp/${PORT_NAME}
+rm -rf ${TMP}
+mkdir -p ${TMP}
+echo ${TMP}
+
+mtree -U -f $(make -V MTREE_FILE) -d -e -p ${TMP}
+(cd ${TMP} && find -d * -type d) | sort > OLD-DIRS
+
+touch pkg-plist
+
+export PREFIX=${TMP}
+export PORTSDIR="${WD}/../../"
+export DISTDIR="${WD}/../../distfiles/"
+export DEPENDS_TARGET=""
+export INSTALL_AS_USER="yes"
+export NO_PKG_REGISTER="yes"
+export OLD_SECURITY_CHECK="yes"
+
+make clean
+if [ ${PORT_NAME} = 'openxpki-deployment' ]; then
+  make extract
+  myWRKSRC=`make -V WRKSRC`
+  sed -i.bak \
+      -e 's|\(ERRORS=1\)|# \1|g' \
+      ${myWRKSRC}/configure
+
+  sed -i.bak \
+      -e 's|\(^.*\)\(OpenXPKI::VERSION\)|# \1\2|g' \
+      ${myWRKSRC}/bin/openxpki-metaconf
+fi
+make
+
+make install PREFIX=${TMP} 
+(cd ${TMP} && find -d * \! -type d) | sort > pkg-plist
+
+(cd ${TMP} && find -d * -type d) | sort | \
+  comm -13 OLD-DIRS - | sort -r | sed -e 's#^#@dirrm #' >> pkg-plist
+rm OLD-DIRS
---
+++ trunk/package/freebsd/update-port.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+##
+## Written 2006 by Julia Dubenskaya
+## for the OpenXPKI project
+## (C) Copyright 2006 by The OpenXPKI Project
+
+myPWD=`pwd`
+PKGNAME_PREFIX="p5-"
+MAJOR_VERSION="0.9"
+PORT_PATH="/usr/ports/security"
+DISTS_PATH="/usr/ports/distfiles/openxpki"
+PORT_NAME_SHORT=$1
+PORT_NAME_SHORT=`echo ${PORT_NAME_SHORT} | sed -e "s/^${PKGNAME_PREFIX}//"`
+PORT_NAME=${PKGNAME_PREFIX}${PORT_NAME_SHORT}
+TMP=${HOME}/tmp/${PORT_NAME_SHORT}
+
+if [ ! -d ${myPWD}/${PORT_PATH}/${PORT_NAME} ]; then
+  echo -e "Usage: update-port.sh PORT_NAME. \nPossible values of PORT_NAME:\n$(cd ${myPWD}/${PORT_PATH}/ && ls | grep ${PKGNAME_PREFIX})"
+  exit
+fi
+
+if [ -d ${TMP} ]; then
+  rm -rf ${TMP} 
+fi
+
+cd ${myPWD}/${DISTS_PATH}/
+TARBALL_NAME=`find . -iname ${PORT_NAME_SHORT}-${MAJOR_VERSION}.*\.gz | sed -e "s/\.\///"`
+if [ ${TARBALL_NAME} ]; then
+  echo "New tarball is " ${TARBALL_NAME}
+  cp -Rf ${myPWD}/${DISTS_PATH}/${TARBALL_NAME} ${DISTS_PATH}/
+else
+  echo "No new tarball for the port ${PORT_NAME} was found in"
+  echo "${myPWD}/${DISTS_PATH}/"
+  echo "Port was not updated."
+  exit
+fi
+
+echo "========================================================="
+echo "Building pkg-plist"
+echo "========================================================="
+cp ${myPWD}/build-plist.sh ${myPWD}/${PORT_PATH}/${PORT_NAME}/
+cd ${myPWD}/${PORT_PATH}/${PORT_NAME}/
+
+./build-plist.sh
+rm ${myPWD}/${PORT_PATH}/${PORT_NAME}/build-plist.sh
+
+mv pkg-plist pkg-plist.0
+mv Makefile Makefile.bak
+
+${myPWD}/get-final-plist.pl ${PORT_NAME_SHORT}
+echo "pwd = " $(pwd)
+export NOCLEANDEPENDS="yes"
+make clean
+rm Makefile.bak
+rm pkg-plist.0
