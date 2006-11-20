c25174ce74863e57adf2e3339b091f0bfb8b0694
alech <alech@95d9436f-6502-0410-902c-bd9569d1a17e>
2006-11-20 15:41:49 +0000

    CRLs can now be published to LDAP
    git-svn-id: https://openxpki.svn.sourceforge.net/svnroot/openxpki@624 95d9436f-6502-0410-902c-bd9569d1a17e

--- trunk/perl-modules/core/trunk/OpenXPKI/Server/Workflow/Activity/CRLIssuance/PublishCRL.pm
+++ trunk/perl-modules/core/trunk/OpenXPKI/Server/Workflow/Activity/CRLIssuance/PublishCRL.pm
@@ -15,6 +15,7 @@ use OpenXPKI::Serialization::Simple;
 use OpenXPKI::FileUtils;
 use OpenXPKI::Crypto::TokenManager;
 use DateTime;
+use Net::LDAP;
 
 use Data::Dumper;
 
@@ -107,6 +108,167 @@ sub execute {
             FORCE    => 1,
         });
     }
+    # do LDAP publication if configured
+    my $config = CTX('xml_config');
+
+    my $ldap_servers = 0;
+    my $realm_index; 
+    my $nr_of_realms = $config->get_xpath_count(XPATH => [ 'pki_realm' ]);
+    ##! 64: 'nr of realms: ' . $nr_of_realms
+
+    SEARCH_REALM_INDEX:
+    for (my $i = 0; $i < $nr_of_realms; $i++) {
+        next SEARCH_REALM_INDEX if ($pki_realm ne $config->get_xpath(
+                                    XPATH   => [ 'pki_realm', 'name' ],
+                                    COUNTER => [ $i,          0]));
+        $realm_index = $i;
+        last SEARCH_REALM_INDEX;
+    }
+    ##! 64: 'realm_index: '  . $realm_index
+
+    my $nr_of_cas = $config->get_xpath_count(
+                        XPATH   => [ 'pki_realm', 'ca' ],
+                        COUNTER => [ $realm_index ],
+    );
+
+    my $ca_index;
+    SEARCH_CA_INDEX:
+    for (my $i = 0; $i < $nr_of_cas; $i++) {
+        next SEARCH_CA_INDEX if ($current_ca ne $config->get_xpath(
+                                    XPATH   => [ 'pki_realm' , 'ca', 'id' ],
+                                    COUNTER => [ $realm_index, $i, 0 ]));
+        $ca_index = $i;
+        last SEARCH_CA_INDEX;
+    }
+    ##! 64: 'ca index: ' . $ca_index
+
+    eval {
+        $ldap_servers = $config->get_xpath_count(
+            XPATH   => [ 'pki_realm' , 'ca'     , 'crl_publication', 'ldap' ],
+            COUNTER => [ $realm_index, $ca_index, 0 ],
+        );
+    };
+    for (my $i = 0; $i < $ldap_servers; $i++) {
+        my @basepath = ('pki_realm', 'ca', 'crl_publication', 'ldap');
+        my @basectr = ($realm_index, $ca_index, 0, $i);
+        my $ldap_server = $config->get_xpath(
+            XPATH   => [ @basepath, 'server' ],
+            COUNTER => [ @basectr , '0'],
+        );
+        my $ldap_port = $config->get_xpath(
+            XPATH   => [ @basepath, 'port' ],
+            COUNTER => [ @basectr , '0'],
+        );
+        my $ldap_bind_dn = $config->get_xpath(
+            XPATH   => [ @basepath, 'bind_dn' ],
+            COUNTER => [ @basectr , '0'],
+        );
+        my $ldap_pass = $config->get_xpath(
+            XPATH   => [ @basepath, 'pass' ],
+            COUNTER => [ @basectr , '0'],
+        );
+        my $ldap_base_dn = $config->get_xpath(
+            XPATH   => [ @basepath, 'base_dn' ],
+            COUNTER => [ @basectr , '0'],
+        );
+        my $ldap_search_dn = $config->get_xpath(
+            XPATH   => [ @basepath, 'search_dn' ],
+            COUNTER => [ @basectr , '0'],
+        );
+        if (! $ldap_server || ! $ldap_port    || ! $ldap_bind_dn ||
+            ! $ldap_pass   || ! $ldap_base_dn || ! $ldap_search_dn) {
+            OpenXPKI::Exception->throw(
+                message => 'I18N_OPENXPKI_SERVER_WORKFLOW_ACTIVITY_CRLISSUANCE_PUBLISHCRL_LDAP_CONFIGURATION_BROKEN',
+            );
+        }
+        ##! 2: 'connecting to ldap server ' . $ldap_server . ':' . $ldap_port
+        my $ldap = Net::LDAP->new(
+            "$ldap_server",
+            port    => $ldap_port,
+            onerror => undef,
+        );
+
+        ##! 2: 'ldap object created'
+
+        if (! defined $ldap) {
+            OpenXPKI::Exception->throw(
+                message => 'I18N_OPENXPKI_SERVER_WORKFLOW_ACTIVITY_CRLISSUANCE_PUBLISHCRL_LDAP_CONNECTION_FAILED',
+                params => {
+                    'LDAP_SERVER' => $ldap_server,
+                    'LDAP_PORT'   => $ldap_port,
+                },
+            );
+        }
+
+        my $mesg = $ldap->bind(
+                        $ldap_bind_dn,
+                        password => $ldap_pass
+        );
+        if ($mesg->is_error()) {
+            OpenXPKI::Exception->throw(
+                message => 'I18N_OPENXPKI_SERVER_WORKFLOW_ACTIVITY_CRLISSUANCE_PUBLISHCRL_LDAP_BIND_FAILED',
+                params  => {
+                    ERROR      => $mesg->error(),
+                    ERROR_DESC => $mesg->error_desc(),
+                }
+            );
+        }
+        ##! 2: 'ldap->bind() done'
+    
+        $mesg = $ldap->search(base      => $ldap_base_dn,
+                              filter    => "($ldap_search_dn)",
+        );
+        if ($mesg->is_error()) {
+            OpenXPKI::Exception->throw(
+                message => 'I18N_OPENXPKI_SERVER_WORKFLOW_ACTIVITY_CRLISSUANCE_PUBLISHCRL_LDAP_SEARCH_FAILED',
+                params  => {
+                    ERROR      => $mesg->error(),
+                    ERROR_DESC => $mesg->error_desc(),
+                }
+            );
+        }
+        ##! 2: 'ldap->search() done'
+        ##! 16: 'mesg->count: ' . $mesg->count
+
+        if ($mesg->count == 0) {
+            OpenXPKI::Exception->throw(
+                message => 'I18N_OPENXPKI_SERVER_WORKFLOW_ACTIVITY_CRLISSUANCE_PUBLISHCRL_LDAP_ENTRY_NOT_FOUND',
+            );
+        }
+        elsif ($mesg->count > 1) {
+            OpenXPKI::Exception->throw(
+                message => 'I18N_OPENXPKI_SERVER_WORKFLOW_ACTIVITY_CRLISSUANCE_PUBLISHCRL_MORE_THAN_ONE_LDAP_ENTRY_FOUND',
+            );
+        }
+        my $tm = OpenXPKI::Crypto::TokenManager->new();
+        my $default_token = $tm->get_token(
+            TYPE      => 'DEFAULT',
+            PKI_REALM => $pki_realm,
+        );
+        my $crl_der = $default_token->command({
+            COMMAND => 'convert_crl',
+            DATA    => $crl,
+            OUT     => 'DER',
+        });
+        foreach my $entry ($mesg->entries) {
+            ##! 32: 'foreach entry'
+            my $mesg = $ldap->modify($entry,
+                replace => {
+                    'certificateRevocationList;binary' => $crl_der,
+                }
+            );
+            if ($mesg->is_error()) {
+                OpenXPKI::Exception->throw(
+                    message => 'I18N_OPENXPKI_SERVER_WORKFLOW_ACTIVITY_CRLISSUANCE_PUBLISHCRL_LDAP_MODIFY_FAILED',
+                    params  => {
+                        ERROR      => $mesg->error(),
+                        ERROR_DESC => $mesg->error_desc(),
+                    },
+                );
+            }
+        }
+    }
+
     # set publication_date in CRL DB
     my $date = DateTime->now();
     $dbi->update(
