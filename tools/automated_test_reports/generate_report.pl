c718a7c755f40d3e94f6f8ca7046a077ddb85cc3
alech <alech@95d9436f-6502-0410-902c-bd9569d1a17e>
2007-05-14 10:44:33 +0000

    generate_report: chdir to $basedir before getting shortlog
    git-svn-id: https://openxpki.svn.sourceforge.net/svnroot/openxpki@880 95d9436f-6502-0410-902c-bd9569d1a17e

--- tools/automated_test_reports/generate_report.pl
+++ tools/automated_test_reports/generate_report.pl
@@ -130,10 +130,10 @@ print INDEX << "XEOF";
       </tr>
 XEOF
 
-chdir $basedir;
 print STDERR "Generating report ... ";
 foreach my $rev (@tested_revisions) {
     print STDERR "r$rev ";
+    chdir $basedir;
     open my $SVN, "svn log -qr $rev ..|";
     my $line = <$SVN>;
     my $svn_info = <$SVN>;
