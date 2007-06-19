34a7bc9f73f2150dbda7b9c5b0633ee6d7a3b210
alech <alech@95d9436f-6502-0410-902c-bd9569d1a17e>
2007-06-19 14:08:12 +0000

    Cleaned up SCEP module documentation (also removed reference to Perl license)
    git-svn-id: https://openxpki.svn.sourceforge.net/svnroot/openxpki@957 95d9436f-6502-0410-902c-bd9569d1a17e

--- trunk/clients/perl/OpenXPKI-Client-SCEP/lib/OpenXPKI/Client/SCEP.pm
+++ trunk/clients/perl/OpenXPKI-Client-SCEP/lib/OpenXPKI/Client/SCEP.pm
@@ -112,19 +112,31 @@ This document describes OpenXPKI::Client::SCEP version $VERSION
 =head1 SYNOPSIS
 
     use OpenXPKI::Client::SCEP;
+    use CGI;
 
-=for author to fill in:
-    Brief code example(s) here showing commonest usage(s).
-    This section will be as far as many users bother reading
-    so make it as educational and exeplary as possible.
-  
-  
+    my $query     = CGI->new();
+    my $operation = $query->param('operation');
+    my $message   = $query->param('message');
+    
+    my $scep_client = OpenXPKI::Client::SCEP->new(
+        {
+        SERVICE    => 'SCEP',
+        REALM      => $realm,
+        SOCKETFILE => $socket,
+        TIMEOUT    => 120, # TODO - make configurable?
+        PROFILE    => $profile, 
+        OPERATION  => $operation,
+        MESSAGE    => $message,
+        SERVER     => $server,
+        ENCRYPTION_ALGORITHM => $enc_alg,
+        });
+    my $result = $scep_client->send_request();
+
 =head1 DESCRIPTION
 
-=for author to fill in:
-    Write a full description of the module and its features here.
-    Use subsections (=head2, =head3) as appropriate.
-
+OpenXPKI::Client::SCEP acts as a client that sends an SCEP request
+to the OpenXPKI server. It is typically called from within a CGI
+script that acts as the SCEP server.
 
 =head1 INTERFACE 
 
@@ -154,115 +166,3 @@ SCEP message to send.
 =head2 send_request
 
 Sends SCEP request to OpenXPKI server.
-
-=head1 DIAGNOSTICS
-
-=for author to fill in:
-    List every single error and warning message that the module can
-    generate (even the ones that will "never happen"), with a full
-    explanation of each problem, one or more likely causes, and any
-    suggested remedies.
-
-=over
-
-=item C<< Error message here, perhaps with %s placeholders >>
-
-[Description of error here]
-
-=item C<< Another error message here >>
-
-[Description of error here]
-
-[Et cetera, et cetera]
-
-=back
-
-
-=head1 CONFIGURATION AND ENVIRONMENT
-
-=for author to fill in:
-    A full explanation of any configuration system(s) used by the
-    module, including the names and locations of any configuration
-    files, and the meaning of any environment variables or properties
-    that can be set. These descriptions must also include details of any
-    configuration language used.
-  
-OpenXPKI::Client::SCEP requires no configuration files or environment variables.
-
-
-=head1 DEPENDENCIES
-
-=for author to fill in:
-    A list of all the other modules that this module relies upon,
-    including any restrictions on versions, and an indication whether
-    the module is part of the standard Perl distribution, part of the
-    module's distribution, or must be installed separately. ]
-
-None.
-
-
-=head1 INCOMPATIBILITIES
-
-=for author to fill in:
-    A list of any modules that this module cannot be used in conjunction
-    with. This may be due to name conflicts in the interface, or
-    competition for system or program resources, or due to internal
-    limitations of Perl (for example, many modules that use source code
-    filters are mutually incompatible).
-
-None reported.
-
-
-=head1 BUGS AND LIMITATIONS
-
-=for author to fill in:
-    A list of known problems with the module, together with some
-    indication Whether they are likely to be fixed in an upcoming
-    release. Also a list of restrictions on the features the module
-    does provide: data types that cannot be handled, performance issues
-    and the circumstances in which they may arise, practical
-    limitations on the size of data sets, special cases that are not
-    (yet) handled, etc.
-
-No bugs have been reported.
-
-Please report any bugs or feature requests to
-C<bug-openxpki-client-scep@rt.cpan.org>, or through the web interface at
-L<http://rt.cpan.org>.
-
-
-=head1 AUTHOR
-
-Martin Bartosch  C<< <m.bartosch@cynops.de> >>
-
-
-=head1 LICENCE AND COPYRIGHT
-
-Copyright (c) 2006, Martin Bartosch C<< <m.bartosch@cynops.de> >>. All rights reserved.
-
-This module is free software; you can redistribute it and/or
-modify it under the same terms as Perl itself. See L<perlartistic>.
-
-
-=head1 DISCLAIMER OF WARRANTY
-
-BECAUSE THIS SOFTWARE IS LICENSED FREE OF CHARGE, THERE IS NO WARRANTY
-FOR THE SOFTWARE, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN
-OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES
-PROVIDE THE SOFTWARE "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
-EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE
-ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE SOFTWARE IS WITH
-YOU. SHOULD THE SOFTWARE PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL
-NECESSARY SERVICING, REPAIR, OR CORRECTION.
-
-IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING
-WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MAY MODIFY AND/OR
-REDISTRIBUTE THE SOFTWARE AS PERMITTED BY THE ABOVE LICENCE, BE
-LIABLE TO YOU FOR DAMAGES, INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL,
-OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE
-THE SOFTWARE (INCLUDING BUT NOT LIMITED TO LOSS OF DATA OR DATA BEING
-RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD PARTIES OR A
-FAILURE OF THE SOFTWARE TO OPERATE WITH ANY OTHER SOFTWARE), EVEN IF
-SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF
-SUCH DAMAGES.
