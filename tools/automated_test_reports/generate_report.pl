c491c5b0dce8a52fa8417da128b19a94dc04d985
djulia <djulia@95d9436f-6502-0410-902c-bd9569d1a17e>
2007-07-16 14:54:33 +0000

    Fix generate_report: - tests could be run on a 'fresh' machine (with no previously installed OpenXPKI); - minor fixes with respect to FreeBSD peculiarities.
    git-svn-id: https://openxpki.svn.sourceforge.net/svnroot/openxpki@1012 95d9436f-6502-0410-902c-bd9569d1a17e

--- tools/automated_test_reports/generate_report.pl
+++ tools/automated_test_reports/generate_report.pl
@@ -5,6 +5,11 @@ use warnings;
 use English;
 use XML::Feed;
 use XML::Simple;
+use Config;
+
+# Define your gmake. Needed for Unixes which have both gmake and make installed.
+# On Linux it could be named just 'make'.
+my $MAKE = 'make';
 
 # define your path here if run_test.pl is not in path
 # e.g.
@@ -16,11 +21,35 @@ my $run_test;
 # my $PREFIX = 'PREFIX=~/usr/local';
 my $PREFIX = '';
 
+# add installation prefix to PERL5LIB
+my $PREFIX_DIR = $PREFIX;
+$PREFIX_DIR =~ s{ \A \s* PREFIX=}{}xms if ($PREFIX ne '');
+
+if ($PREFIX_DIR ne '') {
+    my $installstyle = $Config{installstyle};
+    my $perlversion = $Config{version};
+    my $archname = $Config{archname};
+    $ENV{"PERL5LIB"} = $PREFIX_DIR . "/$installstyle/site_perl/$perlversion/:" .
+                       $PREFIX_DIR . "/$installstyle/site_perl/$perlversion/$archname/:" .
+                       $PREFIX_DIR . "/$installstyle/site_perl/$perlversion/mach/";
+    print STDERR "PERL5LIB contains the following dirs: ".$ENV{"PERL5LIB"}."\n";
+}
+
 # define a deployment prefix (passed to ./configure in trunk/deployment)
 # if necessary
 # e.g.
 # my $DEPLOYMENT_PREFIX = '--prefix ~/usr/local';
 my $DEPLOYMENT_PREFIX = '';
+
+# add deployment prefix to PATH
+# define DEPLOYMENT_PREFIX variable (to be used in OpenXPKI/Tests.pm)
+my $DEPLOYMENT_DIR = $DEPLOYMENT_PREFIX;
+$DEPLOYMENT_DIR =~ s{ \A \s* --prefix \s* (\S+)}{$1}xms if ($DEPLOYMENT_PREFIX ne '');
+
+if ($DEPLOYMENT_DIR ne '') {
+    $ENV{"PATH"} = $DEPLOYMENT_DIR."/bin:" . $ENV{"PATH"};
+    $ENV{"DEPLOYMENT_PREFIX"} = $DEPLOYMENT_DIR."/bin";
+}
 
 # define the format of the feed (see XML::Feed)
 my $FEED_FORMAT = 'Atom';
@@ -54,20 +83,24 @@ if (! defined $output_dir) {
     die "Usage: $0 <output directory>";
 }
 
+# Numbers added to the test names to provide correct sequence of installation:
+# server, client, other clients. Needed for testing on machines with no 
+# pre-installed OpenXPKI.
+
 my $tests = {
-    'server'       => {
+    '0_server'       => {
         DIRECTORY => $basedir . "/perl-modules/core/trunk",
         NAME      => 'Server',
     },
-    'client'       => {
+    '1_client'       => {
         DIRECTORY => $basedir . "/clients/perl/OpenXPKI-Client",
         NAME      => 'Client',
     },
-    'client_mason' => {
+    '2_client_mason' => {
         DIRECTORY => $basedir . "/clients/perl/OpenXPKI-Client-HTML-Mason",
         NAME      => 'Mason client',
     },
-    'client_scep'  => {
+    '3_client_scep'  => {
         DIRECTORY => $basedir . "/clients/perl/OpenXPKI-Client-SCEP",
         NAME      => 'SCEP client',
     },
@@ -81,27 +114,36 @@ if (! -x $run_test) {
     die "run_test.pl not found or not executable";
 }
 
-# install new deployment tools before testing
-chdir $basedir . '/deployment';
-if (system("./configure $DEPLOYMENT_PREFIX && make && make install") != 0) {
-    die "Could not install new deployment tools";
-}
+# will contain test name without a number
+my $test;
 
 INSTALL:
-foreach my $test (sort keys %{ $tests }) {
+foreach my $testname (sort keys %{ $tests }) {
+    $test = $testname;
+    $test =~ s{\d_}{}xms;
+
     print STDERR "Compiling and installing for $test\n";
-    chdir $tests->{$test}->{DIRECTORY};
-    if (system("perl Makefile.PL $PREFIX && make && make install") != 0) {
+    chdir $tests->{$testname}->{DIRECTORY};
+    if (system("perl Makefile.PL $PREFIX && $MAKE && $MAKE install") != 0) {
         die "Could not compile for $test test";
     }
 }
 
+# install new deployment tools before testing
+chdir $basedir . '/deployment';
+if (system("./configure $DEPLOYMENT_PREFIX && $MAKE && $MAKE install") != 0) {
+    die "Could not install new deployment tools";
+}
+
 TEST:
-foreach my $test (sort keys %{ $tests }) {
+foreach my $testname (sort keys %{ $tests }) {
+    $test = $testname;
+    $test =~ s{\d_}{}xms;
+
     print STDERR "Running test for $test (revision $revision)\n";
+    chdir $tests->{$testname}->{DIRECTORY};
 
     # compile and run each test
-    chdir $tests->{$test}->{DIRECTORY};
     my $output_filename = $revision . '_output.txt';
     if (-e $output_filename) {
         # test has already been run, skip
@@ -116,7 +158,7 @@ foreach my $test (sort keys %{ $tests }) {
     system("$ENV $run_test $revision $test");
 }
 
-chdir $tests->{'server'}->{DIRECTORY};
+chdir $tests->{'0_server'}->{DIRECTORY};
 my @tested_revisions = reverse glob('*_output.txt');
 map { s/_output.txt// } @tested_revisions;
 
@@ -147,31 +189,49 @@ print INDEX << "XEOF";
 XEOF
 
 print STDERR "Generating report ... ";
+REV_REPORT:
 foreach my $rev (@tested_revisions[0..$MAX_HTML_REVISIONS-1]) {
+    next REV_REPORT if (!$rev);
     print STDERR "r$rev ";
     chdir $basedir;
     open my $SVN, "svn log -qr $rev ..|";
-    my $line = <$SVN>;
-    my $svn_info = <$SVN>;
+    
+    # my $line = <$SVN>;
+    # my $svn_info = <$SVN>;
+    ## execution of the two previous lines fails on FreeBSD 
+    ## with 'Broken pipe' error message 
+
+    my $svn_info;
+    my $i = 0;
+    while (my $line = <$SVN>) {
+        chomp $line;
+        $svn_info = $line if ($i == 1);
+        $i++;
+    }
     close $SVN;
     $svn_info =~ s{ (.*) \|\ \d+\ lines}{$1}xms;
 
     print INDEX "<tr><td colspan=12>Report for $svn_info</td></tr>";
-    foreach my $test (sort keys %{ $tests }) {
-        chdir $tests->{$test}->{DIRECTORY};
+    foreach my $testname (sort keys %{ $tests }) {
+        $test = $testname;
+        $test =~ s{\d_}{}xms;
+
+        chdir $tests->{$testname}->{DIRECTORY};
         my $files = $rev . '_coverage ' . $rev . '_output.txt ';
         system("mkdir $output_dir/$test 2>/dev/null");
-        system("cp -r $files $output_dir/$test");
+
+        ## 'cp -r' does not work on FreeBSD
+        system("cp -R $files $output_dir/$test");
 
         my $summary_filename = $rev . '_summary.html';
         my $opened = open my $SUMMARY, '<', $summary_filename;
         my $summary;
         if (! $opened) {
-            $summary = '<tr color="303030"><td colspan=12>' . $tests->{$test}->{NAME} . ' summary unavailable</td></tr>' . "\n";
+            $summary = '<tr color="303030"><td colspan=12>' . $tests->{$testname}->{NAME} . ' summary unavailable</td></tr>' . "\n";
         }
         else {
             $summary = <$SUMMARY>;
-            $summary =~ s{Coverage\ report}{$tests->{$test}->{NAME} coverage report}xms;
+            $summary =~ s{Coverage\ report}{$tests->{$testname}->{NAME} coverage report}xms;
             close $SUMMARY;
         }
         print INDEX $summary;
@@ -185,14 +245,27 @@ my $feed = XML::Feed->new($FEED_FORMAT);
 $feed->title($FEED_TITLE);
 $feed->link($FEED_LINK);
 
+REV_FEED:
 foreach my $rev (@tested_revisions[0..$MAX_ATOM_REVISIONS-1]) {
+    next REV_FEED if (!$rev);
     my $entry = XML::Feed::Entry->new($FEED_FORMAT);
 
     print STDERR "r$rev ";
     chdir $basedir;
     open my $SVN, "svn log -qr $rev ..|";
-    my $line = <$SVN>;
-    my $svn_info = <$SVN>;
+
+    # my $line = <$SVN>;
+    # my $svn_info = <$SVN>;
+    ## execution of the two previous lines fails on FreeBSD
+    ## with 'Broken pipe' error message
+
+    my $svn_info;
+    my $i = 0;
+    while (my $line = <$SVN>) {
+        chomp $line;
+        $svn_info = $line if ($i == 1);
+        $i++;
+    }
     close $SVN;
 
     my @svn_info = split(/ \| /, $svn_info);
@@ -220,8 +293,11 @@ foreach my $rev (@tested_revisions[0..$MAX_ATOM_REVISIONS-1]) {
     my $all_green = 1;
     my $all_summaries = '<table border=0 cellspacing=0 cellpadding=5>';
     my @failed;
-    foreach my $test (sort keys %{ $tests }) {
-        chdir $tests->{$test}->{DIRECTORY};
+    foreach my $testname (sort keys %{ $tests }) {
+        $test = $testname;
+        $test =~ s{\d_}{}xms;
+
+        chdir $tests->{$testname}->{DIRECTORY};
         my $summary_filename = $rev . '_summary.html';
         my $opened = open my $SUMMARY, '<', $summary_filename;
         if (! $opened) {
@@ -230,7 +306,7 @@ foreach my $rev (@tested_revisions[0..$MAX_ATOM_REVISIONS-1]) {
         }
         else {
             my $summary = <$SUMMARY>;
-            $summary =~ s{Coverage\ report}{$tests->{$test}->{NAME} coverage report}xms;
+            $summary =~ s{Coverage\ report}{$tests->{$testname}->{NAME} coverage report}xms;
             my $xs = XML::Simple->new();
             my $summary_ref = $xs->XMLin($summary);
             if ($summary_ref->{'td'}->[6] ne '0 (0.00%)') {
