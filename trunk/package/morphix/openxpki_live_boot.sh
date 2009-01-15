b12aa722cf8d0b2acf7ae43c15fdfcb55d33eb0e
alech <alech@95d9436f-6502-0410-902c-bd9569d1a17e>
2009-01-15 15:40:30 +0000

    Morphix: updated boot script
    copying of files is now done in the mini-module definition itself, so no
    longer needed, mounted USB media is below /media, no longer below /mnt
    
    
    git-svn-id: https://openxpki.svn.sourceforge.net/svnroot/openxpki@1390 95d9436f-6502-0410-902c-bd9569d1a17e

--- trunk/package/morphix/openxpki_live_boot.sh
+++ trunk/package/morphix/openxpki_live_boot.sh
@@ -4,16 +4,6 @@
 update-alternatives --install /etc/alternatives/desktop-background desktop-background /morphix/background.png 1
 update-alternatives --set desktop-background /morphix/background.png
 ln -s /etc/alternatives/desktop-background /usr/share/images/desktop-base/desktop-background
-# kill networking
-#rm /etc/rcS.d/S39ifupdown
-#rm /etc/rcS.d/S40networking
-# install tools
-cp $1/morphix/files/openxpki_live_persist.sh /usr/sbin
-chmod 755 /usr/sbin/openxpki_live_persist.sh
-# update webpages
-cp $1/morphix/files/livecd_index.html /var/www/index.htm
-cp $1/morphix/files/livecd_persist.html /var/www/persist.htm
-rm /var/www/index.html
 # FIXME - does not seem to work
 echo 'pref("browser.startup.homepage", "http://localhost/");'  >> /etc/iceweasel/pref/iceweasel.js 
 # hide xfce tips
@@ -21,11 +11,11 @@ mkdir -p ~openxpkilive/.config/autostart
 (echo '[Desktop Entry]'; echo 'Hidden=true') > ~openxpkilive/.config/autostart/xfce4-tips-autostart.desktop
 /etc/init.d/apache start
 # copy index from drive/USB if present
-LOCALINDEX=$(find /mnt -maxdepth 2 -name 'openxpki_live_index.html' 2>/dev/null|head -1)
+LOCALINDEX=$(find /media -maxdepth 2 -name 'openxpki_live_index.html' 2>/dev/null|head -1)
 if [ "$LOCALINDEX" != "" ]; then
     cp $LOCALINDEX /var/www/index.htm
 fi
-MOUNTPOINT=$(find /mnt -maxdepth 2 -name 'openxpki_live.ext2' 2>/dev/null|head -1)
+MOUNTPOINT=$(find /media -maxdepth 2 -name 'openxpki_live.ext2' 2>/dev/null|head -1)
 MOUNTPOINT=$(dirname $MOUNTPOINT)
 if [ "$MOUNTPOINT" == "" ]; then
     echo "No OpenXPKI live persistent image found, using temporary CA for testing only"
