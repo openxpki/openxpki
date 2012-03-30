d0910a731cc231a91c7ca19d59863011d8584d92
oliwel <github@oliwel.de>
2012-03-30 11:52:52 +0200

    Picked qatest into smartcard branch

---
+++ trunk/qatest/backend/module.mk
@@ -0,0 +1,21 @@
+# backend/module.mk
+#
+# This module file is responsible for including the individual backend test
+# modules.
+#
+# The individual sub-modules should populate TESTS_BACKEND
+
+# Add each full sub-module directory here
+MODULES_BACKEND := backend/workflow
+
+# For local modules, create module.mk.local and add additional
+# MODULES_BACKEND entries as shown above
+-include module.mk.local
+
+# include the description for each sub-module
+include $(patsubst %,%/module.mk,$(MODULES_BACKEND))
+
+# add the sub-module tests found to the full list of tests
+TESTS += $(TESTS_BACKEND)
+
+
