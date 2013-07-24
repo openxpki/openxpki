d8286496909830c8c4e269c5763d773ffe535086
Oliver Welter <github@oliwel.de>
2013-07-25 00:06:23 +0200

    Fix naming issue

--- core/server/OpenXPKI/Server/Workflow/Condition/NICE/IsCertificateIssued.pm
+++ core/server/OpenXPKI/Server/Workflow/Condition/NICE/IsCertificateIssued.pm
@@ -17,9 +17,9 @@ sub evaluate {
     my ( $self, $workflow ) = @_;
     my $context = $workflow->context();
     
-    condition_error("I18N_OPENXPKI_SERVER_CONNECTOR_VICE_CONDITION_CERTIFICATEISSUED_NOENTRY") unless $context->param('cert_identifier');
+    condition_error("I18N_OPENXPKI_SERVER_CONNECTOR_NICE_CONDITION_CERTIFICATEISSUED_NOENTRY") unless $context->param('cert_identifier');
     
-    condition_error("I18N_OPENXPKI_SERVER_CONNECTOR_VICE_CONDITION_CERTIFICATEISSUED_STILLPENDING") if $context->param('cert_identifier') eq 'pending';
+    condition_error("I18N_OPENXPKI_SERVER_CONNECTOR_NICE_CONDITION_CERTIFICATEISSUED_STILLPENDING") if $context->param('cert_identifier') eq 'pending';
     
     return 1;
 }
@@ -30,7 +30,7 @@ __END__
 
 =head1 NAME
 
-OpenXPKI::Server::Workflow::Condition::VICE::IsCertificateIssued
+OpenXPKI::Server::Workflow::Condition::NICE::IsCertificateIssued
  
 =head1 DESCRIPTION
 
