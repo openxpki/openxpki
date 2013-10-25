92b670c05e7f41ff31f8b9f78a22d6a4f5b0324f
Oliver Welter <github@oliwel.de>
2013-10-25 09:34:21 +0200

    Fix filter spec for re_cert_format

--- core/server/OpenXPKI/Server/API.pm
+++ core/server/OpenXPKI/Server/API.pm
@@ -65,7 +65,7 @@ sub BUILD {
     my $re_cert_string       = qr{ \A [A-Za-z0-9\+/=_\-\ \n]+ \z }xms;
     my $re_filename_string   = qr{ \A [A-Za-z0-9\+/=_\-\.]* \z }xms;
     my $re_image_format      = qr{ \A (ps|png|jpg|gif|cmapx|imap|svg|svgz|mif|fig|hpgl|pcl|NULL) \z }xms;
-    my $re_cert_format       = qr{ \A (PEM|DER|TXT|PKCS7) \z }xms;
+    my $re_cert_format       = qr{ \A (PEM|DER|TXT|PKCS7|HASH) \z }xms;
     my $re_crl_format        = qr{ \A (PEM|DER|TXT|HASH|RAW) \z }xms;
     my $re_privkey_format    = qr{ \A (PKCS8_PEM|PKCS8_DER|OPENSSL_PRIVKEY|PKCS12|JAVA_KEYSTORE) \z }xms;
     # TODO - consider opening up re_sql_string even more, currently this means
