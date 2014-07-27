a45ce0b3b4e014716ea20cb5efd23c2cff49a9eb
Oliver Welter <github@oliwel.de>
2014-07-27 13:42:23 +0200

    Workaround mysql bind param bug when storing metadata
    see #198 - might be reverted after bug is fixed in upstream

--- core/server/OpenXPKI/Server/Workflow/Activity/Tools/PersistCertificateMetadata.pm
+++ core/server/OpenXPKI/Server/Workflow/Activity/Tools/PersistCertificateMetadata.pm
@@ -14,31 +14,31 @@ use OpenXPKI::Serialization::Simple;
 use Data::Dumper;
 
 sub execute {
-    
+
     ##! 1: 'start'
     my $self     = shift;
     my $workflow = shift;
     my $context  = $workflow->context();
     my $params = $self->param();
-    
+
     my $ser  = OpenXPKI::Serialization::Simple->new();
 
     my $cert_identifier = $context->param('cert_identifier');
-    ##! 16: ' cert_identifier' . $cert_identifier   
-     
+    ##! 16: ' cert_identifier' . $cert_identifier
+
     # Get the profile name and style
     my $profile = $context->param('cert_profile');
     my $style = $context->param('cert_subject_style');
-    
+
     if (!$profile) {
         OpenXPKI::Exception->throw(
             message => 'I18N_OPENXPKI_SERVER_WORKFLOW_ACTIVITY_TOOLS_PERSIST_CERTIFICATE_METADATA_NO_PROFILE',
             params  => {
-                PROFILE => $profile,                
+                PROFILE => $profile,
             }
         );
     }
-    
+
     if (!$style) {
         CTX('log')->log(
             MESSAGE => "No style defined, skipping metadata",
@@ -47,37 +47,37 @@ sub execute {
         );
         return 1;
     }
-        
+
     my $cert_info  = $context->param('cert_info');
     my $subject_vars = $cert_info ? $ser->deserialize( $cert_info ) : {};
-    my $template_vars = $ser->deserialize(  $context->param('cert_subject_parts') );    
+    my $template_vars = $ser->deserialize(  $context->param('cert_subject_parts') );
     # Remove the "cert_subject" prefix and append the subject info
     foreach my $key (keys %{$template_vars}) {
-        my ($template_key) = ($key =~ m{ \A cert_subject_(.*) \z }xms); 
+        my ($template_key) = ($key =~ m{ \A cert_subject_(.*) \z }xms);
         $subject_vars->{$template_key} = $template_vars->{$key} unless(defined $subject_vars->{$template_key});
     }
     # Add params from the activity definition
     $subject_vars->{data} = $params;
-    
-    ##! 32: 'Combined vars hash ' . Dumper $subject_vars 
+
+    ##! 32: 'Combined vars hash ' . Dumper $subject_vars
 
     my $metadata = CTX('api')->render_metadata_from_template({
         PROFILE => $profile,
         STYLE   => $style,
         VARS    => $subject_vars
     });
-        
+
     if (not defined $metadata) {
         CTX('log')->log(
             MESSAGE => "No metadata for $profile / $style ",
             PRIORITY => 'info',
             FACILITY => 'application',
-        );         
+        );
         return 1;
     }
-    
+
     ##! 32: 'Metadata is ' . Dumper $metadata
-        
+
     # Add result to the cert_attributes table
     my $dbi = CTX('dbi_backend');
     foreach my $key (keys(%{$metadata})) {
@@ -88,27 +88,43 @@ sub execute {
             MESSAGE => sprintf ('cert metadata added, cert %s, attr %s, value %s',
                $cert_identifier, $key, $value),
             PRIORITY => 'info',
-            FACILITY => 'audit',        
+            FACILITY => 'audit',
         );
-                             
+
         ##! 32: 'Add new attribute ' . $key . ' value ' . $value
         my $serial = $dbi->get_new_serial(
             TABLE => 'CERTIFICATE_ATTRIBUTES',
         );
+
+        ## This is a workaround for an upstream bug in the mysql driver
+        # we expand a single dash, dot (or e,+) to the verbose "n/a"
+        # see https://github.com/openxpki/openxpki/issues/198
+        # and https://rt.cpan.org/Public/Bug/Display.html?id=97541
+
+        if ($value =~ m{ \A (-|\.|e|\+) \z }x) {
+            $value = 'n/a';
+            CTX('log')->log(
+                MESSAGE => sprintf ('Replace metadata dash/dot by verbose "n/a" on %s / %s',
+                    $cert_identifier, $key),
+                PRIORITY => 'debug',
+                FACILITY => 'application',
+            );
+        }
+
         $dbi->insert(
-            TABLE => 'CERTIFICATE_ATTRIBUTES', 
+            TABLE => 'CERTIFICATE_ATTRIBUTES',
             HASH => {
                 ATTRIBUTE_SERIAL => $serial,
                 IDENTIFIER => $cert_identifier,
                 ATTRIBUTE_KEY => 'meta_'.$key,
                 ATTRIBUTE_VALUE => $value,
             }
-        );        
+        );
     }
-    
+
     $dbi->commit();
     return 1;
-    
+
 }
-    
+
 1;
