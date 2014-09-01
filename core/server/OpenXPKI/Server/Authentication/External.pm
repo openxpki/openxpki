5d68b4a75bb757e016f8b1a02847ada6f83a3bd4
Oliver Welter <github@oliwel.de>
2014-09-01 16:54:53 +0200

    Add some documentation to External Authhandler

--- core/server/OpenXPKI/Server/Authentication/External.pm
+++ core/server/OpenXPKI/Server/Authentication/External.pm
@@ -1,4 +1,4 @@
-## OpenXPKI::Server::Authentication::External.pm 
+## OpenXPKI::Server::Authentication::External.pm
 ##
 ## Written 2005 by Martin Bartosch and Michael Bell
 ## Rewritten 2006 by Michael Bell
@@ -32,21 +32,21 @@ sub new {
     ##! 2: "load name and description for handler"
 
     $self->{DESC} = $config->get("$path.description");
-    $self->{NAME} = $config->get("$path.label"); 
+    $self->{NAME} = $config->get("$path.label");
 
     ##! 2: "load command"
-    $self->{COMMAND} = $config->get("$path.command"); 
+    $self->{COMMAND} = $config->get("$path.command");
 
     ##! 2: "command: ".$self->{COMMAND}
-    
-    $self->{ROLE} = $config->get("$path.role"); 
-    
+
+    $self->{ROLE} = $config->get("$path.role");
+
     ##! 2: "role: ".$self->{ROLE}
-    
+
     if (not length $self->{ROLE})
     {
         delete $self->{ROLE};
-        $self->{PATTERN} = $config->get("$path.pattern"); 
+        $self->{PATTERN} = $config->get("$path.pattern");
         $self->{REPLACE} = $config->get("$path.replacement");
     }
 
@@ -55,9 +55,9 @@ sub new {
 
     my @clearenv;
     my $environment = $config->get_hash("$path.env");
-    	
+
     foreach my $name (keys %{$environment}) {
-        
+
         $self->{ENV}->{$name} = $environment->{$name};
         if (exists $self->{CLEARENV})
         {
@@ -67,31 +67,31 @@ sub new {
         }
         ##! 4: "setenv: $name ::= $value"
     }
-		
+
     ##! 2: "finished"
 
     return $self;
 }
 
 sub login_step {
-    ##! 1: 'start' 
+    ##! 1: 'start'
     my $self    = shift;
     my $arg_ref = shift;
- 
+
     my $name    = $arg_ref->{HANDLER};
     my $msg     = $arg_ref->{MESSAGE};
     my $answer  = $msg->{PARAMS};
 
     if (! exists $msg->{PARAMS}->{LOGIN} ||
         ! exists $msg->{PARAMS}->{PASSWD}) {
-        ##! 4: 'no login data received (yet)' 
-        return (undef, undef, 
+        ##! 4: 'no login data received (yet)'
+        return (undef, undef,
             {
-		SERVICE_MSG => "GET_PASSWD_LOGIN",
-		PARAMS      => {
+        SERVICE_MSG => "GET_PASSWD_LOGIN",
+        PARAMS      => {
                     NAME        => $self->{NAME},
                     DESCRIPTION => $self->{DESC},
-	        },
+            },
             },
         );
     }
@@ -106,13 +106,13 @@ sub login_step {
     foreach my $name (keys %{$self->{ENV}})
     {
         my $value = $self->{ENV}->{$name};
-	# we don't want to see expanded passwords in the log file,
-	# so we just replace the password after logging it
-	$value =~ s/__USER__/$account/g;
-	$value =~ s/__PASSWD__/$passwd/g;
+    # we don't want to see expanded passwords in the log file,
+    # so we just replace the password after logging it
+    $value =~ s/__USER__/$account/g;
+    $value =~ s/__PASSWD__/$passwd/g;
 
-	# set environment for executable
-	$ENV{$name} = $value;
+    # set environment for executable
+    $ENV{$name} = $value;
     }
     my $command = $self->{COMMAND};
     ##! 2: "execute command"
@@ -121,7 +121,7 @@ sub login_step {
     # is taken literally from the configuration.
     # NOTE: do not extend this code to allow login parameters
     # to be passed on the command line.
-    # - the credentials may be visible in the OS process 
+    # - the credentials may be visible in the OS process
     #   environment
     # - worse yet, it is untrusted user input that might
     #   easily be used to execute arbitrary commands on the
@@ -131,14 +131,14 @@ sub login_step {
     map { delete $ENV{$_} } @{$self->{CLEARENV}}; # clear environment
 
     ##! 2: "command returned $?, STDOUT was: $out"
-		
+
     if ($? != 0)
     {
         OpenXPKI::Exception->throw (
             message => "I18N_OPENXPKI_SERVER_AUTHENTICATION_EXTERNAL_LOGIN_FAILED",
             params  => {
-		USER => $account,
-	    });
+        USER => $account,
+        });
         return (undef, undef, {});
     }
 
@@ -147,13 +147,15 @@ sub login_step {
     if (! exists $self->{ROLE})
     {
         $out =~ s/$self->{PATTERN}/$self->{REPLACE}/ if (defined $self->{PATTERN} && defined $self->{REPLACE});
-        
+
+        # trimming does not hurt
+        $out =~ s/\s+$//g;
         # Assert if the role is not defined
         if (!CTX('config')->get_hash("auth.roles.$out")) {
             OpenXPKI::Exception->throw (
                 message => "I18N_OPENXPKI_SERVER_AUTHENTICATION_EXTERNAL_UNKNOWN_ROLE",
                 params  => { ROLE => $out });
-        }        
+        }
         $self->{ROLE} = $out;
     }
 
@@ -184,15 +186,49 @@ via an external program. The parameters are passed as a hash reference.
 
 is the constructor. It requires the config prefix as single argument.
 
-This is the minimum parameter set for any authentication class.
-The configuration includes a command tag and a role or a regular expression
-configuration (pattern and replacement). Additionally it is possible to
-specify environment variables. The tag env must include a name and a value
-parameter. Please note that the strings __USER__ and __PASSWORD__ in the value
-parameter will be replaced by the entered user and passphrase.
+=head1 Configuration
 
-=head2 login
+=head2 Exit code only / static role
 
-returns (user, role, service ready message) triple if login was
-successful, (undef, undef, {}) otherwise.
+In this mode, you need to specify the role for the user as a static value
+inside the configuration. The Username/Password is passed via the environment.
+
+ MyHandler:
+   type: External
+   label: My Auth Handler
+   command: /path/to/your/script
+   role: 'RA Operator'
+     env:
+        LOGIN: __USER__
+        PASSWD: __PASSWD__
+
+The login will succeed if the script has exitcode 0. Here is a stub that
+logs in user "john" with password "doe":
+
+  #!/bin/bash
+
+  if [ "$LOGIN" == "john" ] && [ "$PASSWD" == "doe" ]; then
+    exit 0;
+  fi;
+
+  exit 1;
+
+=head2 Output evaluation
+
+If you do not set the role in the configuration, it is determined from the
+scripts output. Trailing spaces are always stripped by the handler internally.
+If your output needs more postprocessing (e.g. strip away a prefix), you can
+specify a pattern and replacement, that are placed into a perl regex and
+applied to the output.
+
+ MyHandler:
+   type: External
+   label: My Auth Handler
+   command: /path/to/your/script
+   role: ''
+   pattern: 'role_'
+   replacement: ''
+     env:
+        LOGIN: __USER__
+        PASSWD: __PASSWD__
 
