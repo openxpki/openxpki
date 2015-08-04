1637d4cd517b262b166a759f8c8e9640ffce1285
Oliver Welter <github@oliwel.de>
2015-08-04 12:46:31 +0200

    Remove obsolete / xml based tests

--- core/server/t/60_workflow/36_enrollment.d/mocklib/OpenXPKI/VERSION.pm
+++
@@ -1,9 +0,0 @@
-# This mock module allows us to use the oxi libs directly from the
-# git working repo rather than installing, which is what creates 
-# the OpenXPKI::VERSION
-#
-
-package OpenXPKI::VERSION;
-our $VERSION = '0.10.0';
-1;
-
