2d1bdd6ed5cf8e60028e340c5eba7846c66621b9
Oliver Welter <github@oliwel.de>
2015-10-22 18:08:25 +0200

    Add new crypto command "asn1parse -genconf"

---
+++ core/server/OpenXPKI/Crypto/Backend/OpenSSL/Command/asn1_genconf.pm
@@ -0,0 +1,53 @@
+use strict;
+use warnings;
+
+package OpenXPKI::Crypto::Backend::OpenSSL::Command::asn1_genconf;
+
+use base qw(OpenXPKI::Crypto::Backend::OpenSSL::Command);
+
+sub get_command
+{
+    my $self = shift;
+
+    if (!$self->{DATA}) {
+        OpenXPKI::Exception->throw (
+            message => "I18N_OPENXPKI_CRYPTO_OPENSSL_COMMAND_ASN1_GENCONF_DATA_MISSING"
+        );
+    }
+    
+    $self->get_tmpfile ('IN');
+    $self->write_file (
+        FILENAME => $self->{INFILE},
+        CONTENT  => $self->{DATA},
+        FORCE    => 1);
+    
+    $self->get_tmpfile ('OUT');
+
+    my $command = "asn1parse ";
+    
+    $command .= "-genconf " . $self->{INFILE};    
+    $command .= " -out ". $self->{OUTFILE};
+    
+
+    return [ $command ];
+}
+
+sub hide_output
+{
+    return 1;
+}
+
+sub key_usage
+{
+    return 0;
+}
+
+sub get_result
+{
+    my $self = shift;
+    return $self->read_file ($self->{OUTFILE});
+}
+
+
+1;
+__END__
