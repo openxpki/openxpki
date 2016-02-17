c23071017146947e1128e9db19c0d9ff8bcb33d0
Oliver Welter <github@oliwel.de>
2016-02-17 18:33:36 +0100

    Fix and cleanup Tools::SetContext activity
    Removed the legacy parameter settings which have not be in use since
    the rewrite. As the class was broken, I doubt anybody used it beyond
    our sample code.

--- core/server/OpenXPKI/Server/Workflow/Activity/Tools/SetContext.pm
+++ core/server/OpenXPKI/Server/Workflow/Activity/Tools/SetContext.pm
@@ -21,88 +21,27 @@ sub execute
     my $workflow   = shift;
     my $context    = $workflow->context();
 
-    ##! 16: 'SetContext'
-
-    my %options = (
-	overwritecontext => 0,
-	extendedsyntax   => 0,
-	);
-
-    my $parameters = $self->param('SetContextParameters');
-    foreach my $entry (split /,\s*/, $parameters) {
-	my ($bool, $entry) = ($entry =~ m{ \A (!?)(.*) \z }xms);
-	$bool = 0 + ($bool ne '!');
-	$options{$entry} = $bool;
-    }
-    ##! 16: 'options: ' . Dumper \%options
+    ##! 1: 'start'
+  
     ##! 16: ' parameters: ' . Dumper $self->{PARAMS}
+    
   KEY:
-    foreach my $key (%{$self->{PARAMS}}) {
-	next KEY if ($key eq 'SetContextParameters');
-	my $value = $self->param($key);
+    foreach my $key (keys %{$self->{PARAMS}}) {
 
-	# execute configured value in current context. since $value comes
-	# only from the configured parameters ($self->param) instead of the
-	# context, this is safe as long as the configuration file is not
-	# compromised.
-	if ($options{extendedsyntax}) {
-	    $value = eval $value;
-
-	    if ($EVAL_ERROR) {
-		OpenXPKI::Exception->throw(
-		    message =>
-		    'I18N_OPENXPKI_SERVER_WORKFLOW_ACTIVITY_TOOLS_SETCONTEXT_INVALID_EXTENDED_SYNTAX',
-		    params => {
-			EVAL_ERROR => $EVAL_ERROR,
-		    },
-		    log => {
-			logger   => CTX('log'),
-			priority => 'error',
-			facility => 'system',
-		    },
-		    );
+        ##! 16: 'Key ' . $key
+    	my $value = $self->param($key);
+     
+    	if (defined $value) {
+		    $context->param($key => $value);
 	    }
-	    # allow anonymous subroutines in configuration
-	    if (ref $value eq 'CODE') {
-		eval {
-		    $value = &{$value}($workflow);
-		};
-		if ($EVAL_ERROR) {
-		    OpenXPKI::Exception->throw(
-			message =>
-			'I18N_OPENXPKI_SERVER_WORKFLOW_ACTIVITY_TOOLS_SETCONTEXT_INVALID_EXTENDED_SYNTAX_CODEREF',
-			params => {
-			    EVAL_ERROR => $EVAL_ERROR,
-			},
-			log => {
-			    logger   => CTX('log'),
-			    priority => 'error',
-			    facility => 'system',
-			},
-			);
-		}
-	    }
-	}
-	
-	my $old = $context->param($key);
-	if (! defined $old) {
-	    ##! 16: "setting context $key: $value"
-	    $context->param($key => $value);
-	} else {
-	    if ($options{overwritecontext}) {
-		##! 16: "overwriting context $key: $value"
-		$context->param($key => $value);
-	    }
-	}
          
-    CTX('log')->log(
-        MESSAGE => "Setting context $key to $value",
-        PRIORITY => 'debug',
-        FACILITY => [ 'application', ],
-    );
-             
+        CTX('log')->log(
+            MESSAGE => "Setting context $key to $value",
+            PRIORITY => 'debug',
+            FACILITY => [ 'application', ],
+        );
     }
-
+             
     return 1;
 }
 
@@ -115,28 +54,15 @@ OpenXPKI::Server::Workflow::Activity::Tools::SetContext
 
 =head1 Description
 
-Set context parameters from the activity definition.
+Set context parameters from the activity definition. 
 
-This allows to explicitly set workflow context parameters from the XML
-configuration of this activity.
+=head2 Configuration 
 
-Option parameter (set in action definition):
-SetContextParameters   - comma separated list of options
+    class: OpenXPKI::Server::Workflow::Activity::Tools::SetContext
+    param:
+       token: certsign,datasafe       
+       
+This will create a new context item with key "token" and value 
+"certsign,datasafe".       
 
-Possible values:
-!overwritecontext   (DEFAULT) Keep original context value if it exists
-overwritecontext              Overwrite context value
 
-!extendedsyntax     (DEFAULT) Standard behaviour
-extendedsyntax                Configured values are evaluated as perl code
-                              (see below).
-
-=head1 Extended Syntax
-
-If the extendedsyntax option is set, the configured values are evaluated
-as Perl
-                              Allows access to internal data structures,
-                              most prominently the workflow context via
-                              the $context variable.
-                              NOTE: use with caution, improper use may cause
-                              security problems.
