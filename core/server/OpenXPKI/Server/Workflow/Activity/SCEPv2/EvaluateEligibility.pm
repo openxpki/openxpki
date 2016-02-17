6546f9f47eb4a04dff367394a8f10935dad6ed20
Oliver Welter <github@oliwel.de>
2016-02-17 18:35:59 +0100

    Enhance EvalEligibility activity to allow validation of the return value
    Adds the "expect" paramter to only become true on specific return values,
    also adds the verbose return value to the context for debugging or later
    processing.

--- core/server/OpenXPKI/Server/Workflow/Activity/SCEPv2/EvaluateEligibility.pm
+++ core/server/OpenXPKI/Server/Workflow/Activity/SCEPv2/EvaluateEligibility.pm
@@ -63,29 +63,55 @@ sub execute {
 
         ##! 16: 'Lookup at path ' . Dumper @path
         if (@path) {
-            my $plain_result = $config->get( [ @prefix, 'value', @path ] ) ;
-            ##! 32: 'result is ' . $plain_result
-            # Evaluate whatever comes back to a boolean 0/1 f
-            $res = $plain_result ? 1 : 0;
-
+            my $plain_result = $config->get( [ @prefix, 'value', @path ] );
+            ##! 32: 'result is ' . $plain_result        
+    
             CTX('log')->log(
                 MESSAGE => "SCEP eligibility check raw result " . $plain_result . ' using path ' . join('|', @path),
                 PRIORITY => 'debug',
                 FACILITY => 'application',
             );
-        }
-    } else {
-    # No attribs, static case
-      my $plain_result = $config->get( [ @prefix, 'value' ] ) ;
-      ##! 32: 'static check - result is ' . Dumper $plain_result
-      # check the ref and explicit return to make sure it was not a stupid config
-      $res = (ref $plain_result eq '' && $plain_result eq '1');
+            
+            $context->param( 'eligibility_result' => $plain_result );  
+                
+            # If a list of expected values is given, we check the return value 
+            my @expect= $config->get_scalar_as_list( [ @prefix, 'expect' ] );
+            if (defined $plain_result && defined $expect[0]) {
 
-      CTX('log')->log(
-        MESSAGE => "SCEP eligibility check without path - result " . $plain_result,
-        PRIORITY => 'debug',
-        FACILITY => 'application',
-      );
+                ##! 32: 'Check against list of expected values'
+                foreach my $valid (@expect) {
+                    ##! 64: 'Probe ' .$valid                     
+                    if ($plain_result eq $valid) {
+                        $res = 1;
+                        ##! 32: 'Match found' .$valid
+                        last;
+                    }
+                }
+                
+                CTX('log')->log(
+                    MESSAGE => "SCEP eligibility check for expected value " . ($res ? 'succeeded' : 'failed'),
+                    PRIORITY => 'debug',
+                    FACILITY => 'application',
+                );
+                
+            } else {                
+                # Evaluate whatever comes back to a boolean 0/1 f                
+                $res = $plain_result ? 1 : 0;                
+            }            
+        }
+        
+    } else {
+        # No attribs, static case
+        my $plain_result = $config->get( [ @prefix, 'value' ] );
+        ##! 32: 'static check - result is ' . Dumper $plain_result
+        # check the ref and explicit return to make sure it was not a stupid config
+        $res = (ref $plain_result eq '' && $plain_result eq '1');
+
+        CTX('log')->log(
+            MESSAGE => "SCEP eligibility check without path - result " . $plain_result,
+            PRIORITY => 'debug',
+            FACILITY => 'application',
+        );
 
     }
 
@@ -114,6 +140,9 @@ OpenXPKI::Server::Workflow::Activity::SCEPv2::EvaluateEligibility
 Check the eligability to perform initial enrollment or renewal against the
 connector. The activity detects if we are in initial or renewal mode and
 writes the decission to "request_mode".
+
+=head2 Configuration 
+
 The data source must be configured in the config of the running scep
 server:
 
@@ -122,8 +151,8 @@ server:
       initial:
         value@: connector:your.connector
         args:
-        - "[% context.cert_subject %]"
-        - "[% context.url_mac %]"
+         - "[% context.cert_subject %]"
+         - "[% context.url_mac %]"
 
       renewal: ''
 
@@ -132,9 +161,26 @@ subject and mac address (gathered by url parameter), e.g.:
 
    your.connector.cn=foo,dc=bar.00:01:02:34:56:78
 
-Renewal is disabled as the path is empty.
+If the connector returns a true value, the enrollment is granted. 
+Renewal is disabled as the path is empty. 
 
 =head2 Configuration alternatives
+
+If you need to make the decission based on the return value, you can add 
+a list of expected values to the definition: 
+
+    initial:
+      value@: connector:your.connector
+      args:
+        - "[% context.cert_subject %]"
+        - "[% context.url_mac %]"
+      expected:
+        - Active
+        - Build
+
+The check will succeed, if the value returned be the connector has a literal
+match in the given list. 
+
 
 To globally enable a feature without taking the request into account, omit the
 args and set value to a literal 1:
@@ -147,3 +193,5 @@ args and set value to a literal 1:
       renewal:
         value: 1
 
+Sidenote: You can use a connector here as well, but in static mode we always 
+test for a literal "1" as return value! 
