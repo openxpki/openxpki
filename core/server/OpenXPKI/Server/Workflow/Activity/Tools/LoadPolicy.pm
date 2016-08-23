60de0bd50b6d3a65cb3a117e87ba999fb21eeefd
Oliver Welter <github@oliwel.de>
2016-08-23 19:58:03 +0200

    New activity LoadPolicy
    Generalized version of SCEPv2/FetchGroupPolicy

---
+++ core/server/OpenXPKI/Server/Workflow/Activity/Tools/LoadPolicy.pm
@@ -0,0 +1,70 @@
+package OpenXPKI::Server::Workflow::Activity::Tools::LoadPolicy;
+
+use strict;
+use base qw( OpenXPKI::Server::Workflow::Activity );
+
+use OpenXPKI::Server::Context qw( CTX );
+use OpenXPKI::Exception;
+use OpenXPKI::Debug;
+use Data::Dumper;
+
+sub execute {
+    ##! 1: 'execute'
+    my $self       = shift;
+    my $workflow   = shift;
+    my $context   = $workflow->context();
+    my $config = CTX('config');
+    
+    my $policy_params;
+    my $config_path = $self->param('config_path');
+    if ($config_path) {
+        ##! 8: 'Load by path ' . $config_path
+        $policy_params = $config->get_hash($config_path);
+    } else {
+        my $server = $context->param('server');
+        my $interface = $context->param('interface');
+        ##! 8: 'Load for server ' . $interface .  ' / ' .  $server
+        $policy_params = $config->get_hash([ $interface, $server, 'policy' ]);
+    }
+
+    foreach my $key (keys (%{$policy_params})) {
+        $context->param( "p_$key" => $policy_params->{$key} );
+    }
+
+    CTX('log')->log(
+        MESSAGE => "Server policy loaded",            
+        PRIORITY => 'debug',
+        FACILITY => 'application',
+    );       
+
+
+    return 1;
+}
+
+1;
+__END__
+
+=head1 Name
+
+OpenXPKI::Server::Workflow::Activity::Tools::LoadPolicy
+
+=head1 Description
+
+Load the policy section for a server endpoint. 
+
+The path to load defaults to $interface.$server.policy where interface
+and server are read from the context. You can override the full path by 
+setting the key I<config_path>.
+
+The given path is expected to return a hash, each key/value pair is read 
+into  the context with the I<p_> prefix added to each key! 
+
+=head2 Activity Parameter
+
+=over
+
+=item config_path
+
+Explict path to read the policy from.
+
+=back
