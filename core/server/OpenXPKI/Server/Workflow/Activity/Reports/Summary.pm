ff39e3efc4ba5665730c943f1e33a762021f5fee
Oliver Welter <github@oliwel.de>
2016-09-21 10:24:03 +0200

    Migrate Report::Summary to new database layer
    Also fixup/redefines some of the numbers (revoked status was not
    considered correctly for all queries)

--- core/server/OpenXPKI/Server/Workflow/Activity/Reports/Summary.pm
+++ core/server/OpenXPKI/Server/Workflow/Activity/Reports/Summary.pm
@@ -12,6 +12,7 @@ use DateTime;
 
 sub execute {
     
+    ##! 1: 'Start'
     my $self = shift;
     my $workflow = shift;
     
@@ -29,52 +30,105 @@ sub execute {
     }
     
     my $epoch = $valid_at->epoch();
-            
-    my $query = {
-        TABLE => [ 'CERTIFICATE' ],
-        JOIN => [ ['IDENTIFIER'] ],
-        DYNAMIC => { 
-            'CERTIFICATE.CSR_SERIAL' => { VALUE => undef, OPERATOR => 'NOT_EQUAL' },
-            'CERTIFICATE.PKI_REALM' => { VALUE => $pki_realm }
-        }        
-    };
+             
+    # For special data types see:
+    #  - https://metacpan.org/pod/SQL::Abstract::More#BIND-VALUES-WITH-TYPES
+    #  - https://metacpan.org/pod/DBI#bind_param
+    my %base_query = (
+        from => 'certificate',
+    );
+    my %base_conditions = (
+        req_key => { '!=' => undef },
+        pki_realm => $pki_realm,
+    );
     
-    # as this brain dead SQL layer mangles the query hash in place when executed
-    # we need to set columns key each time 
+    my $db = CTX('dbi');
+    my $tuple;
+    my $result = {};
     
     # total count
-    $query->{COLUMNS} = [{ COLUMN  => 'CERTIFICATE.IDENTIFIER', AGGREGATE => 'COUNT' }];
-    $context->param( 'total_count', $self->_query( $query ) + 0 );
+    $tuple = $db->select_one(%base_query,
+        columns  => [ 'COUNT(identifier)|amount' ],
+        where => {
+            %base_conditions
+        }
+    );
+    $result->{total_count} = sprintf "%01d", $tuple->{amount};
     
-    # total_revoked
-    $query->{COLUMNS} = [{ COLUMN  => 'CERTIFICATE.IDENTIFIER', AGGREGATE => 'COUNT' }];    
-    $query->{DYNAMIC}->{'CERTIFICATE.STATUS'} = [ 'REVOKED', 'CRL_ISSUANCE_PENDING' ];
-    $context->param( 'total_revoked', $self->_query( $query ) + 0 );
     
-    # total_distinct
-    $query->{COLUMNS} = [{ COLUMN  => 'CERTIFICATE.SUBJECT', AGGREGATE => 'UNIQUE' }];
-    $query->{DYNAMIC}->{'CERTIFICATE.STATUS'} = 'ISSUED';
-    $context->param( 'total_distinct', $self->_query( $query ) + 0 );
-
-    # total_expired
-    $query->{COLUMNS} = [{ COLUMN  => 'CERTIFICATE.IDENTIFIER', AGGREGATE => 'COUNT' }];
-    $query->{DYNAMIC}->{'CERTIFICATE.NOTAFTER'} = { VALUE => $epoch, OPERATOR => 'LESS_THAN' };
-    $context->param( 'total_expired', $self->_query( $query ) + 0 );
+    # Revoked
+    $tuple = $db->select_one(%base_query,
+        columns  => [ 'COUNT(identifier)|amount' ],
+        where => {
+            %base_conditions,
+            status => [ 'REVOKED', 'CRL_ISSUANCE_PENDING' ],
+        }
+    );
+    $result->{total_revoked} = sprintf "%01d", $tuple->{amount} + 0;
     
-    # valid_count
-    $query->{COLUMNS} = [{ COLUMN  => 'CERTIFICATE.IDENTIFIER', AGGREGATE => 'COUNT' }];
-    $query->{DYNAMIC}->{'CERTIFICATE.NOTBEFORE'} = { VALUE => $epoch, OPERATOR => 'LESS_THAN' };
-    $query->{DYNAMIC}->{'CERTIFICATE.NOTAFTER'} = { VALUE => $epoch, OPERATOR => 'GREATER_THAN' };
-    $context->param( 'valid_count', $self->_query( $query ) + 0 );
-
-    # valid_distinct
-    $query->{COLUMNS} = [{ COLUMN  => 'CERTIFICATE.SUBJECT', AGGREGATE => 'UNIQUE' }];
-    $query->{DYNAMIC}->{'CERTIFICATE.NOTBEFORE'} = { VALUE => $epoch, OPERATOR => 'LESS_THAN' };
-    $query->{DYNAMIC}->{'CERTIFICATE.NOTAFTER'} = { VALUE => $epoch, OPERATOR => 'GREATER_THAN' };
-    $context->param( 'valid_distinct', $self->_query( $query ) + 0 );
-      
-    # Expiry cutoff date
     
+    # valid revoked
+    $tuple = $db->select_one(%base_query,
+        columns  => [ 'COUNT(identifier)|amount' ],
+        where => {
+            %base_conditions,
+            status => [ 'REVOKED', 'CRL_ISSUANCE_PENDING' ],
+            notbefore => { '<' => $epoch },
+            notafter  => { '>' => $epoch },
+        }
+    );
+    $result->{valid_revoked} = sprintf "%01d", $tuple->{amount} + 0;
+    
+    
+    # Distinct
+    $tuple = $db->select_one(%base_query,
+        columns  => [ 'COUNT(DISTINCT subject)|amount' ],
+        where => {
+            %base_conditions,
+        }
+    );
+    $result->{total_distinct} = sprintf "%01d", $tuple->{amount} + 0;
+    
+    
+    # Expired
+    $tuple = $db->select_one(%base_query,
+        columns  => [ 'COUNT(identifier)|amount' ],
+        where => {
+            %base_conditions,
+            status => 'ISSUED',
+            notafter => { '<' => $epoch },
+        }
+    );
+    $result->{total_expired} = sprintf "%01d", $tuple->{amount} + 0;
+    
+    
+    # Valid
+    $tuple = $db->select_one(%base_query,
+        columns  => [ 'COUNT(identifier)|amount' ],
+        where => {
+            %base_conditions,
+            status => 'ISSUED',
+            notbefore => { '<' => $epoch },
+            notafter  => { '>' => $epoch },
+        }
+    );
+    $result->{valid_count} = sprintf "%01d", $tuple->{amount} + 0;
+    
+    
+    # Valid distinct
+    $tuple = $db->select_one(%base_query,
+        columns  => [ 'COUNT(DISTINCT subject)|amount' ],
+        where => {
+            %base_conditions,
+            status => 'ISSUED',
+            notbefore => { '<' => $epoch },
+            notafter  => { '>' => $epoch },
+        }
+    );
+    $result->{valid_distinct} = sprintf "%01d", $tuple->{amount} + 0;
+    
+    
+    # Near expiry
     my $near_expiry_validity = $self->param('near_expiry') || '+000030';    
     my $expiry_cutoff = OpenXPKI::DateTime::get_validity({
         REFERENCEDATE => $valid_at,
@@ -82,50 +136,41 @@ sub execute {
         VALIDITYFORMAT => 'detect',
     })->epoch();
     
-    # near_expiry
-    $query->{COLUMNS} = [{ COLUMN  => 'CERTIFICATE.IDENTIFIER', AGGREGATE => 'COUNT' }];   
-    $query->{DYNAMIC}->{'CERTIFICATE.NOTAFTER'} = { OPERATOR => 'BETWEEN', VALUE => [ $epoch, $expiry_cutoff ] };
-    $context->param( 'near_expiry', $self->_query( $query ) + 0 );
-
-
+    $tuple = $db->select_one(%base_query,
+        columns  => [ 'COUNT(identifier)|amount' ],
+        where => {
+            %base_conditions,
+            status => 'ISSUED',
+            notafter  => { -between => [ $epoch, $expiry_cutoff  ] },
+        }
+    );
+    $result->{near_expiry} = sprintf "%01d", $tuple->{amount} + 0;
+    
+    
+    # Recent expiry
     my $recent_expiry_validity = $self->param('recent_expiry') || '-000030';
     $expiry_cutoff = OpenXPKI::DateTime::get_validity({
         REFERENCEDATE => $valid_at,
         VALIDITY => $recent_expiry_validity,
         VALIDITYFORMAT => 'detect',
     })->epoch();    
-
-    $query->{COLUMNS} = [{ COLUMN  => 'CERTIFICATE.IDENTIFIER', AGGREGATE => 'COUNT' }];    
-    $query->{DYNAMIC}->{'CERTIFICATE.NOTAFTER'} = { OPERATOR => 'BETWEEN', VALUE => [ $expiry_cutoff, $epoch ] };    
-    $context->param( 'recent_expiry', $self->_query( $query ) + 0 );
     
-    
+    $tuple = $db->select_one(%base_query,
+        columns  => [ 'COUNT(identifier)|amount' ],
+        where => {
+            %base_conditions,
+            status => 'ISSUED',
+            notafter  => { -between => [ $expiry_cutoff, $epoch ] },
+        }
+    );
+    $result->{recent_expiry} = sprintf "%01d", $tuple->{amount};
+      
+      
+    ##! 32: 'Report result ' . Dumper $result  
+    $context->param( $result );    
       
 } 
-
-sub _query {
-    
-    my $self = shift;
-    my $query = shift;
-
-    ##! 32: 'Query ' . Dumper $query
-    my $col = $query->{COLUMNS}->[0]->{COLUMN};
-    
-    my $result = CTX('dbi_backend')->select(%{$query});
-    
-    ##! 32: 'Result ' . Dumper $result
-    
-    if (!(defined $result && ref $result eq 'ARRAY' && scalar @{$result} == 1)) {
-        OpenXPKI::Exception->throw(
-            message => 'I18N_OPENXPKI_SERVER_API_OBJECT_SEARCH_CERT_COUNT_SELECT_RESULT_NOT_ARRAY',
-            params => { 'TYPE' => ref $result, },
-        );
-    }
-        
-    return $result->[0]->{ $col };
-    
-}
-
+ 
 1;
 __END__
 
@@ -189,23 +234,33 @@ Number of certificates in revoked status (includes CRL pending).
 
 =item total_expired
 
-Number of expired certificates
+Number of expired certificates (without revoked)
 
 =item total_distinct
 
-Number of distinct subjects
+Number of distinct subjects (includes revoked)
 
 =item valid_count
 
-Number of valid certificates
+Number of valid certificates (in validity window and not revoked)
 
 =item valid_distinct
 
 Number of distinct subjects within valid certificates
 
-=item near_expiry, recent_expiry
+=item valid_revoked
 
-Number of valid (not revoked) certificates within expiry window.
+Number of certificates that are in validity window but revoked.
+
+=item near_expiry
+
+Number of valid (not revoked) certificates that will expiry within 
+the given window.
+
+=item recent_expiry
+
+Number of valid (not revoked) certificates that have been expired 
+within the given window.
 
 =back
  
