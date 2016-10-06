27aa31657f5ea609ee25636e81c23292d2913cf7
Oliver Welter <github@oliwel.de>
2016-10-06 17:35:04 +0200

    Support activity parameters and targer_key in CSR::GeneratePKCS10

--- core/server/OpenXPKI/Server/Workflow/Activity/CSR/GeneratePKCS10.pm
+++ core/server/OpenXPKI/Server/Workflow/Activity/CSR/GeneratePKCS10.pm
@@ -20,9 +20,17 @@ sub execute
     my $workflow   = shift;
     my $context    = $workflow->context();
     my $default_token = CTX('api')->get_default_token();
-    my $private_key = $context->param('private_key');
-    my $password    = $context->param('_password');
-    my $subject     = $context->param('cert_subject');
+    
+    my $private_key = $self->param('private_key');
+    my $password    = $self->param('password');
+    my $subject     = $self->param('cert_subject');
+    my $target_key  = $self->param('target_key') || 'pkcs10';
+
+    # fallback for old workflows
+    $private_key = $context->param('private_key') unless($private_key);
+    $password    = $context->param('_password') unless($password);
+    $subject     = $context->param('cert_subject') unless($subject);
+    
 
     my $pkcs10 = $default_token->command({
         COMMAND => 'create_pkcs10',
@@ -40,7 +48,7 @@ sub execute
         FACILITY => 'application',
     );
 
-    $context->param('pkcs10' => $pkcs10);
+    $context->param($target_key => $pkcs10);
 
     return 1;
 }
@@ -58,3 +66,31 @@ Creates a new PKCS#10 certificate signing request from the
 key and password given in the context parameters private_key
 and _password.
 This request is saved in the context parameter pkcs10.
+
+=head1 Configuration
+
+=head2 Activity Parameters
+
+To support legacy workflows, the values for private_key, password and 
+cert_subject are read from the context if not set in the activity 
+definition. This behaviour is deprecated and will be removed in the future.
+
+=over
+
+=item target_key
+
+Context key to write the generated PKCS10 to, default is 'pksc10'
+
+=item private_key
+
+The private key to use for the request.
+
+=item password
+
+The password for the private key.
+
+=item cert_subject
+
+The full subject to set in the request.
+
+=back
