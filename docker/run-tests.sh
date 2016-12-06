4a912cf906b8d9c151da09ae5ffb0c8fbb847da5
Jens Berthold <jens@jebecs.de>
2016-12-06 01:16:15 +0100

    Find and install missing Perl dependencies in Docker container

--- docker/run-tests.sh
+++ docker/run-tests.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+branch="develop"
+repo="openxpki/openxpki"
+
+test ! -z "$1" && branch="$1"
+test ! -z "$2" && repo="$2"
+
+this_dir=$(dirname $0)
+root=$(readlink -e $this_dir/../)
+
 set -e
-docker build $(dirname $0)/test -t openxpki-test
-docker run -t -i --rm openxpki-test maxhq/openxpki develop
+docker build $(dirname $0)/test -t oxi-test
+docker run -t -i --rm -v $root:/repo oxi-test $branch $repo
--- docker/test/startup.sh
+++ docker/test/startup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-GITHUB_USER_REPO="$1"
-BRANCH="$2"
+BRANCH="$1"
+GITHUB_USER_REPO="$2"
 
-echo "====[ Start MySQL ]===="
+echo -e "\n====[ MySQL ]===="
 nohup sh -c mysqld >/tmp/mysqld.log &
 
 #
 # Wait for database initialization
 #
-echo "MySQL: waiting for DB initialize (max. 120 seconds)"
+echo "Waiting for DB initialize (max. 120 seconds)"
 sec=0; error=1
 while [ $error -ne 0 -a $sec -lt 60 ]; do
     error=$(echo "quit" | mysql -h 127.0.0.1 -uroot --connect_timeout=1 2>&1 | grep -c ERROR)
@@ -27,7 +27,7 @@ set -e
 #
 # Database setup
 #
-echo "MySQL: setting up database (user + schema)"
+echo "Preparing database for OpenXPKI (user + schema)"
 
 cat <<__SQL | mysql -h 127.0.0.1 -uroot
 DROP database IF EXISTS $OXI_TEST_DB_MYSQL_NAME;
@@ -37,17 +37,82 @@ GRANT ALL ON $OXI_TEST_DB_MYSQL_NAME.* TO '$OXI_TEST_DB_MYSQL_USER'@'%';
 flush privileges;
 __SQL
 
+#
+# Repository clone
+#
+set +e
 
-echo "====[ Git checkout: $BRANCH from $GITHUB_USER_REPO ]===="
-git clone --depth=1 --branch=$BRANCH https://github.com/$GITHUB_USER_REPO.git /opt/openxpki
+# Default: remote Github repo
+REPO=https://dummy:nope@github.com/$GITHUB_USER_REPO.git
 
-echo "====[ Compile and test OpenXPKI ]===="
+# Local repo from host (if Docker volume is mounted)
+mountpoint -q /repo && REPO=file:///repo
 
+echo -e "\n====[ Git checkout: $BRANCH from $REPO ]===="
+git ls-remote -h $REPO >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+    2>&1 echo "ERROR: Git repo either does not exist or is not readable for everyone"
+    exit 1
+fi
+set -e
+git clone --depth=1 --branch=$BRANCH $REPO /opt/openxpki
+
+#
+# Grab and install Perl module dependencies from Makefile.PL using PPI
+#
+echo -e "\n====[ Scanning Makefile.PL for new Perl dependencies ]===="
+cpanm --quiet --notest PPI
+
+perl -e '
+use PPI;
+use PPI::Dumper;
+
+$doc = PPI::Document->new("/opt/openxpki/core/server/Makefile.PL") or die "Makefile.PL not found";
+$doc->prune("PPI::Token::Whitespace");
+$doc->prune("PPI::Token::Comment");
+
+my $sub = $doc->find_first(sub {                         # find
+    $_[1]->parent == $_[0]                               # at root level
+    and $_[1]->isa("PPI::Statement")                     # a statement
+    and $_[1]->first_element->content eq "WriteMakefile" # called "WriteMakefile"
+}) or die "Subroutine call WriteMakefile() not found\n";
+
+$key = $sub->find_first(sub {                            # below that find
+    $_[1]->isa("PPI::Token::Quote")                      # a quoted string
+    and $_[1]->content =~ /PREREQ_PM/                    # called "PREREQ_PM"
+}) or die "Argument PREREQ_PM not found in WriteMakefile()\n";
+
+$list = $key->next_sibling->next_sibling; # skip "=>" and go to HashRef "{}"
+$list->prune("PPI::Token::Operator");     # remove all "=>"
+%modmap = map { s/(\x27|\x22)//g; $_ }    # remove single or double quotes
+    map { $_->content }
+    @{$list->find("PPI::Token")};
+
+use version;
+my @modlist =
+    map { "$_~".$modmap{$_} }
+    grep {
+        ! (
+            eval "require $_;" and
+            eval "version->parse($_->VERSION) >= version->parse($modmap{$_})"
+        )
+    }
+    keys %modmap;
+
+if (@modlist) {
+    print "installing missing dependencies\n";
+    system("cpanm --quiet --notest ".join(" ", @modlist));
+}
+'
+
+#
+# Testing
+#
+echo -e "\n====[ Compile and test OpenXPKI ]===="
 # Config::Versioned reads USER env variable
 export USER=dummy
 
 cd /opt/openxpki/core/server
 perl Makefile.PL
+make
 make test
-
-/bin/bash
