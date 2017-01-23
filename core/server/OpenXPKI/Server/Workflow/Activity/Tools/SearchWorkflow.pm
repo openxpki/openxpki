0a64c75e41b1ffc2c6a94ae814c13d1776c03dd0
Oliver Welter <github@oliwel.de>
2017-01-23 21:09:45 +0100

    Allow search for realm, support result > 1 in Tools:SearchWorkflow

--- core/server/OpenXPKI/Server/Workflow/Activity/Tools/SearchWorkflow.pm
+++ core/server/OpenXPKI/Server/Workflow/Activity/Tools/SearchWorkflow.pm
@@ -6,9 +6,8 @@ use base qw( OpenXPKI::Server::Workflow::Activity );
 use OpenXPKI::Server::Context qw( CTX );
 use OpenXPKI::Exception;
 use OpenXPKI::Debug;
+use OpenXPKI::Serialization::Simple;
 use Data::Dumper;
-use OpenXPKI::DateTime;
-use DateTime;
 use Workflow::Exception qw(configuration_error);
 
 
@@ -23,12 +22,18 @@ sub execute {
     
     my $query;
     if ($self->param('wf_type')) {
-        $query->{TYPE} = $self->param('wf_type');        
+        $query->{TYPE} = $self->param('wf_type');
     }
+    
     if ($self->param('wf_state')) {
-        $query->{STATE} = $self->param('wf_state');        
+        $query->{STATE} = $self->param('wf_state');
     }
-
+    
+    if ($self->param('realm')) {
+        ##! 16: 'Adding realm ' . $self->param('realm')
+        $query->{PKI_REALM} = $self->param('realm');
+    }
+    
     my @attr;
     if ($self->param('wf_creator')) {
         push @attr, { KEY => 'creator', VALUE => ~~ $self->param('wf_creator') };
@@ -42,21 +47,31 @@ sub execute {
     
     $query->{ATTRIBUTE} = \@attr;
     
-    ##! 16: 'Query ' . Dumper $query
+    ##! 16: 'Query ' . Dumper $query 
 
-    my $result_count = CTX('api')->search_workflow_instances_count( $query );
+    my $result = CTX('api')->search_workflow_instances( $query );
     
-    if ($result_count > 1) {
-        configuration_error('Ambigous configuration - more than one result found');
-    } elsif ($result_count == 0) {
-        $context->param( $target_key => '' );
+    ##! 64: 'Result ' . Dumper $result
+    
+    my @ids = map { ($_->{'WORKFLOW.WORKFLOW_SERIAL'} != $workflow->id()) ? ($_->{'WORKFLOW.WORKFLOW_SERIAL'}) : () } @{$result};
+    
+    ##! 32: 'Ids ' . Dumper \@ids
+    
+    # check if self was the only match so its empty now
+    if (!scalar @ids) {
+        $context->param( $target_key => undef );
+    } elsif ($self->param('mode') eq 'list') {
+        
+        $context->param( $target_key => OpenXPKI::Serialization::Simple->new()->serialize(\@ids) );
+        
     } else {
-        my $result = CTX('api')->search_workflow_instances( $query );
-        ##! 32: 'Result' . Dumper $result
-        my $wf_id = $result->[0]->{'WORKFLOW.WORKFLOW_SERIAL'};
-        $context->param( $target_key => $wf_id );
+    
+        if (scalar @ids > 1) {
+            configuration_error('Ambigous configuration - more than one result found');
+        } else {
+            $context->param( $target_key => $ids[0] );
+        }
     }
-
     return 1;    
     
 }
@@ -72,21 +87,53 @@ OpenXPKI::Server::Workflow::Activity::Tools::SearchWorkflow
 
 =head1 Description
 
-Load the policy section for a server endpoint. 
+Search the workflow table based on given conditions. The default is to 
+search for a single workflow and get its ID back. If you want to search 
+for multiple workflows or need extra information you can pass the I<mode> 
+parameter. The running workflow is always removed from the result.
 
-The path to load defaults to $interface.$server.policy where interface
-and server are read from the context. You can override the full path by 
-setting the key I<config_path>.
+If no result is found, the targer_key is set to undef.
 
-The given path is expected to return a hash, each key/value pair is read 
-into  the context with the I<p_> prefix added to each key! 
+Also check the documentation of the search_workflow_instances API method.
+
+=head2 Search Modes
+
+=over 
+
+=item id
+
+Expect a single (or no) result from the query. If the given query returns 
+more than one item, the search fails with a configuration error.
+
+=item list
+
+Return the ids of the workflows as list.
 
 =head2 Activity Parameter
 
 =over
 
-=item config_path
+=item mode
 
-Explict path to read the policy from.
+One of I<id, list>, see description.
+
+=item realm
+
+The realm to search in, the default is the current realm. You can use the 
+special word I<_any> to search in all realms. Use this with caution!
+
+=item target_key
+
+Context key to write the search result to, default is search_result.
+
+=item wf_type, wf_state, wf_creator
+
+Values are passed as arguments for the respective workflow properties.
+
+=item attr_*
+
+Any parameter starting with the prefix I<attr_> is used as query condition
+to the workflow attributes table, the prefixed is stripped, the remainder 
+is used as attribute key. Values are passed as full text match.
 
 =back
