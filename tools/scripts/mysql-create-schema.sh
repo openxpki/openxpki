901a83b4fd9fd8aeb37a67f96cc68473d9cc77d2
Jens Berthold <jens@jebecs.de>
2017-01-25 01:41:13 +0100

    Fix "mysql" parameter syntax in helper scripts
    This should now work for different versions of the "mysql" client.

--- tools/scripts/mysql-create-schema.sh
+++ tools/scripts/mysql-create-schema.sh
@@ -3,10 +3,10 @@
 echo "Creating MySQL database schema"
 
 DBPASS=""
-test ! -z "$OXI_TEST_DB_MYSQL_DBPASSWORD" && DBPASS="-p $OXI_TEST_DB_MYSQL_DBPASSWORD"
+test ! -z "$OXI_TEST_DB_MYSQL_DBPASSWORD" && DBPASS="-p$OXI_TEST_DB_MYSQL_DBPASSWORD"
 
 mysql \
     -h $OXI_TEST_DB_MYSQL_DBHOST -P $OXI_TEST_DB_MYSQL_DBPORT \
-    -u $OXI_TEST_DB_MYSQL_DBUSER $DBPASS \
+    -u$OXI_TEST_DB_MYSQL_DBUSER $DBPASS \
     $OXI_TEST_DB_MYSQL_NAME \
     < $(readlink -e $(dirname $0)/../../config/sql/schema-mysql.sql)
