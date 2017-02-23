02cf643b564f16199592a29f00445354ec74eeb4
Oliver Welter <github@oliwel.de>
2017-02-23 15:10:03 +0100

    Change PKCS10 Parser to use Crypt::PKCS10
    Extracted SAN for IP have been renamed to "IP" from former "IP Address"
    (was not working with openssl anyway, so it wont break anything)
    Adds new functionality to extract attributes, extensions and key params

--- core/server/OpenXPKI/Server/Workflow/Activity/Tools/ParsePKCS10.pm
+++ core/server/OpenXPKI/Server/Workflow/Activity/Tools/ParsePKCS10.pm
@@ -12,8 +12,7 @@ use OpenXPKI::Exception;
 use OpenXPKI::Debug;
 use English;
 use OpenXPKI::DN;
-use OpenXPKI::Crypto::CSR;
-use OpenXPKI::Crypto::X509;
+use Crypt::PKCS10;
 use OpenXPKI::Serialization::Simple;
 use Data::Dumper;
 use Template;
@@ -26,98 +25,167 @@ sub execute {
     my $serializer = OpenXPKI::Serialization::Simple->new();
 
     my $context   = $workflow->context();
+
+    my $param = {}; # hash to receive the context updates
     my $config = CTX('config');
 
-    my $pkcs10 = $context->param('pkcs10');
-
-    my $default_token = CTX('api')->get_default_token();
+    my $pkcs10 = $self->param('pkcs10');
+    $pkcs10 = $context->param('pkcs10') unless($pkcs10);
 
     # Cleanup any existing values
-    $context->param( 'csr_subject' => '' );
-    $context->param( 'cert_subject_parts' => '' );
-    $context->param( 'cert_san_parts' => '' );
+    $context->param({
+        'csr_subject' => '',
+        'cert_subject_parts' => '',
+        'cert_san_parts' => '',
+        'cert_subject_alt_name' => '',
+
+    });
+
+    # Source hash
+    my $source_ref = {};
+    my $ctx_source = $context->param('sources');
+    if ($ctx_source) {
+        $source_ref = $serializer->deserialize( $ctx_source );
+    }
 
     # extract subject from CSR and add a context entry for it
-    my $csr_obj = OpenXPKI::Crypto::CSR->new(
-        DATA  => $pkcs10,
-        TOKEN => $default_token
-    );
+    Crypt::PKCS10->setAPIversion(1);
+    my $decoded = Crypt::PKCS10->new( $pkcs10, ignoreNonBase64 => 1, verifySignature => 0);
 
-    my $csr_body = $csr_obj->get_parsed_ref()->{BODY};
-    ##! 32: 'csr_parsed: ' . Dumper $csr_body
+    my %hashed_dn;
+    my $csr_subject = $decoded->subject();
 
-    my $csr_subject = $csr_body->{'SUBJECT'};
-    # Explicit check for empty subject - should never happen but if it crashes the logic
-    if (!$csr_subject) {
-        CTX('log')->log(
-            MESSAGE => "csr has no subject",
-            PRIORITY => 'warn',
-            FACILITY => 'application',
-        );
-        return 1;
+    if ($csr_subject) {
+        # TODO - extend Crypt::PKCS10 to return RFC compliant subject
+        my $dn = OpenXPKI::DN->new( $csr_subject );
+
+        %hashed_dn = $dn->get_hashed_content();
+        $param->{csr_subject} = $dn->get_rfc_2253_dn();
+        ##! 32: 'Subject DN ' . Dumper \%hashed_dn
     }
 
-    my $csr_key_type = $csr_body->{PUBKEY_ALGORITHM};
+    my $key_param = $decoded->subjectPublicKeyParams();
 
-    if ($csr_key_type eq 'rsaEncryption') {
-        $context->param('csr_key_alg' => 'rsa' );
-        $context->param('csr_key_params' => { key_length =>  $csr_body->{KEYSIZE} });
+    if ($self->param('key_params')) {
+        if ($key_param->{keytype} eq 'RSA') {
+            $param->{csr_key_alg} = 'rsa';
+            $param->{csr_key_params} = { key_length =>  $key_param->{keylen} };
+        } elsif ($key_param->{keytype} eq 'DSA') {
+            $param->{csr_key_alg} = 'dsa';
+            $param->{csr_key_params} = { key_length =>  $key_param->{keylen} };
+        } elsif ($key_param->{keytype} eq 'ECC') {
+            $param->{csr_key_alg} = 'ec';
+            $param->{csr_key_params} = { key_length =>  $key_param->{keylen}, curve => $key_param->{curve} };
+        } else {
+            $param->{csr_key_alg} = 'unsupported';
+        }
     } else {
-        # tbd ecc, dsa
-        $context->param('csr_key_alg' => $csr_key_type );
+        my $key_alg = $decoded->pkAlgorithm || '';
+        if( $key_alg eq 'rsaEncryption' ) {
+            $param->{csr_key_alg} = 'rsa';
+        } elsif( $key_alg eq 'ecPublicKey' ) {
+            $param->{csr_key_alg} = 'ec';
+        } elsif( $key_alg eq 'dsa' ) {
+            $param->{csr_key_alg} = 'dsa';
+        } else {
+            $param->{csr_key_alg} = 'unsupported';
+        }
     }
 
-    $context->param('csr_subject' => $csr_subject);
-
-    # Get the profile name and style
+    # Get the profile name and style - required for templating
     my $cert_profile = $self->param('cert_profile');
     $cert_profile = $context->param('cert_profile') unless($cert_profile);
 
     my $cert_subject_style = $self->param('cert_subject_style');
     $cert_subject_style = $context->param('cert_subject_style') unless($cert_subject_style);
 
-    my %hashed_dn = OpenXPKI::DN->new( $csr_subject )->get_hashed_content();
-    ##! 32: 'DN ' . Dumper \%hashed_dn
+    # Map SAN keys from ASN1 names to openssl format (all uppercased)
+    # TODO this should go to a central location
+    my $san_map = {
+        otherName => 'otherName',
+        rfc822Name => 'email',
+        dNSName => 'DNS',
+        x400Address => '', # not supported by openssl
+        directoryName => 'dirName',
+        ediPartyName => '', # not supported by openssl
+        uniformResourceIdentifier => 'URI',
+        iPAddress  => 'IP',
+        registeredID => 'RID',
+    };
 
-    my $csr_san = $csr_obj->get_subject_alt_names({ FORMAT => 'HASH' });
-    ##! 16: 'Found san in csr :' . Dumper $csr_san
-
+    my $csr_san = {};
     my @san_list;
-    # Merge SANs into DN hash
-    if ($csr_san) {
-        foreach my $san_type (keys %{$csr_san}) {
-            # to make it easier for the user, we uppercase all names
-            # as some SAN items can be part of the DN, we add a SAN_ prefix
-            if (defined $csr_san->{$san_type} && 
-                ref $csr_san->{$san_type} eq 'ARRAY' && 
-                scalar @{$csr_san->{$san_type}}) {
 
-                $hashed_dn{'SAN_'.uc($san_type)} = $csr_san->{$san_type};
+    # Retrieve the registered SAN property names
 
-                # push items to @san_list in the nested array format 
-                # required by the csr persister
-                foreach my $value (@{$csr_san->{$san_type}}) {
-                    push @san_list, [ $san_type, $value ] if ($value);
-                }
-            }
-        } 
+    my @san_names = $decoded->subjectAltName();
+    # Walk all san keys
+    foreach my $san (@san_names) {
+        my $san_type = $san_map->{$san};
+
+        if (!$san_type) {
+            # type is not supported
+            next;
+        }
+
+        my @items = $decoded->subjectAltName( $san );
+
+        # san hash
+        $csr_san->{ $san_type } = \@items;
+
+        # merge into dn, uppercase key name
+        $hashed_dn{'SAN_'.uc($san_type)} = \@items;
+
+        # push items to @san_list in the nested array format as required by
+
+        # the csr persister
+        foreach my $value (@items) {
+            push @san_list, [ $san_type, $value ] if ($value);
+        }
+
     }
 
-    # Update the source hash
-    my $source_ref = {};
-    my $ctx_source = $context->param('sources');
-    if ($ctx_source) {
-        $source_ref = $serializer->deserialize( $ctx_source );
-    } 
+    ##! 32: 'Extracted SAN ' . Dumper $csr_san
+
+    ##! 32: 'Merged DN ' . Dumper \%hashed_dn
+
+    # Attributes, must be a list of OIDs, seperated by comma/blank
+    my $attr = $self->param('req_attributes');
+    my $req_attr = {};
+    if ($attr) {
+        my @attr = split /[\s,]+/, $attr;
+        foreach my $oid (@attr) {
+            my $val = $decoded->attributes($oid);
+            if ($val) {
+                $req_attr->{$oid} =$val;
+            }
+         }
+         $param->{req_attributes} = $req_attr;
+         $source_ref->{req_attributes} = 'PKCS10';
+    }
+
+    # Extensions, must be a list of OIDs, seperated by comma/blank
+    my $ext = $self->param('req_extensions');
+    my $req_ext = {};
+    if ($ext) {
+        my @ext = split /[\s,]+/, $ext;
+        foreach my $oid (@ext) {
+            if ($decoded->extensionPresent($oid)) {
+                $req_ext->{$oid} = $decoded->extensionValue($oid);
+            }
+         }
+         $param->{req_extensions} = $req_ext;
+         $source_ref->{req_extensions} = 'PKCS10';
+    }
 
     # If the profile has NO ui section, we write the parsed hash and the SANs "as is" to the context
-    if (!$config->exists(['profile', $cert_profile, 'style', $cert_subject_style, 'ui' ])) {
+    if (!$cert_profile or !$cert_subject_style or !$config->exists(['profile', $cert_profile, 'style', $cert_subject_style, 'ui' ])) {
 
-        $context->param('cert_subject_parts' => $serializer->serialize( \%hashed_dn ) );
+        $param->{'cert_subject_parts'} = $serializer->serialize( \%hashed_dn ) ;
         $source_ref->{'cert_subject_parts'} = 'PKCS10';
 
         if (scalar @san_list) {
-            $context->param('cert_subject_alt_name' => $serializer->serialize( \@san_list ) );
+            $param->{'cert_subject_alt_name'} = $serializer->serialize( \@san_list );
             $source_ref->{'cert_subject_alt_name'} = 'PKCS10';
         }
 
@@ -144,7 +212,8 @@ sub execute {
                     push @val, $v if (defined $v && $v ne '');
                 }
 
-            # Fast path, copy from DN     
+            # Fast path, copy from DN
+
             } elsif ($preset =~ m{ \A \s* (\w+)(\.(\d+))? \s* \z }xs) {
                 my $comp = $1;
                 my $pos = $3 || 0;
@@ -158,7 +227,8 @@ sub execute {
                 my $val;
                 $tt->process(\$preset, \%hashed_dn, \$val) || OpenXPKI::Exception->throw(
                     message => 'I18N_OPENXPKI_ACTIVITY_PARSE_PKCS10_TT_DIED',
-                    params => { PROFILE => $cert_profile, STYLE => $cert_subject_style, 
+                    params => { PROFILE => $cert_profile, STYLE => $cert_subject_style,
+
                         FIELD => $field, PATTERN => $preset, 'ERROR' => $tt->error() }
                 );
 
@@ -193,12 +263,13 @@ sub execute {
             }
 
         }
-        $context->param('cert_subject_parts' => $serializer->serialize( $cert_subject_parts ) );
+        $param->{'cert_subject_parts'} = $serializer->serialize( $cert_subject_parts );
         $source_ref->{'cert_subject_parts'} = 'Parser';
 
         # Load the field spec for the san
         # FIXME: this implies that the id of the field matches the san types name
-        # Evaluate: Replace with data from hashed_dn and preset? 
+        # Evaluate: Replace with data from hashed_dn and preset?
+
         if ($csr_san) {
             my $san_names = CTX('api')->list_supported_san();
             $fields = CTX('api')->get_field_definition( { PROFILE => $cert_profile, STYLE => $cert_subject_style, SECTION => 'san' });
@@ -226,12 +297,14 @@ sub execute {
             }
             ##! 16: 'san preset:' . Dumper $cert_san_parts
             if ($cert_san_parts) {
-                $context->param( 'cert_san_parts' => $serializer->serialize( $cert_san_parts ) );
+                $param->{'cert_san_parts'} = $serializer->serialize( $cert_san_parts );
                 $source_ref->{'cert_san_parts'} = 'Parser';
             }
         }
     }
 
+    ##! 64: 'Params to set ' . Dumper $param
+    $context->param( $param );
     $context->param('sources' => $serializer->serialize( $source_ref) );
 
     return 1;
@@ -246,15 +319,24 @@ OpenXPKI::Server::Workflow::Activity::Tools::ParsePKCS10
 
 =head1 Description
 
-Take a pkcs10 container from the context and extract information to the
-context. The context values are prepared to prefill the subject/san form
-based on the given profile and style.
+Take a pkcs10 container and extract information to the context. If a 
+profile name and style are given and the profile has a ui section, the 
+data extracted from the CSR is used to prefill the profile ui fields.
+Otherwise the extracted subject and san information is put "as is" into
+the context. Output definition is given below.
+
+To get extra information from the CSR, add parameters key_params, 
+req_attributes and req_extensions to your activity configuration.
 
 =head1 Configuration
 
 =head2 Activity Parameters
 
 =over
+
+=item pkcs10
+
+The PEM formatted pkcs10 request, has priority over context key.
 
 =item cert_profile
 
@@ -264,7 +346,14 @@ Determines the used profile, has priority over context key.
 
 Determines the used profile substyle, has priority over context key.
 
-=back 
+=item key_params
+
+If set to a true value, details of the used public key are available
+in the I<key_params> context entry. Note that this requires additional
+
+modules to be installed (Crypt::OpenSSL::RSA/DSA, Crypt::PK::ECC).
+
+=back
 
 =head2 Expected context values
 
@@ -272,9 +361,27 @@ Determines the used profile substyle, has priority over context key.
 
 =item pkcs10
 
+Read pkcs10 request from if not set using activity param.
+
 =item cert_profile
 
+Read cert_profile request from if not set using activity param.
+
 =item cert_subject_style
+
+Read cert_subject_style request from if not set using activity param.
+
+=item req_extensions
+
+List of OIDs (or names) of request extensions, multiple items must be
+seperated by space. For each extensions that is found in the request,
+a item in the req_extension context item is created. The key is the given
+name, the content is the raw data as returned by Crypt::PKCS10 and depends
+on the extensions.
+
+=item req_attributes
+
+List of OIDs (or names) of request attributes, similar to req_extension.
 
 =back
 
@@ -284,15 +391,57 @@ Determines the used profile substyle, has priority over context key.
 
 =item csr_subject
 
-The extracted subject as string
+The extracted subject as string (comma seperated)
 
 =item cert_subject_parts
 
-Hash to prefill the subject template based.
+If a valid profile is given, contains the preset values for all fields given
+in the profiles subject section. The values are determined by running the 
+appropriate template string for each field with the data extracted from the
+csr. 
+
+In plain mode, it contains the parsed DN as key-value pairs where the key 
+is the shortname of the component (e.g: OU) and the value is an array of
+values found. Note that any component is an array even if it has only one
+item. All items found in the SAN part are also added with a prefix "SAN_" 
+and all uppercased names as used by openssl (SAN_OTHERNAME, SAN_EMAIL, 
+SAN_DNS, SAN_DIRNAME, SAN_URI, SAN_IP, SAN_RID)
+
 
 =item cert_san_parts
 
-Hash to prefill the san template. Extensions without a machting rule in the
-form definition are deleted.
+Only in profile mode. Contains the preset values for all fields
+given in the profiles san section. The values are determined by running the 
+appropriate template string for each field with the data extracted from the
+csr.  
+
+=item cert_subject_alt_name
+
+Only in plain mode. All SAN items as nested array list. Each item of the 
+list is a two item array with name and value of one SAN item. The names
+are given as required to build then openssl extension file (otherName,
+email, DNS, dirName, URI, IP, RID).
+
+=item csr_key_alg
+
+Algorithm of the public key, one of rsa, dsa, ec, unsupported
+
+=item csr_key_params
+
+Hash holding additional information on the used public key, only present
+if key_params is set. Keys depend on the type of the key.
+
+=over
+
+=item key_length
+
+Size of the used public key (RSA/DSA)/curve (ECC) in bits
+
+=item curve
+
+ec keys only, name of the curve - can be empty if curve is not known to
+the current openssl version or if custom parameters have been used.
+
+=back
 
 =back
---
+++ qatest/backend/activity/11_pkcs10_parser.pl
@@ -0,0 +1,156 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use lib qw(
+  ../../lib
+);
+
+use Carp;
+use English;
+use Data::Dumper;
+use Config::Std;
+use File::Basename;
+use OpenXPKI::Serialization::Simple;
+use Log::Log4perl qw(:easy);
+Log::Log4perl->easy_init($WARN);
+
+use OpenXPKI::Test::More;
+use TestCfg;
+
+my $dirname = dirname($0);
+
+our @cfgpath = ( $dirname );
+our %cfg = ();
+
+my $test = OpenXPKI::Test::More->new({
+    socketfile => '/var/openxpki/openxpki.socket',
+    realm => '',
+}) or die "Error creating new test instance: $@";
+
+$test->set_verbose(0);
+
+$test->plan( tests => 42 );
+
+# Login to use socket
+$test->connect_ok(
+    user => 'raop',
+    password => 'openxpki',
+) or die "Error - connect failed: $@";
+
+my $pkcs10 = `openssl req -new -nodes -keyout /dev/null -config openssl.conf -reqexts req_san 2>/dev/null`;
+
+# Test without profile
+
+my %wfparam = (
+    cert_profile => 'acme',
+    cert_subject_style => 'none',
+    pkcs10 => $pkcs10,
+);
+
+$test->create_ok( 'test_pkcs10' , \%wfparam, 'Create Parser Workflow')
+ or die "Workflow Create failed: $@";
+
+$test->state_is('SUCCESS');
+
+my $context = $test->get_msg()->{PARAMS}->{WORKFLOW}->{CONTEXT};
+$test->is(ref $context, 'HASH');
+
+$test->is($context->{'csr_key_alg'}, 'rsa');
+$test->is($context->{'csr_key_params'}->{'key_length'}, '2048');
+
+$test->is($context->{csr_subject}, 'DC=com,DC=Company,OU=IT,OU=Test,CN=test.me');
+
+my $ser = OpenXPKI::Serialization::Simple->new();
+
+my $subject = $ser->deserialize( $context->{cert_subject_parts} );
+
+$test->is($subject->{SAN_URI}->[0], 'http://test.me/');
+$test->is($subject->{SAN_IP}->[0], '127.0.0.1');
+$test->is($subject->{SAN_DNS}->[0], 'test.me');
+$test->is($subject->{SAN_DNS}->[1], 'also.test.me');
+$test->is($subject->{SAN_EMAIL}->[0], 'me@test.me');
+$test->is($subject->{CN}->[0], 'test.me');
+$test->is($subject->{OU}->[0], 'IT');
+$test->is($subject->{OU}->[1], 'Test');
+$test->is($subject->{DC}->[0], 'com');
+$test->is($subject->{DC}->[1], 'Company');
+
+my $san = $ser->deserialize( $context->{cert_subject_alt_name} );
+
+$test->is(scalar @{$san}, 5);
+
+# order of keys in SAN hash is not defined, so we use map to check the array
+$test->ok(map { ($_->[0] eq 'IP' &&  $_->[1] eq '127.0.0.1') ? 1 : ();  } @{$san});
+$test->ok(map { ($_->[0] eq 'email' &&  $_->[1] eq 'me@test.me') ? 1 : ();  } @{$san});
+$test->ok(map { ($_->[0] eq 'DNS' &&  $_->[1] eq 'also.test.me') ? 1 : ();  } @{$san});
+
+$test->ok($context->{req_attributes}->{challengePassword}, 'SecretChallenge');
+
+# Test with profile
+%wfparam = (
+    cert_profile => 'I18N_OPENXPKI_PROFILE_TLS_SERVER',
+    cert_subject_style => '00_basic_style',
+    pkcs10 => $pkcs10,
+);
+
+$test->create_ok( 'test_pkcs10' , \%wfparam, 'Create Parser Workflow')
+ or die "Workflow Create failed: $@";
+
+$test->state_is('SUCCESS');
+
+$context = $test->get_msg()->{PARAMS}->{WORKFLOW}->{CONTEXT};
+$test->is(ref $context, 'HASH');
+
+$subject = $ser->deserialize( $context->{cert_subject_parts} );
+
+$test->is($subject->{hostname}, 'test.me');
+$test->is($subject->{hostname2}->[0], 'test.me');
+$test->is($subject->{hostname2}->[1], 'also.test.me');
+$test->is(scalar @{$subject->{hostname2}}, 2);
+
+$san = $ser->deserialize( $context->{cert_san_parts} );
+$test->is($san->{dns}->[1], 'also.test.me');
+$test->is($san->{ip}->[0], '127.0.0.1');
+$test->is($san->{email}, undef);
+
+$pkcs10 = `openssl req -new -nodes -keyout /dev/null -config openssl.conf -reqexts req_template_v1  2>/dev/null`;
+
+%wfparam = (
+    cert_profile => 'I18N_OPENXPKI_PROFILE_TLS_SERVER',
+    cert_subject_style => '00_basic_style',
+    pkcs10 => $pkcs10,
+);
+
+$test->create_ok( 'test_pkcs10' , \%wfparam, 'Create Parser Workflow')
+ or die "Workflow Create failed: $@";
+
+$test->state_is('SUCCESS');
+
+$context = $test->get_msg()->{PARAMS}->{WORKFLOW}->{CONTEXT};
+$test->is(ref $context, 'HASH');
+
+$test->is($context->{req_extensions}->{certificateTemplateName}, 'Machine');
+$test->is($context->{req_extensions}->{certificateTemplate}, undef);
+
+
+$pkcs10 = `openssl req -new -nodes -keyout /dev/null -config openssl.conf -reqexts req_template_v2  2>/dev/null`;
+
+%wfparam = (
+    cert_profile => 'I18N_OPENXPKI_PROFILE_TLS_SERVER',
+    cert_subject_style => '00_basic_style',
+    pkcs10 => $pkcs10,
+);
+
+$test->create_ok( 'test_pkcs10' , \%wfparam, 'Create Parser Workflow')
+ or die "Workflow Create failed: $@";
+
+$test->state_is('SUCCESS');
+
+$context = $test->get_msg()->{PARAMS}->{WORKFLOW}->{CONTEXT};
+$test->is(ref $context, 'HASH');
+
+$test->is($context->{req_extensions}->{certificateTemplateName}, undef);
+$test->is($context->{req_extensions}->{certificateTemplate}->{templateID}, '1.3.6.1.4.1.311.21.8.15138236.9849362.7818410.4518060.12563386.22.5003942.7882920');
+
