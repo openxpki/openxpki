9ea0362cbc5ceefbaf079b5d257e3330a7132a4a
Oliver Welter <github@oliwel.de>
2017-04-04 09:54:44 +0200

    Allow custom configuration for certificate actions on UI

--- core/server/OpenXPKI/Server/API/Object.pm
+++ core/server/OpenXPKI/Server/API/Object.pm
@@ -416,17 +416,46 @@ sub get_profile_for_cert {
 Requires a certificate identifier (IDENTIFIER) and optional ROLE.
 Returns a list of actions that the given role (defaults to current
 session role) can do with the given certificate. The return value is a
-nested hash with options available for lifecyle actions. The list of
-workflows is hardcoded for now, workflows which are not present or not
-accessible by the current user are remove from the result.
+nested hash with options available for lifecyle actions. The list of 
+workflows is read from the roles uicontrol. The key I<certaction>
+must contain a list where each item is a hash giving label and workflow
+and optional a set of conditions to be met.
 
-    {
-        workflow => [
-            { label => I18N_OPENXPKI_UI_CERT_ACTION_REVOKE, workflow => certificate_revocation_request_v2 },
-            { label => I18N_OPENXPKI_UI_CERT_ACTION_RENEW, workflow => certificate_renewal_request_v2 },
-            { label => I18N_OPENXPKI_UI_CERT_ACTION_RENEW, workflow => certificate_renewal_request_v2 }
-        ]
-    }
+Example:
+
+  certaction:
+   - label: I18N_OPENXPKI_UI_DOWNLOAD_PRIVATE_KEY
+     workflow: certificate_privkey_export
+     condition: keyexport 
+    
+   - label: I18N_OPENXPKI_UI_CERT_ACTION_RENEW
+     workflow: certificate_renewal_request
+     condition: issued
+
+Valid conditions are:
+
+=over 
+
+=item keyexport 
+
+A private key must exist in the datapool
+
+=item issued
+
+The certificate is not revoked
+
+=item valid
+
+The certificate is not revoked and within the validity interval
+
+=item owner
+
+current user is the certificate owner (see is_certificate_owner)
+
+=back 
+
+In addition to the conditional checks, the given workflow must be 
+accessible by the given role.
 
 =cut
 
@@ -442,24 +471,74 @@ sub get_cert_actions {
     # check if this is a entity certificate from the current realm
     return {} unless $cert->{CSR_SERIAL} and $cert->{PKI_REALM} eq CTX('session')->get_pki_realm();
 
+    my @actions;
+    my @options;
+
     my $conn = CTX('config');
 
-    my @actions;
-    push @actions, { label => 'I18N_OPENXPKI_UI_DOWNLOAD_PRIVATE_KEY', workflow => 'certificate_privkey_export' }
-        if CTX('api')->private_key_exists_for_cert({ IDENTIFIER => $cert_id })
-        and $conn->exists([ 'workflow', 'def', 'certificate_privkey_export', 'acl', $role, 'creator' ] );
+    # check if certaction list is defined for this role
+    if ($conn->exists( ['uicontrol', $role, 'certaction' ])) {
+        @options = $conn->get_list(['uicontrol', $role, 'certaction']);
+        ##! 32: 'Got action list for role ' . Dumper \@options     
+    # default uicontrol
+    } elsif ($conn->exists( ['uicontrol', '_default', 'certaction'] )) {
+        @options = $conn->get_list(['uicontrol', '_default', 'certaction']) unless(@options);
+        ##! 32: 'Got action list for ui default ' . Dumper \@options
+    # Legacy - fallback to the default set        
+    } else {
 
-    push @actions, { label => 'I18N_OPENXPKI_UI_CERT_ACTION_RENEW', workflow => 'certificate_renewal_request' }
-        if ($cert->{STATUS} eq 'ISSUED' or $cert->{STATUS} eq 'EXPIRED')
-        and $conn->exists([ 'workflow', 'def', 'certificate_renewal_request', 'acl', $role, 'creator' ] );
-
-    push @actions, { label => 'I18N_OPENXPKI_UI_CERT_ACTION_REVOKE', workflow => 'certificate_revocation_request_v2' }
-        if $cert->{STATUS} eq 'ISSUED'
-        and $conn->exists([ 'workflow', 'def', 'certificate_revocation_request_v2', 'acl', $role, 'creator' ]);
-
-    push @actions, { label => 'I18N_OPENXPKI_UI_CERT_ACTION_UPDATE_METADATA', workflow => 'change_metadata' }
-        if $conn->exists([ 'workflow', 'def', 'change_metadata', 'acl', $role, 'creator' ] );
-
+        ##! 32: 'No action list, fall back to default'
+        @options = ({ 
+            label => 'I18N_OPENXPKI_UI_DOWNLOAD_PRIVATE_KEY', 
+            workflow => 'certificate_privkey_export', 
+            condition => 'keyexport'
+        }, {
+            label => 'I18N_OPENXPKI_UI_CERT_ACTION_RENEW', 
+            workflow => 'certificate_renewal_request',
+            condition => 'issued' 
+        }, { 
+            label => 'I18N_OPENXPKI_UI_CERT_ACTION_REVOKE', 
+            workflow => 'certificate_revocation_request_v2',
+            condition => 'issued' 
+        }, { 
+            label => 'I18N_OPENXPKI_UI_CERT_ACTION_UPDATE_METADATA', 
+            workflow => 'change_metadata' 
+        });
+    }
+    
+        
+    OPTION:
+    foreach my $item (@options) {
+        ##! 32: 'Checking Item ' . Dumper $item
+        if ($item->{condition}) {
+            my @cond = split /[\W]/, $item->{condition};
+            ##! 32: 'Conditions ' . join " + ", @cond
+            foreach my $rule (@cond) {
+                
+                if ($rule eq 'keyexport') {
+                    next OPTION if CTX('api')->private_key_exists_for_cert({ IDENTIFIER => $cert_id });
+                                            
+                } elsif ($rule eq 'issued') {
+                    next OPTION  unless ($cert->{STATUS} eq 'ISSUED' or $cert->{STATUS} eq 'EXPIRED');
+                
+                } elsif ($rule eq 'valid') {
+                    next OPTION  unless ($cert->{STATUS} eq 'ISSUED');
+                
+                } elsif ($rule eq 'owner') {
+                    next OPTION  unless ($self->is_certificate_owner({ IDENTIFIER => $cert_id }));
+                }
+                
+            }
+        }
+        
+        # all conditions are met, check workflow permissions
+        if ($conn->exists([ 'workflow', 'def', $item->{workflow}, 'acl', $role, 'creator' ] )) {
+            ##! 32: 'Adding Item ' . $item->{label}
+            push @actions, { label => $item->{label}, workflow => $item->{workflow} };
+        }
+        
+    }
+    
     return { workflow => \@actions };
 }
 
