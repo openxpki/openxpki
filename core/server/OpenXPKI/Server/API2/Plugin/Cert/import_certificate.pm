625b21b737dac344acc95af3aadb5ba01e057312
Oliver Welter <github@oliwel.de>
2018-08-08 13:09:02 +0200

    Improve import_certificate API method
    Use new x509 parser instead openssl cli
    Allow import of metadata and revocation details

--- core/server/OpenXPKI/Server/API2/Plugin/Cert/import_certificate.pm
+++ core/server/OpenXPKI/Server/API2/Plugin/Cert/import_certificate.pm
@@ -11,11 +11,12 @@ OpenXPKI::Server::API2::Plugin::Cert::import_certificate
 use Data::Dumper;
 
 # Project modules
+use OpenXPKI::Debug;
 use OpenXPKI::Server::Context qw( CTX );
 use OpenXPKI::Server::API2::Types;
 use OpenXPKI::MooseParams;
 
-
+use OpenXPKI::Server::Database; # to get AUTO_ID
 
 =head1 COMMANDS
 
@@ -54,8 +55,9 @@ command "import_certificate" => {
     force_nochain  => { isa => 'Bool', default => 0, },
     force_issuer   => { isa => 'Bool', default => 0, },
     force_noverify => { isa => 'Bool', default => 0, },
-    revoked        => { isa => 'Bool', default => 0, },
+    revoked        => { isa => 'Bool|HashRef', default => 0, },
     update         => { isa => 'Bool', default => 0, },
+    attributes     => { isa => 'HashRef'},
 } => sub {
     my ($self, $params) = @_;
 
@@ -67,13 +69,10 @@ command "import_certificate" => {
     }
 
     my $dbi = CTX('dbi');
-    my $default_token = $self->api->get_default_token();
 
-    my $cert = OpenXPKI::Crypto::X509->new(
-        TOKEN => $default_token,
-        DATA  => $params->data,
-    );
-    my $cert_identifier = $cert->get_identifier();
+    my $x509 = OpenXPKI::Crypt::X509->new( $params->data );
+
+    my $cert_identifier = $x509->get_cert_identifier();
 
     # Check if the certificate is already in the PKI
     my $existing_cert = $dbi->select_one(
@@ -91,21 +90,75 @@ command "import_certificate" => {
         },
     ) if ($existing_cert and not $params->update);
 
-    # Prepare hash to be inserted into DB
-    my $cert_legacy = { $cert->to_db_hash() };
-    $cert_legacy->{STATUS} = ($params->revoked ? 'REVOKED' : 'ISSUED');
-    $cert_legacy->{PKI_REALM} = $params->pki_realm if $params->has_pki_realm;
+
+    my $cert_hash = {
+        status => 'ISSUED',
+        identifier => $cert_identifier,
+        issuer_dn => $x509->get_issuer,
+        cert_key => $x509->get_serial,
+        subject => $x509->get_subject,
+        subject_key_identifier =>  $x509->get_subject_key_id,
+        authority_key_identifier => $x509->get_authority_key_id,
+        notbefore => $x509->get_notbefore()->epoch(),
+        notafter => $x509->get_notafter()->epoch(),
+    };
+
+    $cert_hash->{pki_realm} = $params->pki_realm if $params->has_pki_realm;
+
+    # attach revocation information if provided
+    if ($params->has_revoked) {
+        if (ref $params->revoked eq 'HASH' && scalar keys %{$params->revoked}) {
+
+            $cert_hash->{status} = 'REVOKED';
+
+            my $r = $params->revoked;
+            if (!$r->{revocation_time}) {
+                $cert_hash->{revocation_time} = time();
+            } elsif ($r->{revocation_time} =~ m{\A\d+\z}) {
+                $cert_hash->{revocation_time} = $r->{revocation_time};
+            } else {
+                OpenXPKI::Exception->throw(
+                    message => 'Provided revocation_time has invalid format (epoch expected)',
+                );
+            }
+
+            if (!$r->{invalidity_time}) {
+                # leave null
+            } elsif ($r->{invalidity_time} =~ m{\A\d+\z}) {
+                $cert_hash->{invalidity_time} = $r->{invalidity_time};
+            } else {
+                OpenXPKI::Exception->throw(
+                    message => 'Provided invalidity_time has invalid format (epoch expected)',
+                );
+            }
+
+            my $reason_code = $r->{reason_code} || 'unspecified';
+            if ($reason_code !~ m{ \A (?: unspecified | keyCompromise | CACompromise | affiliationChanged | superseded | cessationOfOperation | certificateHold | removeFromCRL ) \z }xms) {
+                OpenXPKI::Exception->throw(
+                    message => 'Provided reason_code is not valid',
+                );
+            }
+            $cert_hash->{reason_code} = $reason_code;
+
+            #  hold_instruction_code not supported yet
+        } elsif (ref $params->revoked eq '' && $params->revoked) {
+            $cert_hash->{status} = 'REVOKED';
+            $cert_hash->{revocation_time} = time();
+            $cert_hash->{reason_code} = 'unspecified';
+        }
+    }
+
 
     # Query issuer certificate
     my $issuer_cert = $self->_get_issuer(
-        cert            => $cert,
+        cert            => $x509,
         explicit_issuer => $params->issuer,
         force_nochain   => $params->force_nochain,
     );
 
     # cert is self signed
     if ($issuer_cert and $issuer_cert eq "SELF") {
-        $cert_legacy->{ISSUER_IDENTIFIER} = $cert_identifier;
+        $cert_hash->{issuer_identifier} = $cert_identifier;
     }
     # cert has known issuer
     elsif ($issuer_cert) {
@@ -114,17 +167,16 @@ command "import_certificate" => {
         # No verfication requested ?
         #
         if ($params->force_noverify) {
-            CTX('log')->system()->warn("Importing certificate without chain verification! $cert_identifier / " . $cert->get_subject);
+            CTX('log')->system()->warn("Importing certificate without chain verification! $cert_identifier / " . $x509->get_subject);
             CTX('log')->audit('system')->warn('certificate import without chain validation', {
                 certid    => $cert_identifier,
-                key       => $cert->get_subject_key_id(),
+                key       => $x509->get_subject_key_id(),
             });
             $valid = 1;
         }
         else {
             $valid = $self->_is_issuer_valid(
-                default_token  => $default_token,
-                cert           => $cert,
+                cert           => $x509,
                 issuer_cert    => $issuer_cert,
                 force_nochain  => $params->force_nochain,
             );
@@ -133,10 +185,10 @@ command "import_certificate" => {
         if (!$valid) {
             # force the invalid issuer
             if ($params->force_issuer) {
-                CTX('log')->system->warn("Importing certificate with invalid chain with force! $cert_identifier / " . $cert->get_subject());
+                CTX('log')->system->warn("Importing certificate with invalid chain with force! $cert_identifier / " . $x509->get_subject());
                 CTX('log')->audit('system')->warn('certificate import without chain validation', {
                     certid    => $cert_identifier,
-                    key       => $cert->get_subject_key_id(),
+                    key       => $x509->get_subject_key_id(),
                 });
             } else {
                 OpenXPKI::Exception->throw(
@@ -146,13 +198,11 @@ command "import_certificate" => {
             }
         }
 
-        $cert_legacy->{ISSUER_IDENTIFIER} = $issuer_cert->{identifier};
+        $cert_hash->{issuer_identifier} = $issuer_cert->{identifier};
         # if the issuer is in a realm, it forces the entity into the same one
-        $cert_legacy->{PKI_REALM} = $issuer_cert->{pki_realm} if $issuer_cert->{pki_realm};
+        $cert_hash->{pki_realm} = $issuer_cert->{pki_realm} if $issuer_cert->{pki_realm};
     }
 
-    # TODO #legacydb Mapping for compatibility to old DB layer
-    my $cert_hash = OpenXPKI::Server::Database::Legacy->certificate_from_legacy($cert_legacy);
 
     $dbi->merge(
         into => 'certificate',
@@ -163,6 +213,36 @@ command "import_certificate" => {
     # unset data to save bytes and return the remainder of the hash
     delete $cert_hash->{data};
 
+    # append attributes if any
+    my $attr = $params->has_attributes ? $params->attributes : {};
+
+    if ($attr) {
+        ##! 32: 'Attributes ' . Dumper $attr
+        foreach my $key (keys %{$attr}) {
+            my $val = $attr->{$key};
+            if (ref $val eq '') { $val = [ $val ] };
+
+            $dbi->delete(from => 'certificate_attributes',
+            where => {
+                identifier => $cert_identifier,
+                attribute_contentkey => 'meta_'.$key
+            });
+
+            foreach my $v (@{$val}) {
+                ##! 64: "Adding meta $key : $v"
+                $dbi->insert(
+                    into => 'certificate_attributes',
+                    values => {
+                        attribute_key        => AUTO_ID,
+                        identifier           => $cert_identifier,
+                        attribute_contentkey => 'meta_'.$key,
+                        attribute_value      => $v,
+                    }
+                );
+            }
+        }
+    }
+
     return $cert_hash;
 };
 
@@ -170,12 +250,12 @@ command "import_certificate" => {
 # C<undef> if no issuer was found (and force_nochain = 1).
 sub _get_issuer {
     my ($self, %args) = named_args(\@_,   # OpenXPKI::MooseParams
-        cert            => { isa => 'OpenXPKI::Crypto::X509' },
+        cert            => { isa => 'OpenXPKI::Crypt::X509' },
         explicit_issuer => { isa => 'Maybe[Str]' },
         force_nochain   => { isa => 'Maybe[Bool]' },
     );
     my $cert            = $args{cert};
-    my $cert_identifier = $cert->get_identifier;
+    my $cert_identifier = $cert->get_cert_identifier;
     my $explicit_issuer = $args{explicit_issuer};
     my $force_nochain   = $args{force_nochain};
 
@@ -194,9 +274,9 @@ sub _get_issuer {
 
     # certificates without AIK/SK set
     } else {
-        $condition = { subject => $cert->{PARSED}->{BODY}->{ISSUER} };
+        $condition = { subject => $cert->get_issuer };
         # self signed
-        return "SELF" if $cert->{PARSED}->{BODY}->{SUBJECT} eq $cert->{PARSED}->{BODY}->{ISSUER};
+        return "SELF" if $cert->get_issuer eq $cert->get_subject;
     }
 
     #
@@ -240,14 +320,13 @@ sub _get_issuer {
 
 sub _is_issuer_valid  {
     my ($self, %args) = named_args(\@_,   # OpenXPKI::MooseParams
-        default_token  => { isa => 'Object' },
-        cert           => { isa => 'OpenXPKI::Crypto::X509' },
+        cert           => { isa => 'OpenXPKI::Crypt::X509' },
         issuer_cert    => { isa => 'HashRef' },
         force_nochain  => { isa => 'Maybe[Bool]' },
     );
-    my $default_token   = $args{default_token};
+    my $default_token   = CTX('api')->get_default_token();
     my $cert            = $args{cert};
-    my $cert_identifier = $cert->get_identifier;
+    my $cert_identifier = $cert->get_cert_identifier;
     my $issuer_cert     = $args{issuer_cert};
     my $force_nochain   = $args{force_nochain};
 
@@ -257,7 +336,7 @@ sub _is_issuer_valid  {
     if ($issuer_cert->{identifier} eq $issuer_cert->{issuer_identifier}) {
         return $default_token->command({
             COMMAND => 'verify_cert',
-            CERTIFICATE => $cert->{DATA},
+            CERTIFICATE => $cert->pem,
             TRUSTED => $issuer_cert->{data},
         });
     }
@@ -276,7 +355,7 @@ sub _is_issuer_valid  {
 
         return $default_token->command({
             COMMAND => 'verify_cert',
-            CERTIFICATE => $cert->{DATA},
+            CERTIFICATE => $cert->pem,
             TRUSTED => $root,
             CHAIN => join "\n", @work_chain
         });
