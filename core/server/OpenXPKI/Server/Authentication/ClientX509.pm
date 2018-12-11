3a15595fd03aa64e12dcbc13ec3589a440524689
Oliver Welter <github@oliwel.de>
2018-12-11 10:59:56 +0100

    Change validation in ClientX509 to work with external certs

--- core/server/OpenXPKI/Server/Authentication/ClientX509.pm
+++ core/server/OpenXPKI/Server/Authentication/ClientX509.pm
@@ -40,7 +40,7 @@ sub login_step {
     ##! 16: 'Service Answer ' . Dumper $answer
 
     my $validate = CTX('api2')->validate_certificate(
-        pem => $params->{certificate},
+        chain => [ $params->{certificate} ],
         anchor => $self->trust_anchors(),
     );
 
