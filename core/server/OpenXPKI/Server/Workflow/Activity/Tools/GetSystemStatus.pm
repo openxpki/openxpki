71be633d01e5b5abc834ad52449d29cf82b43ce1
Oliver Welter <github@oliwel.de>
2019-12-02 08:32:36 +0100

    Add activity wrapper around get_ui_system_status

---
+++ core/server/OpenXPKI/Server/Workflow/Activity/Tools/GetSystemStatus.pm
@@ -0,0 +1,25 @@
+package OpenXPKI::Server::Workflow::Activity::Tools::GetSystemStatus;
+
+use strict;
+use base qw(OpenXPKI::Server::Workflow::Activity);
+
+use OpenXPKI::Server::Context qw(CTX);
+use OpenXPKI::Exception;
+use OpenXPKI::Debug;
+use OpenXPKI::Serialization::Simple;
+
+use Data::Dumper;
+
+sub execute {
+
+    my $self       = shift;
+    my $workflow   = shift;
+    my $context    = $workflow->context();
+
+    my $status =  CTX('api2')->get_ui_system_status();
+
+    $context->param($status);
+}
+
+1;
+__END__
