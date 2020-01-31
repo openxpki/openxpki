00127c4a09a9785bde82f618f10290ca4f548cb3
Oliver Welter <github@oliwel.de>
2020-01-31 08:28:57 +0100

    Extend ExportCRL to work with profile and serial number

--- core/server/OpenXPKI/Server/Workflow/Activity/Tools/CRLExport.pm
+++ core/server/OpenXPKI/Server/Workflow/Activity/Tools/CRLExport.pm
@@ -21,13 +21,31 @@ sub execute {
 
     my $context = $workflow->context();
 
-    my $issuer_identifier = $self->param('issuer_identifier');
+    my %query;
+    if (my $crl_serial = $self->param('crl_serial')) {
+        ##! 32: 'Use crl_serial ' . $crl_serial
+        $query{crl_serial} = $crl_serial;
+    }
+    elsif (my $issuer_identifier = $self->param('issuer_identifier')) {
+        ##! 32: 'Use issuer_identifier ' . $issuer_identifier
+        $query{issuer_identifier} = $issuer_identifier;
+    }
+    else {
+        configuration_error('Either crl_serial or issuer_identifier must be given');
+    }
+
+    if (my $crl_profile = $self->param('crl_profile')) {
+        ##! 32: 'Use profile ' . $crl_profile
+        $query{profile} = $crl_profile;
+    }
+
     my $target_key = $self->param('target_key') || 'crl_export';
     my $prefix = $self->param('prefix') || 'crl_';
     my $format = $self->param('format') || 'PEM';
 
     if ($format eq 'HASH') {
-        my $crl = CTX('api2')->get_crl( issuer_identifier => $issuer_identifier, format => 'DBINFO' );
+        my $crl = CTX('api2')->get_crl( %query, format => 'DBINFO' );
+        ##! 64: $crl
         $context->param({
             crl_serial => $crl->{crl_key},
             crl_number => $crl->{crl_number},
@@ -37,11 +55,12 @@ sub execute {
             $prefix.'publication_date' => $crl->{publication_date},
         });
     } elsif ($format eq 'PEM') {
-        my $pem = CTX('api2')->get_crl( issuer_identifier => $issuer_identifier, format => 'PEM' );
+        my $pem = CTX('api2')->get_crl( %query, format => 'PEM' );
+        ##! 128: $pem
         $context->param( $target_key  => $pem );
     } elsif ($format eq 'DER') {
         configuration_error('You can not export binary data to a non-volatile context item') if ($target_key !~ /^_/);
-        my $der = CTX('api2')->get_crl( issuer_identifier => $issuer_identifier, format => 'DER' );
+        my $der = CTX('api2')->get_crl( %query, format => 'DER' );
         $context->param( $target_key  => $der );
     }
 
@@ -57,8 +76,10 @@ OpenXPKI::Server::Workflow::Activity::Tools::CRLExport
 
 =head1 Description
 
-Export the latest CRL for a given issued to the context. Can export
-PEM/DER encoded CRL or metadata.
+Export CRL to the context, can use a CRL by its serial number or get the
+latest one for a given issuer.
+
+Export format is PEM/DER encoded CRL or metadata.
 
 =head1 Configuration
 
@@ -66,9 +87,18 @@ PEM/DER encoded CRL or metadata.
 
 =over
 
+=item crl_serial
+
+The crl serial number (database key) to export
+
 =item issuer_identifier
 
-The issuer to get the CRL for.
+Determine the latest CRL for this issuer, mutually exclusive to crl_serial
+
+=item crl_profile
+
+Export a crl that has a profile assigned, must also be given when using
+crl_serial on such a crl!
 
 =item format
 
@@ -112,7 +142,7 @@ The context key to write the result to, default is I<crl_export>.
 
 =item prefix
 
-Prefix to add to the context keys for format I<HASH>.
+Prefix to add to the context keys for format I<HASH>. Default is I<crl_>.
 
 =back
 
