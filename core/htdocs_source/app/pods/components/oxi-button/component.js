d86cd796c9074bf53b63b8c01423f689a770189a
Jens Berthold <jens@jebecs.de>
2020-03-17 18:05:42 +0100

    JS: update jsdoc in oxi-button

--- core/htdocs_source/app/pods/components/oxi-button/component.js
+++ core/htdocs_source/app/pods/components/oxi-button/component.js
@@ -4,39 +4,47 @@ import { action, set } from "@ember/object";
 
 /**
 Shows a button with an optional confirm dialog.
+
 The component has two modes:
 
-1. show a `<button/>` tag and handle clicks via callback:
-   `<OxiButton @button={{myDef1}} @onClick={{sendData}}/>`
-2. show a `<a href/>` tag and simply open the given URL:
-   `<OxiButton @button={{myDef2}}/>`
+1. show a `<a href/>` tag and simply open the given URL:
+   ```html
+   <OxiButton @button={{myDef2}} class="btn btn-default"/>
+   ```
+2. show a `<button/>` tag and handle clicks via callback:
+   ```html
+   <OxiButton @button={{myDef1}} @onClick={{sendData}} class="btn btn-default"/>
+   ```
 
 @module oxi-button
 @param { hash } button - Hash containing the button definition.
-    Mode 1:
-    ```json
-        {
-            label: "Move",
-            className: "green",
-            tooltip: "This should move it"
-            disabled: false,
-            confirm: {
-                label: "Really sure?",
-                description: "Think about it one more time."
-            },
-        }
-    ```
-    Mode 2:
-    ```json
-        {
-            label: "Learn",
-            className: "blue",
-            tooltip: "Just fyi"
-            href: "https://www.openxpki.org",
-            target: "_blank",
-        }
-    ```
-
+Mode 1 `<a href>`:
+```javascript
+{
+    className: "btn-info",
+    label: "Learn",                     // mandatory
+    tooltip: "Just fyi",
+    href: "https://www.openxpki.org",   // mandatory
+    target: "_blank",
+}
+```
+Mode 2 `<button>`:
+```javascript
+{
+    className: "btn-info",
+    label: "Move",                      // mandatory
+    tooltip: "This should move it",
+    disabled: false,
+    confirm: {
+        label: "Really sure?",          // mandatory if "confirm" exists
+        description: "Think!",          // mandatory if "confirm" exists
+        confirm_label: ""
+        cancel_label: ""
+    },
+}
+```
+@param { callback } onClick - Action handler to be called.
+The `button` hash will be passed on to the handler as single parameter.
 */
 export default class OxiButtonComponent extends Component {
     tagName = "span";
