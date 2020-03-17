fcf1f485cf8c7780a986b389b1a1b48247778360
Jens Berthold <jens@jebecs.de>
2020-03-17 15:42:12 +0100

    JS: add JSDoc configuration and npm script

---
+++ core/htdocs_source/.jsdoc.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+    source: {
+        includePattern: ".+\\.js(doc|x)?$",
+        excludePattern: "((^|\\/|\\\\)_)|(node_modules)|^dist|^dist-dev",
+    },
+    opts: {
+        source: "app",
+        recurse: true,
+        destination: "./docs-api/",
+    },
+    plugins: ['plugins/markdown'],
+    markdown: {
+        hardwrap: true,
+    }
+};
