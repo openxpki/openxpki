0b69e80c25bf53fab212291ac21d6cfb7533647f
Jens Berthold <jens@jebecs.de>
2020-03-19 00:38:47 +0100

    JS: also reset "loading" state on errors in AJAX call in oxisection-form

--- core/htdocs_source/app/pods/components/oxisection-form/component.js
+++ core/htdocs_source/app/pods/components/oxisection-form/component.js
@@ -179,6 +179,7 @@ export default class OxisectionFormComponent extends Component {
                     }
                 }
             }
-        });
+        })
+        .finally(() => this.loading = false);
     }
 }
