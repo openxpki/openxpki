52000650d793bd1ce806999e275eff8fe16f96f4
Oliver Welter <github@oliwel.de>
2020-03-18 09:26:06 +0100

    Add new method Utils.ascii_to_hex to Template::Toolkit
    Convert a given ASCII string into hexadecimal encoding.
    Used to e.g. convert keyids for AWS Cloud HSM

--- core/server/OpenXPKI/Template/Plugin/Utils.pm
+++ core/server/OpenXPKI/Template/Plugin/Utils.pm
@@ -12,6 +12,25 @@ use Data::Dumper;
 
 extends 'Template::Plugin';
 
+
+=head1 OpenXPKI::Template::Plugin::Utils
+
+Plugin for Template::Toolkit providing some string manipulation functions.
+
+=cut
+
+=head2 How to use
+
+You need to load the plugin into your template before using it. As we do not
+export the methods, you need to address them with the plugin name, e.g.
+
+    [% USE Utils %]
+    [% Utils.ascii_to_hex(value) %]
+
+Will output the converted string.
+
+=cut
+
 has 'uuid_gen' => (
     is => 'ro',
     isa => 'Object',
@@ -19,9 +38,36 @@ has 'uuid_gen' => (
     default => sub { use Data::UUID; return Data::UUID->new(); }
 );
 
+
+=head2 Methods
+
+=head3 uuid
+
+Generate a UUID v3 string (e.g. 4162F712-1DD2-11B2-B17E-C09EFE1DC403)
+
+This is simply a wrapper around Data::UUID->gen_str.
+
+=cut
+
 sub uuid {
     my $self = shift;
     return $self->uuid_gen()->create_str();
+}
+
+=head3 ascii_to_hex
+
+Convert a ascii string to its hexadecimal representation, e.g. "OpenXPKI"
+becomes 4f70656e58504b49.
+
+=cut
+
+sub ascii_to_hex {
+
+    my $self = shift;
+    my $string = shift;
+    $string =~ s/(.)/sprintf("%02x",ord($1))/eg;
+    return $string
+
 }
 
 
