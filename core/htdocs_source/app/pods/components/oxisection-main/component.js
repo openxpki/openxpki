40f83b52bca2c8a8e46a00e2cce2d45b0fd07583
Jens Berthold <jens@jebecs.de>
2020-03-19 00:41:03 +0100

    JS: convert oxisection-main from @ember/component to @glimmer/component

--- core/htdocs_source/app/pods/components/oxisection-main/component.js
+++ core/htdocs_source/app/pods/components/oxisection-main/component.js
@@ -1,30 +1,29 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
+import { action } from "@ember/object";
 
-const OxisectionMainComponent = Component.extend({
-    classNameBindings: ["type"],
-    type: Em.computed("content.type", function() {
-        return "oxisection-" + this.get("content.type");
-    }),
-    actions: {
-        buttonClick: function(button) {
-            Em.set(button, "loading", true);
-            if (button.action) {
-                return this.container.lookup("route:openxpki")
-                .sendAjax({
-                    data: {
-                        action: button.action
-                    }
-                })
-                .then(
-                    () => Em.set(button, "loading", false),
-                    () => Em.set(button, "loading", false)
-                );
-            }
-            else {
-                return this.container.lookup("route:openxpki").transitionTo("openxpki", button.page);
-            }
+export default class OxisectionMainComponent extends Component {
+    get type() {
+        return "oxisection-" + this.args.content.type;
+    }
+
+    @action
+    buttonClick(button) {
+        console.error("oxisection-main buttonClick");
+        Em.set(button, "loading", true);
+        if (button.action) {
+            return this.container.lookup("route:openxpki")
+            .sendAjax({
+                data: {
+                    action: button.action
+                }
+            })
+            .then(
+                () => Em.set(button, "loading", false),
+                () => Em.set(button, "loading", false)
+            );
+        }
+        else {
+            return this.container.lookup("route:openxpki").transitionTo("openxpki", button.page);
         }
     }
-});
-
-export default OxisectionMainComponent;
+}
