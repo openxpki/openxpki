825c559c6523e6d418c6e5d760244fb855565370
Jens Berthold <jens@jebecs.de>
2020-03-26 00:56:57 +0100

    JS: migrate oxifield-password back to <input>
    This is due to problems with Ember <Input>'s @value property being
    updated after an {{on "input" xxx}} event handler

---
+++ core/htdocs_source/app/pods/components/oxifield-password/component.js
@@ -0,0 +1,11 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action, set } from '@ember/object';
+
+export default class OxifieldPasswordComponent extends Component {
+    @action
+    onInput(event) {
+        // we need to update using set() because this.args.content.value is not a @tracked property
+        set(this.args.content, "value", event.target.value);
+    }
+}
