06e71feeb91b8c0cbc8f6b3dacf9496f258c8e69
Jens Berthold <jens@jebecs.de>
2020-03-26 23:56:03 +0100

    JS: fix stylesheets by adding SCSS support to Ember

--- core/htdocs_source/ember-cli-build.js
+++ core/htdocs_source/ember-cli-build.js
@@ -10,7 +10,7 @@ module.exports = function(defaults) {
 
       'ember-bootstrap': {
         bootstrapVersion: 3,
-        importBootstrapCSS: true,
+        importBootstrapCSS: false,
         importBootstrapFont: true
         // whitelist: ['bs-button', 'bs-modal', 'bs-form']
       }
@@ -28,6 +28,7 @@ module.exports = function(defaults) {
     // modules that you would like to import into your application
     // please specify an object with the list of modules as keys
     // along with the exports of each module as its value.
+    app.import("node_modules/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.css");
 
     return app.toTree();
 };
