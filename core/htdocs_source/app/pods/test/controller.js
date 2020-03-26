5de01d9d9e5eff62034be9fb53e2cdcd28e63936
Jens Berthold <jens@jebecs.de>
2020-03-26 01:09:30 +0100

    JS: update test controller

--- core/htdocs_source/app/pods/test/controller.js
+++ core/htdocs_source/app/pods/test/controller.js
@@ -3,39 +3,63 @@ import { tracked } from '@glimmer/tracking';
 import { action, set } from "@ember/object";
 
 export default class TestController extends Controller {
-    @tracked testLinkA = {
-        label: "Learn",
+    testButton = {
+        label: "Button",
         className: "btn-info",
-        tooltip: "Just fyi",
-        href: "https://www.openxpki.org",
-        target: "_blank",
-    };
-    @tracked testLinkButton = {
-        label: "Move",
-        className: "btn-info",
-        tooltip: "This should move it",
+        tooltip: "This should do it",
         disabled: false,
     };
-    @tracked testLinkButtonConfirm = {
-        ...this.testLinkButton,
-        label: "Confirm & Move",
-        confirm: {
-            label: "Really sure?",
-            description: "Think about it one more time.",
-        },
+
+    @tracked formDef = {
+        type: "form",
+        action: "dummy",
+        content: {
+            title: "Test input",
+            submit_label: "Perform",
+            fields: [
+                {
+                    type: "select",
+                    name: "test_select",
+                    label: "Your choice:",
+                    options: [
+                        { value: 1, label: "Major" },
+                        { value: 2, label: "Tom" },
+                    ],
+                },
+                {
+                    type: "password",
+                    name: "pwd",
+                    label: "Password",
+                },
+                {
+                    type: "passwordverify",
+                    name: "pwd_verified",
+                    label: "Verified password",
+                },
+            ],
+            buttons: [
+                {
+                    label: "Link to external",
+                    className: "btn-info",
+                    tooltip: "Just fyi",
+                    href: "https://www.openxpki.org",
+                    target: "_blank",
+                },
+                this.testButton,
+                {
+                    ...this.testButton,
+                    label: "With confirmation",
+                    confirm: {
+                        label: "Really sure?",
+                        description: "Think about it one more time.",
+                    },
+                },
+                {
+                    ...this.testButton,
+                    label: "Disabled",
+                    disabled: true,
+                },
+            ],
+        }
     };
-    @tracked list = [
-        { value: 1, label: "Major" },
-        { value: 2, label: "Tom" },
-    ];
-    @tracked listSelection = null;
-
-    @action testAction(button) {
-        console.log(button);
-        set(this.testLinkButton, "className", "btn-warning");
-    }
-
-    @action itemSelected(value, label) {
-        console.log("SELECTED", value, label);
-    }
 }
