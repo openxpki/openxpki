449c3a56ea1f1ccc102742a20601f149442448ac
Jens Berthold <jens@jebecs.de>
2020-03-29 00:51:38 +0100

    JS: migrate oxisection-keyvalue to @glimmer/component

--- core/htdocs_source/app/pods/components/oxisection-keyvalue/component.js
+++ core/htdocs_source/app/pods/components/oxisection-keyvalue/component.js
@@ -1,19 +1,14 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
+import { computed } from '@ember/object';
 import types from "../oxivalue-format/types";
 
-const OxisectionKeyvalueComponent = Component.extend({
-    items: Em.computed("content.content.data", function() {
-        let items = this.get("content.content.data") || [];
+export default class OxisectionKeyvalueComponent extends Component {
+    @computed("args.content.content.data")
+    get items() {
+        let items = this.args.content.content.data || [];
         for (const i of items) {
             if (i.format === 'head') { i.isHead = 1 }
         }
         return items.filter(item => types[item.format || 'text'](item.value) !== '');
-    }),
-    actions: {
-        buttonClick: function(button) {
-            return this.sendAction("buttonClick", button);
-        }
     }
-});
-
-export default OxisectionKeyvalueComponent;
+}
