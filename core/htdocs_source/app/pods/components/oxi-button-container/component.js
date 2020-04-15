de4ecca32d9159fc764360c87a2b9ef20376b1b6
Jens Berthold <jens@jebecs.de>
2020-04-15 16:23:13 +0200

    JS: justify buttons in button groups

--- core/htdocs_source/app/pods/components/oxi-button-container/component.js
+++ core/htdocs_source/app/pods/components/oxi-button-container/component.js
@@ -2,6 +2,19 @@ import Component from '@glimmer/component';
 import { computed } from '@ember/object';
 
 export default class OxiButtonContainerComponent extends Component {
+    get buttonGroups() {
+        let currentGroup = [];
+        let groups = [currentGroup];
+        let buttons = this.args.buttons;
+
+        for (const btn of buttons) {
+            if (btn.break_before) { currentGroup = []; groups.push(currentGroup) }
+            currentGroup.push(btn);
+            if (btn.break_after)  { currentGroup = []; groups.push(currentGroup) }
+        }
+        return groups;
+    }
+
     @computed("args.buttons.@each.description")
     get hasDescription() {
         let ref;
