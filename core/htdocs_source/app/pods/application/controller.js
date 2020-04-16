755785464c3f6a5d9da7a991c7636a7b54a1c29a
Jens Berthold <jens@jebecs.de>
2020-04-17 00:35:59 +0200

    JS: update layout for "Loading..." message + remove related jQuery usage

--- core/htdocs_source/app/pods/application/controller.js
+++ core/htdocs_source/app/pods/application/controller.js
@@ -1,13 +1,12 @@
 import Controller, { inject as injectCtrl } from '@ember/controller';
 import { alias } from '@ember/object/computed';
 import { action } from "@ember/object";
-import $ from "jquery";
 
 export default class ApplicationController extends Controller {
     @injectCtrl openxpki; // injects OpenxpkiController
     @alias("openxpki.model.user") user;
 
     @action removeLoader() {
-        $(".waiting-for-ember").remove();
+        document.getElementById("waiting-for-ember").remove();
     }
 }
