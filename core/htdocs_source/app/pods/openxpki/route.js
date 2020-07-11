235348d663fd36fe58e0da846a18c942c597b585
Jens Berthold <jens@jebecs.de>
2020-07-11 10:51:12 +0200

    UI: Switch to YAML config format; load YAML+CSS asynchronously

--- core/htdocs_source/app/pods/openxpki/route.js
+++ core/htdocs_source/app/pods/openxpki/route.js
@@ -63,13 +63,16 @@ export default class OpenXpkiRoute extends Route {
          * load base page structure first first time or for special pages ("needReboot")
          */
         if (!this.content.navEntries.length || this.needReboot.indexOf(modelId) >= 0) {
-            structureIfNeeded = this.sendAjax({
-                page: "bootstrap!structure",
-                baseurl: window.location.pathname,
-            });
+            // don't send request yet, only create a lambda via arrow function expression
+            structureIfNeeded = () => {
+                return this.sendAjax({
+                    page: "bootstrap!structure",
+                    baseurl: window.location.pathname,
+                });
+            };
         }
         else {
-            structureIfNeeded = Promise.resolve();
+            structureIfNeeded = () => { Promise.resolve() };
         }
 
         /*
@@ -86,9 +89,9 @@ export default class OpenXpkiRoute extends Route {
         if (flatList.findBy("key", modelId) || this.needReboot.indexOf(modelId) >= 0) {
             request.target = "top";
         }
-        return structureIfNeeded.then(() => {
-            return this.sendAjax(request);
-        });
+        return this.config.ready // localconfig.js might change rootURL, so first thing is to query it
+            .then( () => structureIfNeeded() )
+            .then( () => this.sendAjax(request) );
     }
 
     // Reserved Ember function "model"
--- core/htdocs_source/app/pods/oxi-config/service.js
+++ core/htdocs_source/app/pods/oxi-config/service.js
@@ -1,22 +1,65 @@
 import Service from '@ember/service';
 import ENV from 'openxpki/config/environment';
+import { tracked } from '@glimmer/tracking';
+import fetch from 'fetch';
+import { isNotFoundResponse } from 'ember-fetch/errors';
+import yaml from 'js-yaml';
 
 export default class OxiConfigService extends Service {
     localConfig = {};
+    ready; // will be set to a Promise that will fulfill if localconfig.js is loaded (or server returned error)
+    @tracked localCSS;
 
     constructor() {
         super(...arguments);
-        /* eslint-disable-next-line no-undef */
-        if (typeof OXI_LOCALCONFIG !== 'undefined') {
-            /* eslint-disable-next-line no-undef */
-            this.localConfig = OXI_LOCALCONFIG;
+
+        // load custom YAML config
+        this.ready = this._loadRemote('localconfig.yaml')
+            .then( yamlStr => {
+                try {
+                    let doc = yaml.safeLoad(yamlStr);
+                    this.localConfig = doc;
+                }
+                catch (err) {
+                    /* eslint-disable-next-line no-console */
+                    console.error(`Error parsing localconfig.yaml:\n${err}`);
+                }
+            } )
+            .catch( () => {} ); // ignore errors as they were logged in _loadRemote()
+
+        // load custom CSS
+        this._loadRemote('localconfig.css')
+        .then( css => this.localCSS = css )
+        .catch( () => {} ); // ignore errors as they were logged in _loadRemote()
+    }
+
+    // Tries to load the given file below server root URL.
+    // Returns a Promise.
+    _loadRemote(file) {
+        let rootURL = ENV.rootURL.replace(/\/$/, '');
+        return fetch(`${rootURL}/${file}`)
+        .then(response => {
+            if (response.ok) {
+                /* eslint-disable-next-line no-console */
+                console.log(`Using custom configuration ${file}`);
+                return response.text()
+            }
+            else {
+                if (isNotFoundResponse(response)) {
+                    /* eslint-disable-next-line no-console */
+                    console.info(`No ${file} provided - using defaults`);
+                }
+                else {
+                    /* eslint-disable-next-line no-console */
+                    console.error(`Error loading ${file} (server error: ${response.status})`);
+                }
+                return null;
+            }
+        })
+        .catch(error => {
             /* eslint-disable-next-line no-console */
-            console.log("Using custom configuration in localconfig.js");
-        }
-        else {
-            /* eslint-disable-next-line no-console */
-            console.log("No localconfig.js found - using defaults");
-        }
+            console.error(`Error loading ${file} (network error: ${error.name})`);
+        });
     }
 
     get backendPath() {
