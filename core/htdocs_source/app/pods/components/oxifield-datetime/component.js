f3294b2b29b726e02ab6e3cb15e9015f06d7b361
Jens Berthold <jens@jebecs.de>
2020-08-07 16:00:29 +0200

    UI: use locale in oxifield-datetime (doesn't work yet, to be examined)

--- core/htdocs_source/app/pods/components/oxifield-datetime/component.js
+++ core/htdocs_source/app/pods/components/oxifield-datetime/component.js
@@ -1,13 +1,17 @@
 import Component from '@glimmer/component';
 import { action, computed } from '@ember/object';
+import { inject as service } from '@ember/service';
 import moment from "moment-timezone";
 
 export default class OxifieldDatetimeComponent extends Component {
+    @service('oxi-locale') oxiLocale;
+
     format = "DD.MM.YYYY HH:mm";
     value;
 
     constructor() {
         super(...arguments);
+
         let val = this.args.content.value;
         if ("now" === val) val = (new Date()).valueOf() / 1000;
         let tz = this.args.content.timezone;
