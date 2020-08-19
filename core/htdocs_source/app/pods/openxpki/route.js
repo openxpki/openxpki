openxpki/openxpki

commit 5b1f12e6789c84c0e2d7406ff22cfbc2652ec637

UI: small refactoring of openxpki/route.js; modify debug statements

core/htdocs_source/app/pods/openxpki/route.js
@@ -114,7 +114,7 @@ export default class OpenXpkiRoute extends Route {
 
     sendAjax(request) {
         this.setLoadingState(true);
-        debug("openxpki/route - sendAjax: page = " + request.page);
+        debug("openxpki/route - sendAjax: " + ['page','action'].map(p=>request[p]?`${p} = ${request[p]}`:null).filter(e=>e!==null).join(", "));
         // assemble request parameters
         let req = {
             data: {
@@ -125,20 +125,7 @@ export default class OpenXpkiRoute extends Route {
             type: request.action ? "POST" : "GET",
             url: this.oxiConfig.backendUrl,
         };
-        if (req.type === "POST") {
-            req.data._rtoken = this.content.rtoken;
-        }
-
-        // Fetch "targetElement" parameter for use in AJAX response handler later on.
-        // Pseudo-target "self" is transformed so new content will be shown in the
-        // currently active place: a modal popup, an active tab or on top (i.e. single hidden tab)
-        let targetElement = req.data.target || "self";
-        if (targetElement === "modal") targetElement = "popup"; // legacy naming
-        if (targetElement === "self") {
-            if (this.content.popup) { targetElement = "popup" }
-            else if (this.content.tabs.length > 1) { targetElement = "active" }
-            else { targetElement = "top" }
-        }
+        if (req.type === "POST") req.data._rtoken = this.content.rtoken;
 
         if (this.content.refresh) {
             cancel(this.content.refresh);
@@ -181,47 +168,12 @@ export default class OpenXpkiRoute extends Route {
                         this.content.rtoken = doc.rtoken;
 
                         // set locale
-                        if (doc.language) {
-                            this.oxiLocale.locale = doc.language;
-                        }
+                        if (doc.language) this.oxiLocale.locale = doc.language;
                     }
                     else {
                         if (doc.page && doc.main) {
                             debug("openxpki/route - sendAjax response: \"page\" and \"main\"");
-
-                            let newTab = {
-                                active: true,
-                                page: doc.page,
-                                main: doc.main,
-                                right: doc.right
-                            };
-
-                            // Mark the first form on screen: only the first one is allowed to focus
-                            // its first input field.
-                            let isFirst = true;
-                            for (const section of [...(newTab.main||[]), ...(newTab.right||[])]) {
-                                if (section.type === "form") {
-                                    section.content.isFirstForm = isFirst;
-                                    if (isFirst) isFirst = false;
-                                }
-                            }
-
-                            if (targetElement === "popup") {
-                                this.content.popup = newTab;
-                            }
-                            else if (targetElement === "tab") {
-                                let tabs = this.content.tabs;
-                                tabs.setEach("active", false);
-                                tabs.pushObject(newTab);
-                            }
-                            else if (targetElement === "active") {
-                                let tabs = this.content.tabs;
-                                let index = tabs.indexOf(tabs.findBy("active"));
-                                tabs.replace(index, 1, [newTab]); // top
-                            }
-                            else {
-                                this.content.tabs = [newTab];
-                            }
+                            this._setPageContent(req.data.target, doc.page, doc.main, doc.right);
                         }
                         this.setLoadingState(false);
                     }
@@ -237,6 +189,55 @@ export default class OpenXpkiRoute extends Route {
                 }
             );
         });
+    }
+
+    _setPageContent(target = 'self', page, main, right) {
+        let newTab = {
+            active: true,
+            page: page,
+            main: main,
+            right: right
+        };
+
+        // Mark the first form on screen: only the first one is allowed to focus
+        // its first input field.
+        let isFirst = true;
+        for (const section of [...(newTab.main||[]), ...(newTab.right||[])]) {
+            if (section.type === "form") {
+                section.content.isFirstForm = isFirst;
+                if (isFirst) isFirst = false;
+            }
+        }
+
+        // Pseudo-target "self" is transformed so new content will be shown in the
+        // currently active place: a modal popup, an active tab or on top (i.e. single hidden tab)
+        if (target === 'self') {
+            if (this.content.popup) { target = 'popup' }
+            else if (this.content.tabs.length > 1) { target = 'active' }
+            else { target = 'top' }
+        }
+        if (target === 'modal') target = 'popup'; // FIXME legacy naming
+
+        // Popup
+        if (target === "popup") {
+            this.content.popup = newTab;
+        }
+        // New tab
+        else if (target === "tab") {
+            let tabs = this.content.tabs;
+            tabs.setEach("active", false);
+            tabs.pushObject(newTab);
+        }
+        // Current tab
+        else if (target === "active") {
+            let tabs = this.content.tabs;
+            let index = tabs.indexOf(tabs.findBy("active"));
+            tabs.replace(index, 1, [newTab]); // top
+        }
+        // Set as only tab
+        else {
+            this.content.tabs = [newTab];
+        }
     }
 
     updateNavEntryActiveState() {

core/htdocs_source/app/pods/components/oxi-section/form/component.js
@@ -40,8 +40,8 @@ class Field {
             if (! this.prototype.hasOwnProperty(attr)) {
                 /* eslint-disable-next-line no-console */
                 console.error(
-                    `oxisection/form: unknown property "${attr}" in field "${sourceHash.name}". ` +
-                    `If it's a new property, please add it to class 'Field' defined in ../oxisection/form/component.js.`
+                    `oxi-section/form (${this.args.def.action}): unknown property "${attr}" in field "${sourceHash.name}". ` +
+                    `If it's a new property, please add it to class 'Field' defined in ../oxi-section/form (${this.args.def.action})/component.js.`
                 );
             }
             else {
@@ -228,14 +228,14 @@ export default class OxiSectionFormComponent extends Component {
      */
     @action
     setFieldValue(field, value) {
-        debug(`oxisection/form: setFieldValue (${field.name} = "${value}")`);
+        debug(`oxi-section/form (${this.args.def.action}): setFieldValue (${field.name} = "${value}")`);
         field.value = value;
         field.error = null;
 
         // action on change?
         if (!field.actionOnChange) { return }
 
-        debug(`oxisection/form: executing actionOnChange ("${field.actionOnChange}")`);
+        debug(`oxi-section/form (${this.args.def.action}): executing actionOnChange ("${field.actionOnChange}")`);
         let request = {
             action: field.actionOnChange,
             _sourceField: field.name,
@@ -264,7 +264,7 @@ export default class OxiSectionFormComponent extends Component {
      */
     @action
     setFieldName(field, name) {
-        debug(`oxisection/form: setFieldName (${field.name} -> ${name})`);
+        debug(`oxi-section/form (${this.args.def.action}): setFieldName (${field.name} -> ${name})`);
         field.name = name;
     }
 
@@ -273,7 +273,7 @@ export default class OxiSectionFormComponent extends Component {
      */
     @action
     setFieldError(field, message) {
-        debug(`oxisection/form: setFieldError (${field.name} = ${message})`);
+        debug(`oxi-section/form (${this.args.def.action}): setFieldError (${field.name} = ${message})`);
         field.error = message;
     }
 
@@ -292,25 +292,25 @@ export default class OxiSectionFormComponent extends Component {
         // if all form fields sent feedback, choose first focusable field
         let feedbackCount = Object.keys(this.focusFeedback).length;
         if (feedbackCount === this.originalFieldCount) {
-            debug(`oxi-section/form: focus feedback completed from all fields`);
             this.focusFirstField();
         }
     }
 
     focusFirstField() {
         if (!this.args.def.isFirstForm) {
-            debug(`oxi-section/form: we are not the first form - NOT setting focus`);
+            debug(`oxi-section/form (${this.args.def.action}): we are not the first form - NOT setting focus`);
             return;
         }
         this.focusFeedbackComplete = true;
         for (const field of this.visibleFields) {
             if (this.focusFeedback[field._refName] !== null) {
-                debug(`oxi-section/form: first focusable field: ${field._refName}`);
+                debug(`oxi-section/form (${this.args.def.action}): first focusable field: ${field._refName}`);
                 let elementToFocus = this.focusFeedback[field._refName];
                 elementToFocus.focus();
-                break;
+                return;
             }
         }
+        debug(`oxi-section/form (${this.args.def.action}): no focusable field found`);
     }
 
     @action
@@ -320,7 +320,7 @@ export default class OxiSectionFormComponent extends Component {
 
     @action
     submit() {
-        debug("oxisection/form: submit");
+        debug(`oxi-section/form (${this.args.def.action}): submit`);
 
         // check validity and gather form data
         let isError = false;

core/server/htdocs/assets/openxpki.js
@@ -454,7 +454,7 @@ return Object.keys(r).forEach((function(e){i[e]=r[e]})),i.enumerable=!!i.enumera
 var je=(n=Ember._tracked,r=Ember._tracked,o=Ember._tracked,i=Ember._tracked,a=Ember._tracked,l=Ember._tracked,u=Ember._tracked,c=Ember._tracked,s=Ember._tracked,f=Ember._tracked,p=Ember._tracked,d=Ember._tracked,b=Ember._tracked,m=Ember._tracked,y=Ember._tracked,v=Ember._tracked,h=Ember._tracked,g=Ember._tracked,O=Ember._tracked,w=Ember._tracked,x=Ember._tracked,_=_e((k=function(){function e(){Oe(this,e),ge(this,"type",_,this),ge(this,"name",j,this),ge(this,"_refName",P,this),ge(this,"value",E,this),ge(this,"label",S,this),ge(this,"is_optional",C,this),ge(this,"tooltip",D,this),ge(this,"placeholder",z,this),ge(this,"actionOnChange",R,this),ge(this,"error",A,this),ge(this,"clonable",M,this),ge(this,"max",T,this),ge(this,"_canDelete",I,this),ge(this,"_canAdd",B,this),ge(this,"_focusClone",N,this),ge(this,"keys",F,this),ge(this,"timezone",L,this),ge(this,"options",G,this),ge(this,"prompt",U,this),ge(this,"editable",Y,this),ge(this,"verbose",Q,this)}return xe(e,[{key:"clone",value:function(){var t=this,n=new e
 return Object.keys(Object.getPrototypeOf(this)).forEach((function(e){return n[e]=t[e]})),n}},{key:"toPlainHash",value:function(){var e=this,t={}
 return Object.keys(Object.getPrototypeOf(this)).forEach((function(n){return t[n]=e[n]})),t}}],[{key:"fromHash",value:function(e){for(var t=new this,n=0,r=Object.keys(e);n<r.length;n++){var o=r[n]
-this.prototype.hasOwnProperty(o)?t[o]=e[o]:console.error('oxisection/form: unknown property "'.concat(o,'" in field "').concat(e.name,'". ')+"If it's a new property, please add it to class 'Field' defined in ../oxisection/form/component.js.")}return t}}]),e}()).prototype,"type",[n],{configurable:!0,enumerable:!0,writable:!0,initializer:null}),j=_e(k.prototype,"name",[r],{configurable:!0,enumerable:!0,writable:!0,initializer:null}),P=_e(k.prototype,"_refName",[o],{configurable:!0,enumerable:!0,writable:!0,initializer:null}),E=_e(k.prototype,"value",[i],{configurable:!0,enumerable:!0,writable:!0,initializer:null}),S=_e(k.prototype,"label",[a],{configurable:!0,enumerable:!0,writable:!0,initializer:null}),C=_e(k.prototype,"is_optional",[l],{configurable:!0,enumerable:!0,writable:!0,initializer:null}),D=_e(k.prototype,"tooltip",[u],{configurable:!0,enumerable:!0,writable:!0,initializer:null}),z=_e(k.prototype,"placeholder",[c],{configurable:!0,enumerable:!0,writable:!0,initializer:null}),R=_e(k.prototype,"actionOnChange",[s],{configurable:!0,enumerable:!0,writable:!0,initializer:null}),A=_e(k.prototype,"error",[f],{configurable:!0,enumerable:!0,writable:!0,initializer:null}),M=_e(k.prototype,"clonable",[p],{configurable:!0,enumerable:!0,writable:!0,initializer:null}),T=_e(k.prototype,"max",[d],{configurable:!0,enumerable:!0,writable:!0,initializer:null}),I=_e(k.prototype,"_canDelete",[b],{configurable:!0,enumerable:!0,writable:!0,initializer:null}),B=_e(k.prototype,"_canAdd",[m],{configurable:!0,enumerable:!0,writable:!0,initializer:null}),N=_e(k.prototype,"_focusClone",[y],{configurable:!0,enumerable:!0,writable:!0,initializer:function(){return!1}}),F=_e(k.prototype,"keys",[v],{configurable:!0,enumerable:!0,writable:!0,initializer:null}),L=_e(k.prototype,"timezone",[h],{configurable:!0,enumerable:!0,writable:!0,initializer:null}),G=_e(k.prototype,"options",[g],{configurable:!0,enumerable:!0,writable:!0,initializer:null}),U=_e(k.prototype,"prompt",[O],{configurable:!0,enumerable:!0,writable:!0,initializer:null}),Y=_e(k.prototype,"editable",[w],{configurable:!0,enumerable:!0,writable:!0,initializer:null}),Q=_e(k.prototype,"verbose",[x],{configurable:!0,enumerable:!0,writable:!0,initializer:null}),k),Pe=(H=Ember.inject.service("intl"),V=Ember._tracked,K=Ember._tracked,q=Ember._action,W=Ember._action,J=Ember._action,Z=Ember._action,X=Ember._action,$=Ember._action,ee=Ember._action,te=Ember._action,ae=function(e){(function(e,t){if("function"!=typeof t&&null!==t)throw new TypeError("Super expression must either be null or a function")
+this.prototype.hasOwnProperty(o)?t[o]=e[o]:console.error("oxi-section/form (".concat(this.args.def.action,'): unknown property "').concat(o,'" in field "').concat(e.name,'". ')+"If it's a new property, please add it to class 'Field' defined in ../oxi-section/form (".concat(this.args.def.action,")/component.js."))}return t}}]),e}()).prototype,"type",[n],{configurable:!0,enumerable:!0,writable:!0,initializer:null}),j=_e(k.prototype,"name",[r],{configurable:!0,enumerable:!0,writable:!0,initializer:null}),P=_e(k.prototype,"_refName",[o],{configurable:!0,enumerable:!0,writable:!0,initializer:null}),E=_e(k.prototype,"value",[i],{configurable:!0,enumerable:!0,writable:!0,initializer:null}),S=_e(k.prototype,"label",[a],{configurable:!0,enumerable:!0,writable:!0,initializer:null}),C=_e(k.prototype,"is_optional",[l],{configurable:!0,enumerable:!0,writable:!0,initializer:null}),D=_e(k.prototype,"tooltip",[u],{configurable:!0,enumerable:!0,writable:!0,initializer:null}),z=_e(k.prototype,"placeholder",[c],{configurable:!0,enumerable:!0,writable:!0,initializer:null}),R=_e(k.prototype,"actionOnChange",[s],{configurable:!0,enumerable:!0,writable:!0,initializer:null}),A=_e(k.prototype,"error",[f],{configurable:!0,enumerable:!0,writable:!0,initializer:null}),M=_e(k.prototype,"clonable",[p],{configurable:!0,enumerable:!0,writable:!0,initializer:null}),T=_e(k.prototype,"max",[d],{configurable:!0,enumerable:!0,writable:!0,initializer:null}),I=_e(k.prototype,"_canDelete",[b],{configurable:!0,enumerable:!0,writable:!0,initializer:null}),B=_e(k.prototype,"_canAdd",[m],{configurable:!0,enumerable:!0,writable:!0,initializer:null}),N=_e(k.prototype,"_focusClone",[y],{configurable:!0,enumerable:!0,writable:!0,initializer:function(){return!1}}),F=_e(k.prototype,"keys",[v],{configurable:!0,enumerable:!0,writable:!0,initializer:null}),L=_e(k.prototype,"timezone",[h],{configurable:!0,enumerable:!0,writable:!0,initializer:null}),G=_e(k.prototype,"options",[g],{configurable:!0,enumerable:!0,writable:!0,initializer:null}),U=_e(k.prototype,"prompt",[O],{configurable:!0,enumerable:!0,writable:!0,initializer:null}),Y=_e(k.prototype,"editable",[w],{configurable:!0,enumerable:!0,writable:!0,initializer:null}),Q=_e(k.prototype,"verbose",[x],{configurable:!0,enumerable:!0,writable:!0,initializer:null}),k),Pe=(H=Ember.inject.service("intl"),V=Ember._tracked,K=Ember._tracked,q=Ember._action,W=Ember._action,J=Ember._action,Z=Ember._action,X=Ember._action,$=Ember._action,ee=Ember._action,te=Ember._action,ae=function(e){(function(e,t){if("function"!=typeof t&&null!==t)throw new TypeError("Super expression must either be null or a function")
 e.prototype=Object.create(t&&t.prototype,{constructor:{value:e,writable:!0,configurable:!0}}),t&&be(e,t)})(n,e)
 var t=me(n)
 function n(){var e
@@ -480,18 +480,17 @@ var l=function(e){return e instanceof ArrayBuffer?(a="_encoded_base64_".concat(o
 r[a]=u,Ember.debug("".concat(o," = ").concat(i[0].clonable?"[".concat(r[a],"]"):'"'.concat(r[a],'"')))}
 for(o.s();!(t=o.n()).done;){var a
 i()}}catch(l){o.e(l)}finally{o.f()}return r}},{key:"setFieldValue",value:function(e,t){var n=this
-if(Ember.debug("oxisection/form: setFieldValue (".concat(e.name,' = "').concat(t,'")')),e.value=t,e.error=null,e.actionOnChange){Ember.debug('oxisection/form: executing actionOnChange ("'.concat(e.actionOnChange,'")'))
+if(Ember.debug("oxi-section/form (".concat(this.args.def.action,"): setFieldValue (").concat(e.name,' = "').concat(t,'")')),e.value=t,e.error=null,e.actionOnChange){Ember.debug("oxi-section/form (".concat(this.args.def.action,'): executing actionOnChange ("').concat(e.actionOnChange,'")'))
 var r=ue({action:e.actionOnChange,_sourceField:e.name},this._fields2request(!0)),o=this.fields
 return Ember.getOwner(this).lookup("route:openxpki").sendAjax(r).then((function(e){var t,r=fe(n._prepareFields(e.fields))
 try{for(r.s();!(t=r.n()).done;){var i,a=t.value,l=fe(o)
 try{for(l.s();!(i=l.n()).done;){var u=i.value
 if(u.name===a.name){var c=o.indexOf(u)
-o[c]=a,n.fields=o}}}catch(s){l.e(s)}finally{l.f()}}}catch(s){r.e(s)}finally{r.f()}return null}))}}},{key:"setFieldName",value:function(e,t){Ember.debug("oxisection/form: setFieldName (".concat(e.name," -> ").concat(t,")")),e.name=t}},{key:"setFieldError",value:function(e,t){Ember.debug("oxisection/form: setFieldError (".concat(e.name," = ").concat(t,")")),e.error=t}},{key:"fieldMayFocus",value:function(e,t,n){this.focusFeedbackComplete||(this.focusFeedback[e._refName]=t?n:null,Object.keys(this.focusFeedback).length===this.originalFieldCount&&(Ember.debug("oxi-section/form: focus feedback completed from all fields"),this.focusFirstField()))}},{key:"focusFirstField",value:function(){if(this.args.def.isFirstForm){this.focusFeedbackComplete=!0
+o[c]=a,n.fields=o}}}catch(s){l.e(s)}finally{l.f()}}}catch(s){r.e(s)}finally{r.f()}return null}))}}},{key:"setFieldName",value:function(e,t){Ember.debug("oxi-section/form (".concat(this.args.def.action,"): setFieldName (").concat(e.name," -> ").concat(t,")")),e.name=t}},{key:"setFieldError",value:function(e,t){Ember.debug("oxi-section/form (".concat(this.args.def.action,"): setFieldError (").concat(e.name," = ").concat(t,")")),e.error=t}},{key:"fieldMayFocus",value:function(e,t,n){this.focusFeedbackComplete||(this.focusFeedback[e._refName]=t?n:null,Object.keys(this.focusFeedback).length===this.originalFieldCount&&this.focusFirstField())}},{key:"focusFirstField",value:function(){if(this.args.def.isFirstForm){this.focusFeedbackComplete=!0
 var e,t=fe(this.visibleFields)
 try{for(t.s();!(e=t.n()).done;){var n=e.value
-if(null!==this.focusFeedback[n._refName]){Ember.debug("oxi-section/form: first focusable field: ".concat(n._refName)),this.focusFeedback[n._refName].focus()
-break}}}catch(r){t.e(r)}finally{t.f()}}else Ember.debug("oxi-section/form: we are not the first form - NOT setting focus")}},{key:"reset",value:function(){this.args.buttonClick({page:this.args.def.reset})}},{key:"submit",value:function(){var e=this
-Ember.debug("oxisection/form: submit")
+if(null!==this.focusFeedback[n._refName])return Ember.debug("oxi-section/form (".concat(this.args.def.action,"): first focusable field: ").concat(n._refName)),void this.focusFeedback[n._refName].focus()}}catch(r){t.e(r)}finally{t.f()}Ember.debug("oxi-section/form (".concat(this.args.def.action,"): no focusable field found"))}else Ember.debug("oxi-section/form (".concat(this.args.def.action,"): we are not the first form - NOT setting focus"))}},{key:"reset",value:function(){this.args.buttonClick({page:this.args.def.reset})}},{key:"submit",value:function(){var e=this
+Ember.debug("oxi-section/form (".concat(this.args.def.action,"): submit"))
 var t,n=!1,r=fe(this.fields)
 try{for(r.s();!(t=r.n()).done;){var o=t.value
 o.is_optional||o.value?o.error&&(n=!0):(n=!0,o.error=this.intl.t("component.oxisection_form.missing_value"))}}catch(a){r.e(a)}finally{r.f()}if(!n){var i=ue({action:this.args.def.action},this._fields2request(!1))
@@ -891,18 +890,13 @@ Ember.debug("openxpki/route - beforeModel: model_id = "+o),r.force&&delete r.for
 var i={page:o}
 return r.limit&&(i.limit=r.limit),r.startat&&(i.startat=r.startat),(this.content.navEntries.reduce((function(e,t){return e.concat(t,t.entries||[])}),[]).findBy("key",o)||this.needReboot.indexOf(o)>=0)&&(i.target="top"),this.oxiConfig.ready.then((function(){return t()})).then((function(){return n.sendAjax(i)}))}},{key:"model",value:function(e){return this.content.page=e.model_id,this.updateNavEntryActiveState(this.content),this.content}},{key:"doPing",value:function(e){var n=this
 this.content.ping=Ember.run.later(this,(function(){return t.default.ajax({url:e.href}),n.doPing(e)}),e.timeout)}},{key:"setLoadingState",value:function(e){e&&document.activeElement.blur(),this.content.isLoading=e}},{key:"sendAjax",value:function(e){var n=this
-this.setLoadingState(!0),Ember.debug("openxpki/route - sendAjax: page = "+e.page)
+this.setLoadingState(!0),Ember.debug("openxpki/route - sendAjax: "+["page","action"].map((function(t){return e[t]?"".concat(t," = ").concat(e[t]):null})).filter((function(e){return null!==e})).join(", "))
 var r={data:G(G({},e),{},{_:(new Date).getTime()}),dataType:"json",type:e.action?"POST":"GET",url:this.oxiConfig.backendUrl}
-"POST"===r.type&&(r.data._rtoken=this.content.rtoken)
-var o=r.data.target||"self"
-return"modal"===o&&(o="popup"),"self"===o&&(o=this.content.popup?"popup":this.content.tabs.length>1?"active":"top"),this.content.refresh&&(Ember.run.cancel(this.content.refresh),this.content.refresh=null),new Ember.RSVP.Promise((function(e,i){t.default.ajax(r).then((function(t){if(n.content.status=t.status,n.content.popup=null,t.ping&&(Ember.debug('openxpki/route - sendAjax response: "ping" '+t.ping),n.content.ping&&Ember.run.cancel(n.content.ping),n.doPing(t.ping)),t.refresh&&(Ember.debug('openxpki/route - sendAjax response: "refresh" '+t.refresh.href+", "+t.refresh.timeout),n.content.refresh=Ember.run.later(n,(function(){this.sendAjax({page:t.refresh.href})}),t.refresh.timeout)),t.goto)Ember.debug('openxpki/route - sendAjax response: "goto" '+t.goto),"_blank"===t.target||/^(http|\/)/.test(t.goto)?window.location.href=t.goto:n.transitionTo("openxpki",t.goto)
-else if(t.structure)Ember.debug('openxpki/route - sendAjax response: "structure"'),n.content.navEntries=t.structure,n.updateNavEntryActiveState(),n.content.user=t.user,n.content.rtoken=t.rtoken,t.language&&(n.oxiLocale.locale=t.language)
-else{if(t.page&&t.main){Ember.debug('openxpki/route - sendAjax response: "page" and "main"')
-for(var r={active:!0,page:t.page,main:t.main,right:t.right},i=!0,a=0,l=[].concat(B(r.main||[]),B(r.right||[]));a<l.length;a++){var u=l[a]
-"form"===u.type&&(u.content.isFirstForm=i,i&&(i=!1))}if("popup"===o)n.content.popup=r
-else if("tab"===o){var c=n.content.tabs
-c.setEach("active",!1),c.pushObject(r)}else if("active"===o){var s=n.content.tabs,f=s.indexOf(s.findBy("active"))
-s.replace(f,1,[r])}else n.content.tabs=[r]}n.setLoadingState(!1)}return e(t)}),(function(){return n.setLoadingState(!1),n.content.error={message:n.intl.t("error_popup.message")},e({})}))}))}},{key:"updateNavEntryActiveState",value:function(){var e,t=this.content.page,n=function(e,t){var n
+return"POST"===r.type&&(r.data._rtoken=this.content.rtoken),this.content.refresh&&(Ember.run.cancel(this.content.refresh),this.content.refresh=null),new Ember.RSVP.Promise((function(e,o){t.default.ajax(r).then((function(t){return n.content.status=t.status,n.content.popup=null,t.ping&&(Ember.debug('openxpki/route - sendAjax response: "ping" '+t.ping),n.content.ping&&Ember.run.cancel(n.content.ping),n.doPing(t.ping)),t.refresh&&(Ember.debug('openxpki/route - sendAjax response: "refresh" '+t.refresh.href+", "+t.refresh.timeout),n.content.refresh=Ember.run.later(n,(function(){this.sendAjax({page:t.refresh.href})}),t.refresh.timeout)),t.goto?(Ember.debug('openxpki/route - sendAjax response: "goto" '+t.goto),"_blank"===t.target||/^(http|\/)/.test(t.goto)?window.location.href=t.goto:n.transitionTo("openxpki",t.goto)):t.structure?(Ember.debug('openxpki/route - sendAjax response: "structure"'),n.content.navEntries=t.structure,n.updateNavEntryActiveState(),n.content.user=t.user,n.content.rtoken=t.rtoken,t.language&&(n.oxiLocale.locale=t.language)):(t.page&&t.main&&(Ember.debug('openxpki/route - sendAjax response: "page" and "main"'),n._setPageContent(r.data.target,t.page,t.main,t.right)),n.setLoadingState(!1)),e(t)}),(function(){return n.setLoadingState(!1),n.content.error={message:n.intl.t("error_popup.message")},e({})}))}))}},{key:"_setPageContent",value:function(){for(var e=arguments.length>0&&void 0!==arguments[0]?arguments[0]:"self",t=arguments.length>1?arguments[1]:void 0,n=arguments.length>2?arguments[2]:void 0,r=arguments.length>3?arguments[3]:void 0,o={active:!0,page:t,main:n,right:r},i=!0,a=0,l=[].concat(B(o.main||[]),B(o.right||[]));a<l.length;a++){var u=l[a]
+"form"===u.type&&(u.content.isFirstForm=i,i&&(i=!1))}if("self"===e&&(e=this.content.popup?"popup":this.content.tabs.length>1?"active":"top"),"modal"===e&&(e="popup"),"popup"===e)this.content.popup=o
+else if("tab"===e){var c=this.content.tabs
+c.setEach("active",!1),c.pushObject(o)}else if("active"===e){var s=this.content.tabs,f=s.indexOf(s.findBy("active"))
+s.replace(f,1,[o])}else this.content.tabs=[o]}},{key:"updateNavEntryActiveState",value:function(){var e,t=this.content.page,n=function(e,t){var n
 if("undefined"==typeof Symbol||null==e[Symbol.iterator]){if(Array.isArray(e)||(n=N(e))||t&&e&&"number"==typeof e.length){n&&(e=n)
 var r=0,o=function(){}
 return{s:o,n:function(){return r>=e.length?{done:!0}:{done:!1,value:e[r++]}},e:function(e){throw e},f:o}}throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.")}var i,a=!0,l=!1

commit 639de83d7a61fb0ff83f918292108020709be9dd

UI: fix regression bug introduced in 5b1f12e (target handling)

core/htdocs_source/app/pods/openxpki/route.js
@@ -143,12 +143,13 @@ export default class OpenXpkiRoute extends Route {
             this.content.refresh = null;
         }
 
+        let realTarget = this._resolveTarget(req.data.target); // has to be done before "this.content.popup = null"
+
         /* eslint-disable-next-line no-unused-vars */
         return new Promise((resolve, reject) => {
             $.ajax(req).then(
                 // SUCCESS
                 doc => {
-                    // work with a copy of this.content
                     this.content.status = doc.status;
                     this.content.popup = null;
 
@@ -184,7 +185,7 @@ export default class OpenXpkiRoute extends Route {
                     else {
                         if (doc.page && doc.main) {
                             debug("openxpki/route - sendAjax response: \"page\" and \"main\"");
-                            this._setPageContent(req.data.target, doc.page, doc.main, doc.right);
+                            this._setPageContent(realTarget, doc.page, doc.main, doc.right);
                         }
                     }
                     this.setLoadingState(false);
@@ -202,7 +203,20 @@ export default class OpenXpkiRoute extends Route {
         });
     }
 
-    _setPageContent(target = 'self', page, main, right) {
+    _resolveTarget(requestTarget) {
+        let target = requestTarget || 'self';
+        // Pseudo-target "self" is transformed so new content will be shown in the
+        // currently active place: a modal popup, an active tab or on top (i.e. single hidden tab)
+        if (target === 'self') {
+            if (this.content.popup) { target = 'popup' }
+            else if (this.content.tabs.length > 1) { target = 'active' }
+            else { target = 'top' }
+        }
+        if (target === 'modal') target = 'popup'; // FIXME remove support for legacy target 'modal'
+        return target;
+    }
+
+    _setPageContent(target, page, main, right) {
         let newTab = {
             active: true,
             page: page,
@@ -219,15 +233,6 @@ export default class OpenXpkiRoute extends Route {
                 if (isFirst) isFirst = false;
             }
         }
-
-        // Pseudo-target "self" is transformed so new content will be shown in the
-        // currently active place: a modal popup, an active tab or on top (i.e. single hidden tab)
-        if (target === 'self') {
-            if (this.content.popup) { target = 'popup' }
-            else if (this.content.tabs.length > 1) { target = 'active' }
-            else { target = 'top' }
-        }
-        if (target === 'modal') target = 'popup'; // FIXME legacy naming
 
         // Popup
         if (target === "popup") {