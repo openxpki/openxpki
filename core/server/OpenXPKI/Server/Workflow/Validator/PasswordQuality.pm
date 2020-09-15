dd01e598b2a4b168ab1892e5a22908b6ca8aa79d
Jens Berthold <jens@jebecs.de>
2020-09-15 15:06:33 +0200

    Update POD

--- core/server/OpenXPKI/Server/Workflow/Validator/PasswordQuality.pm
+++ core/server/OpenXPKI/Server/Workflow/Validator/PasswordQuality.pm
@@ -21,23 +21,147 @@ OpenXPKI::Server::Workflow::Validator::PasswordQuality
 
 =head1 SYNOPSIS
 
-class: OpenXPKI::Server::Workflow::Validator::PasswordQuality
-arg:
- - $_password
-param:
-   minlen: 8
-   maxlen: 64
-   dictionary: 4
-   following: 3
+    class: OpenXPKI::Server::Workflow::Validator::PasswordQuality
+    arg:
+     - $password
+    param:
+       checks:
+        - length
+        - dict
+       minlen: 6
 
 =head1 DESCRIPTION
 
 This validator checks a password for its quality. All configuration can be done
 using the validator config file.
+
 Based on this data, the validator fails if it believes the password to be bad.
 
-Default checks to be carried out: C<common>, C<diffchars>, C<dict>, C<sequence>.
 See the L<"checks" parameter|/checks> for more information.
+
+=head2 ARGUMENT
+
+=over
+
+=item $password
+
+The password to be validated
+
+=back
+
+=head1 PARAMETERS
+
+=head2 checks
+
+Set the list of checks to be performed.
+
+Available checks:
+
+I<Default>
+
+=over
+
+=item * C<length> - Is it in the range of permitted lengths (default: 8 - 255)?
+
+=item * C<common> - Is it not a (leet speech obfuscated) known hacked password
+like "password" et similia?
+
+=item * C<diffchars> - Does it contain enough different characters?
+
+=item * C<sequence> - Is it a sequence like 12345, abcde, or qwertz?
+
+=item * C<dict> - Is it not a (reversed) dictionary word?
+
+=item * C<entropy> - Is the password entropy above a certain level?
+
+The entropy score is calculated by first detecting how many different character
+groups are used (those groups are roughly based on blocks of Unicode's Basic
+Multilingual Plane).
+
+The entropy is higher:
+
+=over
+
+=item * the more characters the password contains,
+
+=item * the less adjacent characters the password contains (i.e. "fghijkl"),
+
+=item * the more character groups the password contains,
+
+=item * the more characters a group has in total.
+
+=back
+
+=back
+
+I<Legacy checks>
+
+=over
+
+=item * C<letters> - Does it contain letters?
+
+=item * C<digits> - Does it contain digits?
+
+=item * C<specials> - Does it contain non-word characters?
+
+=item * C<mixedcase> - Does it contain both small and capital letters?
+
+=item * C<groups> - Does it contain a certain number (default: 2) of different
+character groups?
+
+=item * C<partsequence> - Does it not contain usual sequence like 12345, abcde,
+or qwertz (default sequence length to be checked is 5)?
+
+=item * C<partdict> - Does it not contain a dictionary word?
+
+=back
+
+To maintain backwards compatibility some legacy checks are enabled
+automatically depending on the presence of certain configuration parameters
+(see comments below).
+
+=head2 minlen
+
+C<length> check: set minimum password length (default: 8).
+
+=head2 maxlen
+
+C<length> check: set maxmimum password length (default: 255).
+
+=head2 dictionaries
+
+C<dict> check: a comma separated list of files where the first existing one
+is used for dictionary checks (default: /usr/dict/web2, /usr/dict/words, /usr/share/dict/words, /usr/share/dict/linux.words).
+
+=head2 mindiffchars
+
+C<diffchars> check: set minimum required different characters to avoid passwords like
+"000000000000ciao0000000" (default: 6).
+
+=head2 minentropy
+
+C<entropy> check: minimum required entropy (default: 60).
+
+=head2 groups
+
+Enable C<groups> check and set the amount of required different groups
+(default: 2).
+
+There are four groups: digits, small letters, capital letters, others.
+So C<groups> may be set to a value between 1 and 4.
+
+=head2 following
+
+Enable C<partsequence> check and set the length of the sequences that are
+searched for in the password (default: 5).
+
+E.g. a setting of C<following: 4> will complain about passwords containing
+"abcd" or "1234" or "qwer".
+
+=head2 dictionary
+
+Enable C<partdict> check and set the minimal length for dictionary words that
+are tested to occur in the password. (default: 4).
 
 =cut
 
@@ -49,72 +173,6 @@ has api_args => (
     default => sub { {} },
 );
 
-=head1 PARAMETERS
-
-=head2 checks
-
-Override the default set of executed checks.
-
-Available checks for the password:
-
-=over
-
-=item * Is it in the range of permitted lengths (default: 8 - 255)?
-
-=item * C<common> (default: enabled) - Is it not a (leet speech obfuscated) known hacked password like
-"password" et similia?
-
-=item * C<diffchars> (default: enabled) - Does it contain enough different characters?
-
-=item * C<dict> (default: enabled) - Is it not a (reversed) dictionary word?
-
-=item * C<sequence> (default: enabled) - Is it a sequence like 12345, abcde, or qwertz?
-
-=item * C<digits> (default: disabled) - Does it contain digits?
-
-=item * C<letters> (default: disabled) - Does it contain letters?
-
-=item * C<mixedcase> (default: disabled) - Does it contain both small and capital letters?
-
-=item * C<specials> (default: disabled) - Does it contain non-word characters?
-
-=item * C<groups> (default: disabled) - Does it contain a certain number (default: 2) of different character groups?
-
-=item * C<partdict> (default: disabled) - Does it not contain a dictionary word?
-
-=item * C<partsequence> (default: enabled) - Does it not contain usual sequence like 12345, abcde, or
-qwertz (default sequence length to be checked is 5)?
-
-=back
-
-=head2 minlen
-
-Set minimum password length (default: 8).
-
-=head2 maxlen
-
-Set maxmimum password length (default: 255).
-
-=head2 mindiffchars
-
-Enables check C<diffchars> and sets minimum required different characters to
-avoid passwords like "000000000000ciao0000000" (default: 6).
-
-=head2 following
-
-Enables the check C<partsequence> and sets the the length of the
-sequence that are searched for in the password (default: 5).
-
-E.g. settings 'following: 4' will complain about passwords containing "abcd" or "1234" or "qwer".
-
-=head2 groups
-
-Enables the check C<groups> and sets the amount of required different groups (default: 2).
-
-There are four groups: digits, small letters, capital letters, others.
-So C<groups> may be set to a value between 1 and 4.
-
-=cut
 
 # called by Workflow::Validator->init()
 sub _init {
