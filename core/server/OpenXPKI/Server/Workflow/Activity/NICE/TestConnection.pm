f857baace21b146b5fe815584beb3f10903dd2dd
Oliver Welter <github@oliwel.de>
2020-10-14 13:13:47 +0200

    Some more error handling for ConnectionTest

--- core/server/OpenXPKI/Server/Workflow/Activity/NICE/TestConnection.pm
+++ core/server/OpenXPKI/Server/Workflow/Activity/NICE/TestConnection.pm
@@ -3,6 +3,8 @@ package OpenXPKI::Server::Workflow::Activity::NICE::TestConnection;
 use strict;
 use base qw( OpenXPKI::Server::Workflow::Activity );
 
+use English;
+
 use OpenXPKI::Server::Context qw( CTX );
 use OpenXPKI::Exception;
 use OpenXPKI::Debug;
@@ -19,14 +21,26 @@ sub execute {
 
     my $nice_backend = OpenXPKI::Server::NICE::Factory->getHandler( $self );
 
-    my $response = $nice_backend->testConnection();
+    my $set_context;
+    eval{
+        $set_context = $nice_backend->testConnection();
+    };
 
-    foreach my $key (keys %{$response}) {
-        my $val = $response->{$key};
-        ##! 64: 'Set key ' . $key . ' to ' . $val
-        $context->param( $key => $val  );
+    my $error;
+    if ($EVAL_ERROR) {
+        $error = 'I18N_OPENXPKI_UI_NICE_BACKEND_ERROR';
+    } elsif(!$set_context) {
+        $error = $nice_backend->get_last_error() || 'I18N_OPENXPKI_UI_NICE_BACKEND_ERROR';
     }
 
+    $context->param( 'error_code' => $error );
+
+    ##! 64: 'Setting Context ' . Dumper $set_context
+    for my $key (keys %{$set_context} ) {
+        my $value = $set_context->{$key};
+        ##! 64: "Set key: $key to value $value";
+        $context->param( { $key => $value } );
+    }
     return 1;
 }
 
