8c2ce723c0217ddcdc72f12cf96e3c7c8c7359e1
mjungmair@whiterabbitsecurity.com <jungmair@in.tum.de>
2020-10-22 10:24:52 +0200

    add argon2 to Password::check and remove redundancies

--- core/server/OpenXPKI/Password.pm
+++ core/server/OpenXPKI/Password.pm
@@ -18,6 +18,7 @@ use POSIX;
 sub hash {
     my $scheme=shift;
     my $passwd=shift;
+    my $encrypted=shift;
     my $prefix = sprintf '{%s}', $scheme;
     my $computed_secret;
     if ($scheme eq 'sha') {
@@ -32,20 +33,21 @@ sub hash {
 
     } elsif ($scheme eq 'ssha') {
         my $ctx = Digest::SHA->new();
-        my $salt = __create_salt(3);
+        my $salt = $encrypted ? substr(decode_base64($encrypted), 20) : __create_salt(3);
         $ctx->add($passwd);
         $ctx->add($salt);
         $computed_secret = encode_base64( $ctx->digest() . $salt, '');
 
     } elsif ($scheme eq 'smd5') {
         my $ctx = Digest::MD5->new();
-        my $salt = __create_salt(3);
+        my $salt = $encrypted ? substr(decode_base64($encrypted), 16) : __create_salt(3);
         $ctx->add($passwd);
         $ctx->add($salt);
         $computed_secret = encode_base64($ctx->digest() . $salt, '');
 
     } elsif ($scheme eq 'crypt') {
-        $computed_secret = crypt($passwd, __create_salt(3));
+        my $salt = $encrypted ? $encrypted : __create_salt(3);
+        $computed_secret = crypt($passwd, $salt);
 
     } elsif ($scheme eq 'argon2') {
         $computed_secret = Crypt::Argon2::argon2id_pass($passwd, __create_salt(16), 3, '32M', 1, 16);
@@ -62,11 +64,16 @@ sub check {
     my $hash=shift;
     my $encrypted;
     my $scheme;
+
     if ($hash =~ m{ \{ (\w+) \} (.*) }xms) {
-        ##! 8: "database uses RFC2307 password syntax"
+        # handle common case: RFC2307 password syntax
         $scheme = lc($1);
         $encrypted = $2;
+    }elsif( rindex($hash, "\$argon2id", 0) ==0 ){
+        # handle special case of argon2
+        return Crypt::Argon2::argon2id_verify($hash,$passwd);
     }else{
+        # digest is not recognized
         OpenXPKI::Exception->throw (
             message => "Given digest is without scheme",
             params  => {},
@@ -86,37 +93,10 @@ sub check {
                 facility => 'system',
         });
     }
+    
+    my $computed_hash = hash($scheme,$passwd,$encrypted);
 
-    my ($computed_secret, $salt);
-    if ($scheme eq 'sha') {
-        my $ctx = Digest::SHA->new();
-        $ctx->add($passwd);
-        $computed_secret = $ctx->b64digest();
-    }
-    if ($scheme eq 'ssha') {
-        $salt = substr(decode_base64($encrypted), 20);
-        my $ctx = Digest::SHA->new();
-        $ctx->add($passwd);
-        $ctx->add($salt);
-        $computed_secret = encode_base64($ctx->digest() . $salt, '');
-    }
-    if ($scheme eq 'md5') {
-        my $ctx = Digest::MD5->new();
-        $ctx->add($passwd);
-        $computed_secret = $ctx->b64digest();
-    }
-    if ($scheme eq 'smd5') {
-        $salt = substr(decode_base64($encrypted), 16);
-        my $ctx = Digest::MD5->new();
-        $ctx->add($passwd);
-        $ctx->add($salt);
-        $computed_secret = encode_base64($ctx->digest() . $salt, '');
-    }
-    if ($scheme eq 'crypt') {
-        $computed_secret = crypt($passwd, $encrypted);
-    }
-
-    if (! defined $computed_secret) {
+    if (! defined $computed_hash) {
         OpenXPKI::Exception->throw (
             message => "Unable to check password against hash",
             params  => {
@@ -126,9 +106,9 @@ sub check {
     }
 
     ##! 2: "ident user ::= $account and digest ::= $computed_secret"
-    $computed_secret =~ s{ =+ \z }{}xms;
-    $encrypted       =~ s{ =+ \z }{}xms;
-    return $computed_secret eq $encrypted;
+    $computed_hash =~ s{ =+ \z }{}xms;
+    $hash       =~ s{ =+ \z }{}xms;
+    return $computed_hash eq $hash;
     
 }
 sub __create_salt {
@@ -156,18 +136,18 @@ Provides utility functions for hashing passwords and checking passwords against 
 
 hashes a password according to the provided scheme.
 
-SCHEME is one of sha (SHA1), md5 (MD5), crypt (Unix crypt), smd5 (salted
-MD5), ssha (salted SHA1) or argon2.
+SCHEME is one of sha (SHA1), md5 (MD5), crypt (Unix crypt), smd5 (encrypted
+MD5), ssha (encrypted SHA1) or argon2.
 
-It returns a hash in the format C<{SCHEME}encrypted_string> or undef if no hash could be computed
+It returns a hash in the format C<{SCHEME}encrypted_string>, C<$argon...> or undef if no hash could be computed
 
 =head2 check
 
 checks if a password matches the provided digest.
-The digest must have the format: C<{SCHEME}encrypted_string>
+The digest must have the format: C<{SCHEME}encrypted_string> or must start with C<$argon2>
 
-SCHEME is one of sha (SHA1), md5 (MD5), crypt (Unix crypt), smd5 (salted
-MD5) or ssha (salted SHA1).
+SCHEME is one of sha (SHA1), md5 (MD5), crypt (Unix crypt), smd5 (encrypted
+MD5) or ssha (encrypted SHA1).
 
 It returns 1 if the password matches the digest, 0 otherwise.
 
