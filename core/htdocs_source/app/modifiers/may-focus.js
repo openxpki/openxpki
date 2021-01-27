2d39a0975579eeeb1803de9587bebbb7f6217619
Jens Berthold <jens@jebecs.de>
2021-01-27 03:10:40 +0100

    Fix {{on-init}} and {{may-focus}} to only trigger on DOM insertion

--- core/htdocs_source/app/modifiers/may-focus.js
+++ core/htdocs_source/app/modifiers/may-focus.js
@@ -1,9 +1,13 @@
-import { modifier } from 'ember-modifier';
+import Modifier from 'ember-modifier';
 
-export default modifier((element, [emberObject, mayFocus]) => {
-    // note: we don't use an Ember loading substate here as this would
-    // lead to a disruptive UX on every page (route) switch
-    if (!emberObject) throw new Error("{{may-focus}}: no Ember component passed (maybe there is no corresponding component.js or controller.js to the template that called this modifier?), DOM element: " + element.outerHTML.replace(/[\s\n]+/g, " "));
-    if (!emberObject.args.setFocusInfo) throw new Error("{{may-focus}}: Ember component did not get passed a setFocusInfo() method from it's parent, DOM element: " + element.outerHTML.replace(/[\s\n]+/g, " "));
-    emberObject.args.setFocusInfo(mayFocus, element);
-});
+export default class MayFocusModifier extends Modifier {
+  /*
+   * Lifecycle hooks
+   */
+  didInstall() {
+    let [emberObject, mayFocus] = this.args.positional;
+    if (!emberObject) throw new Error("{{may-focus}}: no Ember component passed (maybe there is no corresponding component.js or controller.js to the template that called this modifier?), DOM element: " + this.element.outerHTML.replace(/[\s\n]+/g, " "));
+    if (!emberObject.args.setFocusInfo) throw new Error("{{may-focus}}: Ember component did not get passed a setFocusInfo() method from it's parent, DOM element: " + this.element.outerHTML.replace(/[\s\n]+/g, " "));
+    emberObject.args.setFocusInfo(mayFocus, this.element);
+  }
+}
--- core/htdocs_source/app/modifiers/on-init.js
+++ core/htdocs_source/app/modifiers/on-init.js
@@ -1,8 +1,14 @@
-import { modifier } from 'ember-modifier';
+import Modifier from 'ember-modifier';
 import { debug } from '@ember/debug';
 
-export default modifier((element, [registerFunction]) => {
-    if (typeof registerFunction !== 'function') throw new Error("{{on-init}}: First argument needs to be an Ember action, DOM element: " + element.outerHTML.replace(/[\s\n]+/g, " "));
+export default class OnInitModifier extends Modifier {
+  /*
+   * Lifecycle hooks
+   */
+  didInstall() {
+    let [registerFunction] = this.args.positional;
+    if (typeof registerFunction !== 'function') throw new Error("{{on-init}}: First argument needs to be an Ember action, DOM element: " + this.element.outerHTML.replace(/[\s\n]+/g, " "));
     //debug(`{{on-init}}: ${element.outerHTML.replace(/[\s\n]+/g, " ")}`);
-    registerFunction(element);
-});
+    registerFunction(this.element);
+  }
+}
