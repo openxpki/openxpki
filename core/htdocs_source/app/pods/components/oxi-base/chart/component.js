88b6b44b3f7d3d31438a2cdbd6515b18facaaae0
Jens Berthold <jens@jebecs.de>
2021-01-29 15:16:05 +0100

    Use DD.MM.YYYY format and 24 hours in charts

--- core/htdocs_source/app/pods/components/oxi-base/chart/component.js
+++ core/htdocs_source/app/pods/components/oxi-base/chart/component.js
@@ -79,7 +79,10 @@ export default class OxiChartComponent extends Component {
                 {}, // x values
             ],
             axes: [
-                {}, // x axis
+                {
+                    time: true,
+                    values: this.axisTimestampConfig(),
+                }, // x axis
             ],
         };
 
@@ -101,6 +104,7 @@ export default class OxiChartComponent extends Component {
                 scales: {
                     x: {
                         time: false,
+                        values: undefined,
                     },
                 },
                 plugins: [
@@ -227,5 +231,46 @@ export default class OxiChartComponent extends Component {
         } else {
             throw new Error(`Color ${cssColor} could not be parsed.`);
         }
+    }
+
+    // From https://github.com/leeoniya/uPlot/blob/1.6.3/src/opts.js#L65
+    axisTimestampConfig() {
+        const ms = 1e-3; // 1e-3 = set up for seconds, 1 = set up for milliseconds
+
+        const NL = "\n";
+
+        const yyyy    = "{YYYY}";
+        const NLyyyy  = NL + yyyy;
+        const md      = "{DD}.{MM}";
+        const NLmd    = NL + md;
+        const NLmdyy  = NL + "{DD}.{MM}.{YY}";
+
+        const hmm     = "{H}:{mm}";
+        const NLhmm = NL + hmm;
+        const ss      = ":{ss}";
+
+        const _ = null;
+
+        const s  = ms * 1e3,
+              m  = s  * 60,
+              h  = m  * 60,
+              d  = h  * 24,
+              mo = d  * 30,
+              y  = d  * 365;
+
+        // [0]:   minimum num secs in the tick incr
+        // [1]:   default tick format
+        // [2-7]: rollover tick formats
+        // [8]:   mode: 0: replace [1] -> [2-7], 1: concat [1] + [2-7]
+        return [
+        //   tick incr    default          year                    month   day                   hour    min       sec   mode
+            [y,           yyyy,            _,                      _,      _,                    _,      _,        _,       1],
+            [d * 28,      "{MMM}",         NLyyyy,                 _,      _,                    _,      _,        _,       1],
+            [d,           md,              NLyyyy,                 _,      _,                    _,      _,        _,       1],
+            [h,           "{H}",           NLmdyy,                 _,      NLmd,                 _,      _,        _,       1],
+            [m,           hmm,             NLmdyy,                 _,      NLmd,                 _,      _,        _,       1],
+            [s,           ss,              NLmdyy + " " + hmm,     _,      NLmd + " " + hmm,     _,      NLhmm,    _,       1],
+            [ms,          ss + ".{fff}",   NLmdyy + " " + hmm,     _,      NLmd + " " + hmm,     _,      NLhmm,    _,       1],
+        ];
     }
 }
