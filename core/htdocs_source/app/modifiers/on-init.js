d1a3ab01a779b36d093a2672a76016fb3da803c3
Jens Berthold <jens@jebecs.de>
2021-07-06 14:57:47 +0200

    UI: helper {{on-init}} now supports additional parameters

--- core/htdocs_source/app/modifiers/on-init.js
+++ core/htdocs_source/app/modifiers/on-init.js
@@ -6,9 +6,9 @@ export default class OnInitModifier extends Modifier {
    * Lifecycle hooks
    */
   didInstall() {
-    let [registerFunction] = this.args.positional;
+    let [registerFunction, ...params] = this.args.positional;
     if (typeof registerFunction !== 'function') throw new Error("{{on-init}}: First argument needs to be an Ember action, DOM element: " + this.element.outerHTML.replace(/[\s\n]+/g, " "));
-    //debug(`{{on-init}}: ${element.outerHTML.replace(/[\s\n]+/g, " ")}`);
-    registerFunction(this.element);
+    //debug(`{{on-init}}: ${element.outerHTML.replace(/[\s\n]+/g, " ")}`, ...params);
+    registerFunction(this.element, ...params);
   }
 }
