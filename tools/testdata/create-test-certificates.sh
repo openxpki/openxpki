09a7e43eb45e9084fa09023a796a366c78fd3646
Jens Berthold <jens@jebecs.de>
2021-08-12 13:09:38 +0200

    Support MariaDB socket connection in create-test-certificates.sh

--- tools/testdata/create-test-certificates.sh
+++ tools/testdata/create-test-certificates.sh
@@ -3,6 +3,9 @@
 # Create test certificates and import them into OpenXPKI database for later
 # use in test code / environment.
 #
+# This script expects to be run in a test environment (Docker or Vagrant VM)
+# where the database configuration is set in OXI_TEST_DB_MYSQL_USER etc.
+#
 # The MySQL database should be freshly created, i.e. without any data in it:
 #   openxpkictl stop
 #   tools/testenv/mysql-create-db.sh && tools/testenv/mysql-create-schema.sh
@@ -14,6 +17,12 @@
 # The test certificates only need to be regenerated if there are some major
 # changes to the project (like certificate algorithm, database schema change etc.)
 #
+if [[ -z "$OXI_TEST_DB_MYSQL_USER" ]]; then
+    echo "This script expects to be run in a test environment (Docker or Vagrant VM)"
+    echo "where the database configuration is set in OXI_TEST_DB_MYSQL_USER etc."
+    exit 1
+fi
+
 BASEDIR=$(readlink -e $(dirname $0))
 TEMPDIR=$(mktemp -d)
 
@@ -26,7 +35,7 @@ $BASEDIR/_create-config.pl $TEMPDIR
 
 # 2. Create certificates and store them in $TEMPDIR/etc/openxpki/ca/xxx/
 # 3. Import cert. into MySQL using test config above
-$BASEDIR/_create-certs.sh  $TEMPDIR/etc/openxpki
+$BASEDIR/_create-certs.sh $TEMPDIR/etc/openxpki
 
 # 4. Create Perl code for OpenXPKI::Test::CertHelper::Database
 $BASEDIR/_write-certhelper.pl $TEMPDIR/etc/openxpki/ca > $BASEDIR/generated-certhelper-code.pl
@@ -35,7 +44,13 @@ echo "Code for OpenXPKI::Test::CertHelper::Database is available in:"
 echo "    $BASEDIR/generated-certhelper-code.pl"
 
 # 5. Create MySQL dump
-mysqldump -h $OXI_TEST_DB_MYSQL_DBHOST -u $OXI_TEST_DB_MYSQL_USER -p"$OXI_TEST_DB_MYSQL_PASSWORD" \
+dumpOpts=()
+test ! -z "$OXI_TEST_DB_MYSQL_DBHOST" && dumpOpts=("${dumpOpts[@]}" -h "$OXI_TEST_DB_MYSQL_DBHOST") || true
+test ! -z "$OXI_TEST_DB_MYSQL_DBPORT" && dumpOpts=("${dumpOpts[@]}" -P "$OXI_TEST_DB_MYSQL_DBPORT") || true
+dumpOpts=("${dumpOpts[@]}" -u"$OXI_TEST_DB_MYSQL_USER")
+test ! -z "$OXI_TEST_DB_MYSQL_PASSWORD" && dumpOpts=("${dumpOpts[@]}" -p"$OXI_TEST_DB_MYSQL_PASSWORD") || true
+
+mysqldump "${dumpOpts[@]}" \
     --set-charset --no-create-info \
     --skip-comments --skip-add-locks --skip-disable-keys \
     --extended-insert --complete-insert --single-transaction \
