6466598c3022e46947c2d06579fd3c25c8dde2d5
Jens Berthold <jens@jebecs.de>
2021-09-15 15:41:18 +0200

    bs4: fix layout when combining basic form buttons and ButtonContainer
    <OxiBase::ButtonContainer> now allows to prepend custom buttons

--- core/htdocs_source/app/pods/test/controller.js
+++ core/htdocs_source/app/pods/test/controller.js
@@ -314,6 +314,7 @@ export default class TestController extends Controller {
                             label: "Really sure?",
                             description: "Think about it one more time.",
                         },
+                        break_before: 1,
                     },
                     {
                         ...this._testButton,
