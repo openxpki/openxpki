24de39b152a684be12b35b503c7b5f71914e7699
Jens Berthold <jens@jebecs.de>
2021-10-26 16:29:51 +0200

    UI: use separate method for page bootstrapping request

--- core/htdocs_source/app/pods/openxpki/route.js
+++ core/htdocs_source/app/pods/openxpki/route.js
@@ -20,7 +20,7 @@ export default class OpenXpkiRoute extends Route {
         limit:    { refreshModel: true },
         force:    { refreshModel: true },
     };
-    needsBootstrap = ["login", "login!logout", "welcome"];
+    topTarget = ["login", "login!logout", "welcome"];
 
     /*
     Custom handlers for exceptions returned by the server (HTTP status codes):
@@ -48,20 +48,19 @@ export default class OpenXpkiRoute extends Route {
         let structureIfNeeded; // chain of ajax calls via Promises
 
         /*
-         * load base page structure first first time or for special pages ("needsBootstrap")
+         * load base page structure first first time or for special pages ("topTarget")
          */
-        if (!this.content.navEntries.length || this.needsBootstrap.indexOf(modelId) >= 0) {
+
+        if (!this.content.navEntries.length || this.topTarget.indexOf(modelId) >= 0) {
             // don't send request yet, only create a lambda via arrow function expression
             structureIfNeeded = () => {
-                return this.content.updateRequest({
-                    page: "bootstrap!structure",
-                    baseurl: window.location.pathname,
-                });
+                return this.content.bootstrap();
             };
         }
         else {
             structureIfNeeded = () => { Promise.resolve() };
         }
+//        structureIfNeeded = () => { Promise.resolve() };
 
         /*
          * load requested page part
@@ -72,9 +71,9 @@ export default class OpenXpkiRoute extends Route {
         if (queryParams.limit) { request.limit = queryParams.limit }
         if (queryParams.startat) { request.startat = queryParams.startat }
 
-        // load as top content if 'modelId' is part of navigation or in 'needsBootstrap' list
+        // load as top content if 'modelId' is part of navigation or in 'topTarget' list
         let flatList = this.content.navEntries.reduce((p, n) => p.concat(n, n.entries || []), []);
-        if (flatList.findBy("key", modelId) || this.needsBootstrap.indexOf(modelId) >= 0) {
+        if (flatList.findBy("key", modelId) || this.topTarget.indexOf(modelId) >= 0) {
             request.target = "top";
         }
         return this.config.ready // localconfig.js might change rootURL, so first thing is to query it
