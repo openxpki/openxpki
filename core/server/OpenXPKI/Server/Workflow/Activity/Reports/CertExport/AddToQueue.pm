b0c3bffc2385efe77b91242312cdafeffb54f2c2
Oliver Welter <owelter@whiterabbitsecurity.com>
2022-02-01 12:12:55 +0100

    Remove unsupported XML Export workflow and unused CPAN classes

--- core/server/OpenXPKI/Server/Workflow/Activity/Reports/CertExport/AddToQueue.pm
+++ core/server/OpenXPKI/Server/Workflow/Activity/Reports/CertExport/AddToQueue.pm
@@ -1,73 +1,10 @@
 package OpenXPKI::Server::Workflow::Activity::Reports::CertExport::AddToQueue;
 
+# This class is no longer supported, it stays here to not break existing
+# configs having the old unsupported export workflow installed
+# which was the default in old default installs
 
 use strict;
 use base qw( OpenXPKI::Server::Workflow::Activity );
 
-use OpenXPKI::Server::Context qw( CTX );
-use OpenXPKI::Exception;
-use OpenXPKI::Debug;
-use English;
-use OpenXPKI::Serialization::Simple;
-use Data::Dumper;
-
-sub execute {
-
-    ##! 1: 'execute'
-
-    my $self       = shift;
-    my $workflow   = shift;
-    my $context = $workflow->context();
-
-    my $ser  = OpenXPKI::Serialization::Simple->new();
-
-    my $namespace = $self->param('namespace');
-    $namespace  = 'certificate.export.default' unless($namespace);
-
-    ##! 16: 'namespace: ' . $namespace
-
-    my @attrs = split /,/, $self->param('attribs');
-    my $export = {};
-    foreach my $key (@attrs) {
-       $export->{$key} = $self->param($key) || '';
-    }
-
-    CTX('log')->application()->info("prepare cert ".$context->param( 'cert_identifier' )." for export");
-
-
-    ##! 16: 'Mapping attributes: ' . Dumper $export
-
-    CTX('api2')->set_data_pool_entry(
-        'namespace' => $namespace,
-        'key' => $context->param( 'cert_identifier' ),
-        'value' => $ser->serialize( $export ),
-        'force' => 1
-    );
-
-    return 1;
-
-}
-
 1;
-
-
-=head1 NAME
-
-OpenXPKI::Server::Workflow::Activity::Reports::CertExport::AddToQueue;
-
-=head1 Description
-
-Add an entry to the export queue in the datapool
-
-=head1 Configuration
-
-    <action name="add_to_queue" class="OpenXPKI::Server::Workflow::Activity::Reports::CertExport::AddToQueue"
-         namespace="certificate.export.default"
-         _map_certType="enc"
-         _map_email="$email"/>
-
-The namespace parameter is used as the datapool namespace, the datapool key is
-set to the certificate identifier. Any parameter starting with I<_map_> is
-added to the datapool as attribute for the exporter. Mapped parameters starting
-with a $ are treated as context keys.
-
--- core/server/OpenXPKI/Server/Workflow/Activity/Reports/CertExport/TagAsExported.pm
+++ core/server/OpenXPKI/Server/Workflow/Activity/Reports/CertExport/TagAsExported.pm
@@ -1,58 +1,10 @@
 package OpenXPKI::Server::Workflow::Activity::Reports::CertExport::TagAsExported;
 
+# This class is no longer supported, it stays here to not break existing
+# configs having the old unsupported export workflow installed
+# which was the default in old default installs
 
 use strict;
 use base qw( OpenXPKI::Server::Workflow::Activity );
 
-use OpenXPKI::Server::Context qw( CTX );
-use OpenXPKI::Exception;
-use OpenXPKI::Debug;
-use English;
-use OpenXPKI::Serialization::Simple;
-use Data::Dumper;
-
-sub execute {
-
-    ##! 1: 'execute'
-
-    my $self       = shift;
-    my $workflow   = shift;
-    my $context = $workflow->context();
-
-    my $ser = OpenXPKI::Serialization::Simple->new();
-
-    my $exported_cert_ids = $context->param( 'exported_cert_ids' );
-    if (! $exported_cert_ids) {
-        return 1;
-    }
-
-    foreach my $cert_identifier (@{ $ser->deserialize( $exported_cert_ids ) }) {
-        CTX('log')->application()->info('remove '.$cert_identifier.' from export queue');
-        # set without value deletes the entry
-        CTX('api2')->delete_data_pool_entry(
-            'namespace' => $context->param( 'queue_namespace' ),
-            'key' => $cert_identifier
-        );
-    }
-
-    $context->param( 'exported_cert_ids', '' );
-    $context->param( 'xml_filename' ,  '');
-    $context->param( 'xml_targetname' ,  '' );
-
-    return 1;
-
-}
-
 1;
-
-
-=head1 Name
-
-OpenXPKI::Server::Workflow::Activity::Reports::CertExport::TagAsExported
-
-=head1 Description
-
-Cleanup activity for the export from
-OpenXPKI::Server::Workflow::Activity::Reports::CertExport::GenerateExportFile
-removes the export flag entries from the datapool and clears the temporary
-values from the context.
