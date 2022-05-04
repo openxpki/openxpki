f2023353b68beb8e22604bb330759eb8f7f6e6a5
Jens Berthold <jens@jebecs.de>
2022-05-04 13:14:37 +0200

    UI: warn when using old browser (> 2 years)

--- core/htdocs_source/app/pods/openxpki/controller.js
+++ core/htdocs_source/app/pods/openxpki/controller.js
@@ -3,6 +3,8 @@ import { tracked } from '@glimmer/tracking';
 import { action, computed, set } from '@ember/object';
 import { gt } from '@ember/object/computed';
 import { inject } from '@ember/service';
+import lite from 'caniuse-lite';
+import { detect } from 'detect-browser'
 
 export default class OpenXpkiController extends Controller {
     @inject('oxi-config') config;
@@ -41,6 +43,56 @@ export default class OpenXpkiController extends Controller {
     get statusHidden() {
         let message = this.get("model.status.message");
         return !message;
+    }
+
+    get oldBrowser() {
+        const old_age = 2 * 365
+
+        // map detect-browser names to caniuse names
+        const map = {
+            'edge' : 'edge',
+            'edge-ios': 'ios_saf',
+            'samsung': 'samsung',
+            'edge-chromium': 'edge',
+            'chrome': 'chrome',
+            'firefox': 'firefox',
+            'opera-mini': 'op_mini',
+            'opera': 'opera',
+            'ie': 'ie',
+            'bb10': 'bb',
+            'android': 'android',
+            'ios': 'ios_saf',
+            'safari': 'safari',
+        }
+
+        const browser = detect()
+        if (!browser) return
+
+        // translate 'detect-browser' name to 'caniuse' name
+        let name = map[browser.name] || browser.name
+        if (name == 'chrome' && browser.os.match(/android/i)) name = 'and_chr'
+        if (name == 'firefox' && browser.os.match(/android/i)) name = 'and_ff'
+
+        // look if 'caniuse' knows this browser
+        let agent = lite.agents[name]
+        if (!agent) return
+
+        // look if 'caniuse' knows this version
+        let version = browser.version
+        const known_version = Object.keys(agent.release_date).find(v => version.match(new RegExp(`^${v}(\\.|$)`)))
+        if (!known_version) return
+
+        // calculate browser age (release date)
+        let release_date = agent.release_date[known_version]
+        if (!release_date) return
+
+        let now = parseInt(new Date() / 1000)
+        let age = parseInt((now - release_date) / (60*60*24))
+
+        if (age < old_age) return
+
+        console.info(`Detected browser "${agent.browser} ${known_version}" is ${age} days old (max. supported browser age: ${old_age} days)`)
+        return `${agent.browser} ${known_version}`
     }
 
     @gt("model.tabs.length", 1) showTabs;
