84537499818966782a090830312cab0b3c776685
Jens Berthold <jens@jebecs.de>
2022-05-04 13:20:03 +0200

    UI: change max. age of supported target browsers (JS compilation) to ~2 years

--- core/htdocs_source/config/targets.js
+++ core/htdocs_source/config/targets.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const browsers = [
-  'last 1 Chrome versions',
-  'last 1 Firefox versions',
-  'last 1 Safari versions',
+  'last 20 Chrome versions',
+  'last 20 Firefox versions',
+  'last 3 Safari versions',
   'ie 11',
 ];
 
