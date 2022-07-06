e95dd9651bd24a59f21a43a2d9b50ff1bf9d52e7
Jens Berthold <jens@jebecs.de>
2022-07-06 13:37:29 +0200

    Fix installation of VBoxGuestAdditions in Vagrant box "develop"

--- vagrant/develop/assets/provision-basic.sh
+++ vagrant/develop/assets/provision-basic.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -euo pipefail
+
 # Basic Vagrant Box setup
 
 . /vagrant/assets/functions.sh
@@ -16,6 +18,23 @@ $(grep -q OXI_TEST_SAMPLECONFIG_DIR /etc/environment) || echo "OXI_TEST_SAMPLECO
 
 # Read our configuration and the one written by previous (DB) provisioning scripts
 while read def; do export $def; done < /etc/environment
+
+#
+# Install some requirements (also for VBoxGuestAdditions)
+#
+echo "Apt - update package list"
+apt-get update >$LOG 2>&1
+echo "Apt - upgrade"
+apt-get upgrade --assume-yes >$LOG 2>&1
+# libzip-dev - for Net::SSLeay
+# libexpat1-dev - for XML::Parser
+# linux-headers-amd64 - required to compile guest addons using "vagrant vbguest" (on the host)
+install_packages mc rsync gettext \
+  apache2 libapache2-mod-fcgid \
+  libssl-dev libzip-dev libexpat1-dev \
+  libtest-deep-perl libtest-exception-perl \
+  linux-headers-amd64 \
+  mc
 
 #
 # Install Virtualbox guest addins
@@ -36,33 +55,17 @@ if [ $install_vbox -eq 1 ]; then
     echo "VBoxGuestAdditions - remove old version"
     apt-get -q=2 remove virtualbox-guest-utils >$LOG 2>&1 || echo
     echo "VBoxGuestAdditions - install new version $VBOX"
-    set -e
     cd /tmp
     wget -q http://download.virtualbox.org/virtualbox/$VBOX/VBoxGuestAdditions_$VBOX.iso >$LOG 2>&1
     mount VBoxGuestAdditions_$VBOX.iso -o loop /mnt                   >$LOG 2>&1
-    set +e
     sh /mnt/VBoxLinuxAdditions.run --nox11 -- --force                 >$LOG 2>&1
     umount /mnt
     rm VBoxGuestAdditions_$VBOX.iso
 fi
 
 #
-# Install some requirements
+# Install Git
 #
-echo "Apt - update package list"
-apt-get update >$LOG 2>&1
-echo "Apt - upgrade"
-apt-get upgrade --assume-yes >$LOG 2>&1
-# libzip-dev - for Net::SSLeay
-# libexpat1-dev - for XML::Parser
-# linux-headers-amd64 - required to compile guest addons using "vagrant vbguest" (on the host)
-install_packages mc rsync gettext \
-  apache2 libapache2-mod-fcgid \
-  libssl-dev libzip-dev libexpat1-dev \
-  libtest-deep-perl libtest-exception-perl \
-  linux-headers-amd64 \
-  mc
-
 if ! command -v git >/dev/null; then
     echo "Git"
     apt-get install -q=2 -t $(lsb_release -sc)-backports git >$LOG 2>&1
@@ -89,5 +92,3 @@ if ! $(grep -q $tools_dir /root/.bashrc); then
     echo "export PATH=$PATH:$tools_dir" >> /home/vagrant/.profile
     echo "$tools_dir/oxi-help"          >> /home/vagrant/.profile
 fi
-
-set +e
