6569f3f39aae673ed6bc47b49b137d21cd26380e
Jens Berthold <jens@jebecs.de>
2022-07-11 18:41:37 +0200

    UI: don't use .set() in Ember Octane class

--- core/htdocs_source/app/pods/openxpki/controller.js
+++ core/htdocs_source/app/pods/openxpki/controller.js
@@ -140,7 +140,7 @@ export default class OpenXpkiController extends Controller {
 
     @action
     clearPopupData() {
-        return this.set("model.popup", null);
+        return this.model.popup = null
     }
 
     @action
