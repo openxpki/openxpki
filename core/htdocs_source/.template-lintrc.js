7835b52f3594016d129598f722ba0e5ab7223a34
Jens Berthold <jens@jebecs.de>
2022-07-11 22:00:33 +0200

    UI: configure template linter to allow HTML comments

--- core/htdocs_source/.template-lintrc.js
+++ core/htdocs_source/.template-lintrc.js
@@ -3,6 +3,9 @@ module.exports = {
   extends: 'recommended',
 
   rules: {
+    // complain about bare strings
     'no-bare-strings': true,
+    // don't complain about HTML comments
+    'no-html-comments': 'off',
   }
 };
