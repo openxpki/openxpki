374cef68abb2eff3275e68f84f198171ce0db5a5
Jens Berthold <jens@jebecs.de>
2022-07-12 00:13:18 +0200

    UI: configure template linter to allow input fields without labels

--- core/htdocs_source/.template-lintrc.js
+++ core/htdocs_source/.template-lintrc.js
@@ -7,5 +7,7 @@ module.exports = {
     'no-bare-strings': true,
     // don't complain about HTML comments
     'no-html-comments': 'off',
+    // Our complex form system does not allow for input labels everywhere
+    'require-input-label': 'off',
   }
-};
+}
