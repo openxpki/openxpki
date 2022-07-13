c2ce96243d27539af9191eddc12bca632fc1e833
Jens Berthold <jens@jebecs.de>
2022-07-13 16:21:47 +0200

    UI: fix {{html}} helper to correctly output 'number' and 'boolean' types

--- core/htdocs_source/app/helpers/html.js
+++ core/htdocs_source/app/helpers/html.js
@@ -13,8 +13,10 @@ import { htmlSafe } from '@ember/template'
 export default class Html extends Helper {
     compute([html]) {
         if (html === null || html === undefined) return ""
-        if (typeof html !== 'string') return `[${typeof html}]`
 
-        return htmlSafe(html)
+        let type = typeof html
+        if (type == 'string' || type == 'number' || type == 'boolean') return htmlSafe(html)
+
+        return `[${typeof html}]`
     }
 }
