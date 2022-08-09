36e01c54d303bd43b5ec1017cd2bafcd50d00b2e
Jens Berthold <jens@jebecs.de>
2022-08-09 19:59:56 +0200

    UI: configure tests to use `chromium` instead of `Chrome`

--- core/htdocs_source/testem.js
+++ core/htdocs_source/testem.js
@@ -3,13 +3,13 @@
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
-  launch_in_ci: ['Chrome'],
-  launch_in_dev: ['Chrome'],
+  launch_in_ci: ['chromium'],
+  launch_in_dev: ['chromium'],
   browser_start_timeout: 120,
   browser_args: {
-    Chrome: {
+    chromium: {
       ci: [
-        // --no-sandbox is needed when running Chrome inside a container
+        // --no-sandbox is needed when running chromium inside a container
         process.env.CI ? '--no-sandbox' : null,
         '--headless',
         '--disable-dev-shm-usage',
