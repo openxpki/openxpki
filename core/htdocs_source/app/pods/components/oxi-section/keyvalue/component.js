6127fec88ae5ed96cf096edf3cedacc5ca714cdc
Jens Berthold <jens@jebecs.de>
2022-10-26 15:37:20 +0200

    Fix UI: stop background refreshing if page is refreshed or left

--- core/htdocs_source/app/pods/components/oxi-section/keyvalue/component.js
+++ core/htdocs_source/app/pods/components/oxi-section/keyvalue/component.js
@@ -35,17 +35,22 @@ export default class OxiSectionKeyvalueComponent extends Component {
         return this.items.filter(i => typeof i.label !== 'undefined' && i.label !== 0 && i.label !== null).length > 0
     }
 
+    // lifecycle hook of @glimmer/component
+    willDestroy() {
+        super.willDestroy(...arguments)
+        // Remove all refresh timers to prevent that an OpenXPKI page refresh
+        // (!= browser reload) leads to multiple parallel request timers.
+        // Or that the requests continue if another OpenXPKI page is opened.
+        for (const timer of this.refreshTimers.values()) clearTimeout(timer)
+    }
+
     startRefresh(item) {
         let timeout = item.refresh.timeout
         let uri = item.refresh.uri
         if (! timeout) throw new Error("Key 'timeout' is missing in 'refresh' property.")
         if (! uri) throw new Error("Key 'uri' is missing in 'refresh' property.")
 
-        // cancel old search query timer on new input
-        let oldTimer = this.refreshTimers.get(item._id)
-        if (oldTimer) clearTimeout(oldTimer)
-
-        // set refresh timer
+        // refresh function
         let refreshRequest = () => {
             emSet(item, '_refreshing', true)
             this.content.updateRequestQuiet({
@@ -55,13 +60,19 @@ export default class OxiSectionKeyvalueComponent extends Component {
                 if (! doc.value) return
                 emSet(item, "value", doc.value)
                 // item.value = doc.value
-                this.refreshTimers.set(item._id, setTimeout(refreshRequest, timeout * 1000))
+                if (!this.isDestroying && !this.isDestroyed) {
+                    this.refreshTimers.set(item._id, setTimeout(refreshRequest, timeout * 1000))
+                }
             })
             .finally(() => {
                 emSet(item, '_refreshing', false)
             })
         }
 
+        // cancel old search query timer on new input
+        let oldTimer = this.refreshTimers.get(item._id)
+        if (oldTimer) clearTimeout(oldTimer)
+        // set refresh timer
         this.refreshTimers.set(item._id, setTimeout(refreshRequest, timeout * 1000))
     }
 }
