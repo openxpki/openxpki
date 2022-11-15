455ae3c69fedc632cf7ca703bd27408bea5f12f7
Oliver Welter <owelter@whiterabbitsecurity.com>
2022-11-15 17:13:21 +0100

    Use perl parser instead of CLI in ParseCertBundle activity

--- core/server/OpenXPKI/Server/Workflow/Activity/Tools/ParseCertificateBundle.pm
+++ core/server/OpenXPKI/Server/Workflow/Activity/Tools/ParseCertificateBundle.pm
@@ -7,11 +7,8 @@ use OpenXPKI::Server::Context qw( CTX );
 use OpenXPKI::Exception;
 use OpenXPKI::Debug;
 use English;
-use OpenXPKI::DN;
 use OpenXPKI::Crypt::X509;
-use OpenXPKI::Serialization::Simple;
-use Template;
-use Digest::SHA qw(sha1_hex);
+use OpenXPKI::Crypt::PKCS7;
 
 sub execute {
     ##! 1: 'execute'
@@ -23,31 +20,29 @@ sub execute {
     my $config = CTX('config');
 
     my $pem = $self->param('pem');
-
+    ##! 64: $pem
     my $target_key =  $self->param('target_key ') || 'certificate_list';
 
     OpenXPKI::Exception->throw(
         message => 'No certificate data received'
     ) unless ($pem);
 
-    my $certs = [ $pem =~ m{ ( -----BEGIN\ [\w\s]*CERTIFICATE----- [^-]+ -----END\ [\w\s]*CERTIFICATE----- ) }gmsx ];
-
-    if (!$certs->[0] && $pem =~ m{-----BEGIN PKCS7----}) {
-         $certs = CTX('api2')->get_default_token()->command({
-            COMMAND  => 'pkcs7_get_chain',
-            NOSORT   => 1,
-            PKCS7    => $pem,
-        });
+    my $certs;
+    if (my @certs = $pem =~ m{ ( -----BEGIN\ [\w\s]*CERTIFICATE----- [^-]+ -----END\ [\w\s]*CERTIFICATE----- ) }gmsx) {
+        ##! 16: 'smells like a PEM bundle'
+        $certs = [ map { OpenXPKI::Crypt::X509->new($_) } @certs ]
+    } elsif ($pem =~ m{-----BEGIN PKCS7----}) {
+        ##! 16: 'Looks like a PKCS7 container'
+        $certs = OpenXPKI::Crypt::PKCS7->new($pem)->certificates();
     }
 
     OpenXPKI::Exception->throw(
         message => 'Unable to extract any certs from provided PEM'
     ) unless (ref $certs && $certs->[0]);
 
+    ##! 64: $certs
     my @res;
-    foreach my $data (@$certs) {
-        my $x509 = OpenXPKI::Crypt::X509->new($data);
-
+    foreach my $x509 (@$certs) {
         push @res, {
             cert_subject    => $x509->get_subject(),
             serial          => $x509->get_serial(),
