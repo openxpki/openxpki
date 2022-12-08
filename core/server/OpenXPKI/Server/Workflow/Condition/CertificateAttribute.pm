fa2db7cd25bc6a3a016be62ea87ab01bce9fa83e
Oliver Welter <owelter@whiterabbitsecurity.com>
2022-12-08 12:45:16 +0100

    Extend and rename Certificate(Has)Attribute condition
    Add parameters to check the value of the attribute instead
    of only looking for existence. The name has been aligned
    with other conditions to Certificate_Has_Attribute, the old
    class name still works but logs a deprecation warning.

--- core/server/OpenXPKI/Server/Workflow/Condition/CertificateAttribute.pm
+++ core/server/OpenXPKI/Server/Workflow/Condition/CertificateAttribute.pm
@@ -2,51 +2,15 @@ package OpenXPKI::Server::Workflow::Condition::CertificateAttribute;
 
 use strict;
 use warnings;
-use base qw( OpenXPKI::Server::Workflow::Condition );
+use base qw( OpenXPKI::Server::Workflow::Condition::CertificateHasAttribute );
 use Workflow::Exception qw( condition_error configuration_error );
 use OpenXPKI::Server::Context qw( CTX );
 use OpenXPKI::Debug;
 
-use Data::Dumper;
-
 sub _evaluate {
-
-    ##! 1: 'start'
-    my ( $self, $workflow ) = @_;
-
-    my $context  = $workflow->context();
-    ##! 64: 'context: ' . Dumper($context)
-
-
-    my $attribute = $self->param('attribute');
-
-    if (!$attribute || $attribute =~ m{\W}) {
-        configuration_error('Attribute name must be given and must not contain any non-word characters');
-    }
-
-    my $cert_identifier = $self->param('cert_identifier');
-    $cert_identifier = $context->param('cert_identifier') unless($cert_identifier);
-
-    if (!defined $cert_identifier ) {
-        configuration_error('You need to have cert_identifier in context or set it via the parameters');
-    }
-
-    my $res =  CTX('api2')->get_cert_attributes(
-        identifier => $cert_identifier,
-        attribute => $attribute,
-        tenant => '',
-    );
-
-    if (!defined $res) {
-        ##! 16: 'not defined'
-        CTX('log')->application()->debug("CertificateAttribute condition failed - no values found");
-        condition_error('CertificateAttribute condition failed');
-    }
-
-    CTX('log')->application()->debug("CertificateAttribute condition passed:" . Dumper $res);
-
-    return 1;
-    ##! 16: 'end'
+    my $self = shift;
+    CTX('log')->deprecated()->error('Please rename O::S::W::Condition::CertificateAttribute to CertificateHasAttribute');
+    $self->SUPER::_evaluate();
 }
 
 1;
@@ -59,21 +23,5 @@ OpenXPKI::Server::Workflow::Condition::CertificateAttribute
 
 =head1 SYNOPSIS
 
-=head1 DESCRIPTION
-
-This condition checks whether the given certificate has (at least one)
-value for the certificate_attribute given.
-
-=head2 Activity Parameters
-
-=over
-
-=item attribute
-
-Name of the attribute to look up, wildcards are not allowed!
-
-=item cert_identifier
-
-Certificate to look up, if not given uses the value from the context.
-
-=back
+This class is deprecated and should no longer be used.
+Use OpenXPKI::Server::Workflow::Condition::CertificateB<Has>Attribute.
---
+++ core/server/OpenXPKI/Server/Workflow/Condition/CertificateHasAttribute.pm
@@ -0,0 +1,133 @@
+package OpenXPKI::Server::Workflow::Condition::CertificateHasAttribute;
+
+use strict;
+use warnings;
+use base qw( OpenXPKI::Server::Workflow::Condition );
+use Workflow::Exception qw( condition_error configuration_error );
+use OpenXPKI::Server::Context qw( CTX );
+use OpenXPKI::Debug;
+
+use Data::Dumper;
+
+sub _evaluate {
+
+    ##! 1: 'start'
+    my ( $self, $workflow ) = @_;
+
+    my $context  = $workflow->context();
+    ##! 64: 'context: ' . Dumper($context)
+
+    my $attribute = $self->param('attribute');
+    my $condition = $self->param('condition') || 'defined';
+    my $value = $self->param('value') // '';
+    ##! 32: "$attribute / $condition / $value"
+
+    if (!$attribute || $attribute =~ m{\W}) {
+        configuration_error('Attribute name must be given and must not contain any non-word characters');
+    }
+
+    my $cert_identifier = $self->param('cert_identifier');
+    $cert_identifier = $context->param('cert_identifier') unless($cert_identifier);
+
+    if (!defined $cert_identifier ) {
+        configuration_error('You need to have cert_identifier in context or set it via the parameters');
+    }
+
+    my $res =  CTX('api2')->get_cert_attributes(
+        identifier => $cert_identifier,
+        attribute => $attribute,
+        tenant => '',
+    );
+
+    if (!defined $res) {
+        ##! 16: 'not defined'
+        CTX('log')->application()->debug("CertificateAttribute condition failed - no values found");
+        condition_error('CertificateAttribute condition failed');
+    }
+
+    ##! 64: $res
+    CTX('log')->application()->debug("CertificateHasAttribute $attribute got result " . Dumper $res);
+    if ($condition eq 'defined') {
+        CTX('log')->application()->debug('CertificateHasAttribute is defined');
+        return 1;
+    }
+
+    configuration_error('CertificateAttribute has no value for comparison defined') unless($value ne '');
+
+    my @values = @{$res->{$attribute}};
+    if ($condition eq 'is_value') {
+        condition_error('CertificateAttribute condition is_value failed (more then one item)')
+            if (@values > 1);
+
+        condition_error('CertificateAttribute condition is_value does not match')
+            if ($values[0] ne $value);
+
+        CTX('log')->application()->debug('CertificateHasAttribute is_value passed');
+        return 1;
+    }
+
+    if ($condition eq 'has_value') {
+        if (grep { $_ eq $value } @values) {
+            CTX('log')->application()->debug('CertificateHasAttribute contains value ' . $value);
+            return 1;
+        }
+        condition_error('CertificateAttribute condition has_value failed');
+    }
+
+    condition_error('CertificateAttribute unsupported condition');
+
+    ##! 16: 'end'
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+OpenXPKI::Server::Workflow::Condition::CertificateAttribute
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+This condition checks whether
+
+=head2 Activity Parameters
+
+=over
+
+=item attribute
+
+Name of the attribute to look up, wildcards are not allowed!
+
+=item cert_identifier
+
+Certificate to look up, if not given uses the value from the context.
+
+=item condition
+
+The condition that the attribute must fulfill
+
+=over
+
+=item defined (default)
+
+The certificate has at least one entry for the attribute given.
+
+=item is_value
+
+The certificate has exaclty one item and this matches the given value.
+
+=item has_value
+
+The certificate has at least one item that matches the given value.
+
+=back
+
+=item value
+
+The value to match the attributes value (full string match).
+Ignored when I<condition> is set to I<defined>.
+
+=back
