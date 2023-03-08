c629e0b8c5d50b5f585be4a4380c82b8dce14232
Jens Berthold <jens@jebecs.de>
2023-03-08 15:16:07 +0100

    Update UI assets

--- core/server/htdocs/assets/autoimport-tests-c07621b1ad8c49d8d098e60d662f9bc3.js
+++ core/server/htdocs/assets/autoimport-tests-c07621b1ad8c49d8d098e60d662f9bc3.js
@@ -1,4 +1,4 @@
-var __ember_auto_import__;(()=>{var r,e={85377:function(r,e){window._eai_r=require,window._eai_d=define},27970:(r,e,o)=>{var t,n
+var __ember_auto_import__;(()=>{var r,e={4148:function(r,e){window._eai_r=require,window._eai_d=define},75470:(r,e,o)=>{var t,n
 r.exports=(t=_eai_d,n=_eai_r,window.emberAutoImportDynamic=function(r){return 1===arguments.length?n("_eai_dyn_"+r):n("_eai_dynt_"+r)(Array.prototype.slice.call(arguments,1))},window.emberAutoImportSync=function(r){return n("_eai_sync_"+r)(Array.prototype.slice.call(arguments,1))},t("lodash.castarray",[],(function(){return o(23345)})),t("lodash.last",[],(function(){return o(62218)})),t("lodash.omit",[],(function(){return o(9534)})),void t("qunit",[],(function(){return o(47134)})))}},o={}
 function t(r){var n=o[r]
 if(void 0!==n)return n.exports
@@ -15,6 +15,6 @@ var e=(e,o)=>{var n,i,[a,_,l]=o,u=0
 if(a.some((e=>0!==r[e]))){for(n in _)t.o(_,n)&&(t.m[n]=_[n])
 if(l)var d=l(t)}for(e&&e(o);u<a.length;u++)i=a[u],t.o(r,i)&&r[i]&&r[i][0](),r[i]=0
 return t.O(d)},o=globalThis.webpackChunk_ember_auto_import_=globalThis.webpackChunk_ember_auto_import_||[]
-o.forEach(e.bind(null,0)),o.push=e.bind(null,o.push.bind(o))})(),t.O(void 0,[968],(()=>t(85377)))
-var n=t.O(void 0,[968],(()=>t(27970)))
+o.forEach(e.bind(null,0)),o.push=e.bind(null,o.push.bind(o))})(),t.O(void 0,[968],(()=>t(4148)))
+var n=t.O(void 0,[968],(()=>t(75470)))
 n=t.O(n),__ember_auto_import__=n})()
