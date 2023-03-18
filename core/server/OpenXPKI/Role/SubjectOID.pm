dbb98c249323d486cab88c12fd25ba42ccccd377
Oliver Welter <owelter@whiterabbitsecurity.com>
2023-03-18 09:22:39 +0100

    Add missings OIDs for unstructuredName/unstructuredAddress

--- core/server/OpenXPKI/Role/SubjectOID.pm
+++ core/server/OpenXPKI/Role/SubjectOID.pm
@@ -20,15 +20,22 @@ has subject_oid_reverse_map => (
 
 sub __build_subject_oid_map {
     return {
-        "2.5.4.3"                       => ['CN','commonName'],
-        "2.5.4.6"                       => ['C','countryName'],
-        "2.5.4.7"                       => ['L','localityName'],
-        "2.5.4.8"                       => ['ST','stateOrProvinceName'],
-        "2.5.4.10"                      => ['O','organizationName'],
-        "2.5.4.11"                      => ['OU','organizationalUnitName'],
-        "1.2.840.113549.1.9.1"          => ["emailAddress","E"],
-        "0.9.2342.19200300.100.1.1"     => ['UID','userID'],
-        "0.9.2342.19200300.100.1.25"    => ['DC','domainComponent'],
+        '0.9.2342.19200300.100.1.1'     => ['UID','userID'],
+        '0.9.2342.19200300.100.1.25'    => ['DC','domainComponent'],
+
+        '1.2.840.113549.1.9.1'          => ["emailAddress","E"],
+        '1.2.840.113549.1.9.2'          => 'unstructuredName',
+        '1.2.840.113549.1.9.8'          => 'unstructuredAddress',
+
+        '2.5.4.3'                       => ['CN','commonName'],
+        '2.5.4.4'                       => [ 'surname', 'Surname', 'SN' ],
+        '2.5.4.5'                       => ['serialNumber','SN'],
+        '2.5.4.6'                       => ['C','countryName'],
+        '2.5.4.7'                       => ['L','localityName'],
+        '2.5.4.8'                       => ['ST','stateOrProvinceName'],
+        '2.5.4.9'                       => 'streetAddress',
+        '2.5.4.10'                      => ['O','organizationName'],
+        '2.5.4.11'                      => ['OU','organizationalUnitName'],
         '2.5.4.12'                      => [ 'title', 'Title' ],
         '2.5.4.13'                      => [ 'description', 'Description' ],
         '2.5.4.14'                      => 'searchGuide',
@@ -39,7 +46,7 @@ sub __build_subject_oid_map {
         '2.5.4.19',                     => 'physicalDeliveryOfficeName',
         '2.5.4.20',                     => 'telephoneNumber',
         '2.5.4.23',                     => 'facsimileTelephoneNumber',
-        '2.5.4.4'                       => [ 'surname', 'Surname', 'SN' ],
+
         '2.5.4.41'                      => [ 'name', 'Name' ],
         '2.5.4.42'                      => ['GN','givenName'],
         '2.5.4.43'                      => 'initials',
@@ -48,8 +55,7 @@ sub __build_subject_oid_map {
         '2.5.4.46'                      => 'dnQualifier',
         '2.5.4.51'                      => 'houseIdentifier',
         '2.5.4.65'                      => 'pseudonym',
-        '2.5.4.5'                       => ['serialNumber','SN'],
-        '2.5.4.9'                       => 'streetAddress',
+
     };
 }
 
