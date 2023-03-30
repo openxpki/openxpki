openxpki/openxpki

commit 0ad115ba91c2ccffd720f663c75607b21cab1d11

Fix broken server response if custom I18N strings contain special characters
Using the new i18n_walk() function we properly translate all occuring
I18N strings of a nested data structure *before* the JSON encoding.

Before, depending on the I18N translation strings it could happen that the
already encoded JSON string became broken. E.g. if a custom I18N string
added double quotes to a JSON value already surrounded by quotes.

core/server/OpenXPKI/Client/UI/Result.pm
@@ -20,7 +20,7 @@ use Crypt::JWT qw( encode_jwt );
 use Crypt::PRNG;
 
 # Project modules
-use OpenXPKI::i18n qw( i18nTokenizer );
+use OpenXPKI::i18n qw( i18n_walk );
 use OpenXPKI::Serialization::Simple;
 use OpenXPKI::Client::UI::Response;
 
@@ -569,7 +569,7 @@ sub _render_body_to_str {
     # B) response to a confined request, i.e. no page update (auto-complete etc.)
     #
     if ($self->has_confined_response) {
-        return i18nTokenizer(encode_json($self->confined_response));
+        return encode_json(i18n_walk($self->confined_response)));
     }
 
     #
@@ -585,7 +585,7 @@ sub _render_body_to_str {
     # add session ID
     $result->{session_id} = $self->_session->id;
 
-    return i18nTokenizer(encode_json($result));
+    return encode_json(i18n_walk($result));
 }
 
 =head2 render

core/server/cgi-bin/rpc.fcgi
@@ -21,7 +21,7 @@ use OpenXPKI::Exception;
 use OpenXPKI::Client::Config;
 use OpenXPKI::Client::Service::RPC;
 use OpenXPKI::Serialization::Simple;
-use OpenXPKI::i18n qw( i18nGettext i18nTokenizer );
+use OpenXPKI::i18n qw( i18nGettext i18n_walk );
 
 
 our $config;
@@ -154,11 +154,7 @@ sub send_output {
         $json->canonical( $canonical_keys ? 1 : 0 );
 
         # run i18n tokenzier on output if a language is set
-        if ($config->language()) {
-            print i18nTokenizer($json->encode( $result ));
-        } else {
-            print $json->encode( $result );
-        }
+        print $json->encode( $config->language ? i18n_walk($result) : $result );
     }
 }
 

commit f6efe5f704aa875365d8d4f2f648abb9160a0fb0

Fix bracket imbalance bug introduced in commit 0ad115ba

core/server/OpenXPKI/Client/UI/Result.pm
@@ -569,7 +569,7 @@ sub _render_body_to_str {
     # B) response to a confined request, i.e. no page update (auto-complete etc.)
     #
     if ($self->has_confined_response) {
-        return encode_json(i18n_walk($self->confined_response)));
+        return encode_json(i18n_walk($self->confined_response));
     }
 
     #