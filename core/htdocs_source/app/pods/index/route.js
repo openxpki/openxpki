bc5df893f1b776c4b2e5c5089eecc35bc2e5b39f
Jens Berthold <jens@jebecs.de>
2023-04-25 12:24:52 +0200

    UI: debug statement

--- core/htdocs_source/app/pods/index/route.js
+++ core/htdocs_source/app/pods/index/route.js
@@ -1,10 +1,12 @@
-import Route from '@ember/routing/route';
-import { service } from '@ember/service';
+import Route from '@ember/routing/route'
+import { service } from '@ember/service'
+import { debug } from '@ember/debug'
 
 export default class IndexRoute extends Route {
-    @service router;
+    @service router
 
     redirect(/*model, transition*/) {
-        return this.router.transitionTo("openxpki", "welcome");
+        debug("Redirecting from / to /openxpki/welcome")
+        return this.router.transitionTo("openxpki", "welcome")
     }
 }
