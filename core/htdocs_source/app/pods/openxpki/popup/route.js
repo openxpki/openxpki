42063aef2b90814bb7305a0ecfd37d953161ff95
Jens Berthold <jens@jebecs.de>
2023-05-17 10:57:07 +0200

    UI: code formatting

--- core/htdocs_source/app/pods/openxpki/popup/route.js
+++ core/htdocs_source/app/pods/openxpki/popup/route.js
@@ -11,18 +11,10 @@ export default class OpenXpkiPopupRoute extends Route {
     // Reserved Ember function
     async model(params, transition) {
         let page = params.popup_page
+        let target = this.content.TARGET.POPUP
         debug("openxpki/popup/route - popup_page = " + page)
 
-        await this.content.requestUpdate(
-            {
-                page,
-                target: this.content.TARGET.POPUP,
-            },
-            {
-                verbose: true,
-            }
-        )
-
+        await this.content.requestUpdate({ page, target }, { verbose: true })
         return this.content
     }
 }
