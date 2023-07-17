664bd33063f1d29d6b979ff60b008c7a568fe00c
Jens Berthold <jens@jebecs.de>
2023-07-17 13:50:59 +0200

    UI: comments

--- core/htdocs_source/app/data/pager.js
+++ core/htdocs_source/app/data/pager.js
@@ -8,15 +8,15 @@ export default class Pager extends Base {
     static _type = 'app/data/pager'
     static _idField = 'pagerurl'
 
-    @tracked num
+    @tracked num // page number
     @tracked active
-    @tracked count = 0
-    @tracked startat = 0
-    @tracked limit = Number.MAX_VALUE
+    @tracked count = 0 // total number of items
+    @tracked startat = 0 // current start item index
+    @tracked limit = Number.MAX_VALUE // max. items per page
     @tracked order
     @tracked reverse = false
-    @tracked pagesizes
-    @tracked pagersize
+    @tracked pagesizes // array of selectable page sizes
+    @tracked pagersize // max. number of pages to display before using ellipsis "..."
     @tracked pagerurl
     @tracked disabled = false
 }
