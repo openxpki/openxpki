27816f77b5d9f42419f40d3f033f23c2ca57c318
Jens Berthold <jens@jebecs.de>
2023-07-23 23:14:22 +0200

    UI: fix button container hiding if no buttons defined

--- core/htdocs_source/app/pods/components/oxi-base/button-container/component.js
+++ core/htdocs_source/app/pods/components/oxi-base/button-container/component.js
@@ -42,4 +42,8 @@ export default class OxiButtonContainerComponent extends Component {
         if (!this.buttons) { return false }
         return this.buttons.some(i => i.description)
     }
+
+    get hasButtons() {
+        return this.buttons.length > 0
+    }
 }
