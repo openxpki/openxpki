8d29cc004ccc13d62c1863ab684fddda830d991b
Oliver Welter <owelter@whiterabbitsecurity.com>
2023-10-16 17:02:23 +0200

    Enhance BooleanHasFields to work with >2 args

--- core/server/OpenXPKI/Server/Workflow/Validator/BooleanHasFields.pm
+++ core/server/OpenXPKI/Server/Workflow/Validator/BooleanHasFields.pm
@@ -9,26 +9,27 @@ use OpenXPKI::Server::Context qw( CTX );
 
 sub _validate {
 
-    my ($self, $wf, $arg1, $arg2) = @_;
+    my ($self, $wf, @args) = @_;
 
     my $op = $self->param('operator') || 'or';
 
     my $error = $self->param('error') || 'I18N_OPENXPKI_UI_VALIDATOR_ERROR_BOOLEAN_'.uc($op);
 
-    $arg1 = (defined $arg1 && $arg1 ne '');
-    $arg2 = (defined $arg2 && $arg2 ne '');
+    ##! 64: 'args ' . Dumper \@args
+    my $count = scalar @args;
+    my $setcount = 0;
+    map { $setcount++ if(defined $_ && $_ ne ''); } @args;
+
+    ##! 32: sprintf('Op %s - Count %01d - Setcount %01d', $op, $count, $setcount)
 
     if ($op eq 'or') {
-        validation_error($error)
-            unless ($arg1 or $arg2);
+        validation_error($error) if ($setcount == 0);
     } elsif ($op eq 'xor') {
-        validation_error($error)
-            unless ($arg1 xor $arg2);
+        validation_error($error) if ($setcount != 1);
     } elsif ($op eq 'and') {
         validation_error($error)
-            unless (($arg1 and $arg2) or (not $arg1 and not $arg2));
+            unless (($setcount == 0) || ($setcount == $count));
     }
-
     return 1;
 }
 
@@ -56,13 +57,13 @@ This validator checks whether the number of arguments set matches the
 boolean operator. Set in this validator means not empty or undef, the
 literal value I<0> is therefore treated as "true" in the equotation!
 
-Note: The check for I<and> is also true if both values are B<NOT> set, so
-this option is used to check if two values exist together. To check for
+Note: The check for I<and> is also true if all values are B<NOT> set, so
+this option can be checked if all values exist together. To check for
 their general presence simply use the I<required> attribute.
 
 =head2 Argument
 
-Two values to use in boolean equotation.
+A list of (at least two) values to use in boolean equotation.
 
 =head2 Parameter
 
