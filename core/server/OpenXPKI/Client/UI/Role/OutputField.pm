openxpki/openxpki

commit 8e686f67a0ef650b29de53ce482b4c7a94bb1422

Fix "Use of uninitialized value in pattern match (m//)"

core/server/OpenXPKI/Client/UI/Role/OutputField.pm
@@ -77,8 +77,8 @@ sub render_output_field ($self, $arg) {
 
         # Code format any PEM blocks
         if (( $name =~ m{ \A (pkcs10|pkcs7) \z }x ) ||
-            ( ref $item->{value} eq '' &&
-                $item->{value} =~ m{ \A \s* -----BEGIN([A-Z ]+)-----.*-----END([A-Z ]+)---- }xms)) {
+            ( ref $item->{value} eq '' and
+                $item->{value}//'' =~ m{ \A \s* -----BEGIN([A-Z ]+)-----.*-----END([A-Z ]+)---- }xms)) {
             $item->{format} = 'code';
             $item->{value} =~ s{(\A\s*|\s*\z)}{}sg;
         } elsif ($type eq 'textarea') {

commit 5ff7e374eb64531fa36bd3867d613cd381d7ddc6

Fix regression bug introduced in 8e686f67 (operator precedence)

core/server/OpenXPKI/Client/UI/Role/OutputField.pm
@@ -78,7 +78,7 @@ sub render_output_field ($self, $arg) {
         # Code format any PEM blocks
         if (( $name =~ m{ \A (pkcs10|pkcs7) \z }x ) ||
             ( ref $item->{value} eq '' and
-                $item->{value}//'' =~ m{ \A \s* -----BEGIN([A-Z ]+)-----.*-----END([A-Z ]+)---- }xms)) {
+                ($item->{value}//'') =~ m{ \A \s* -----BEGIN([A-Z ]+)-----.*-----END([A-Z ]+)---- }xms)) {
             $item->{format} = 'code';
             $item->{value} =~ s{(\A\s*|\s*\z)}{}sg;
         } elsif ($type eq 'textarea') {