db78fd3af0093787e9042323cd1265f2d38e5e3f
Jens Berthold <jens@jebecs.de>
2023-11-28 10:15:04 +0100

    Fix: "min"/"max" evaluation did override "clonable" field attribute

--- core/server/OpenXPKI/Workflow/Field.pm
+++ core/server/OpenXPKI/Workflow/Field.pm
@@ -54,7 +54,11 @@ sub process {
 
     # set "clonable" attribute
     # ("min:0" is legacy syntax for "optional" and does NOT indicate a clonable field)
-    $self->field->{clonable} = ($self->field->{min} || $self->field->{max}) ? 1 : 0;
+    $self->field->{clonable} = (
+        $self->field->{clonable} or
+        $self->field->{min} or
+        $self->field->{max}
+    ) ? 1 : 0;
 
     # process select options
     if ($self->field->{type} eq 'select') {
