openxpki/openxpki

commit 69a9e7d6c696ec6fafd00aa046f7b59db1123725

Properly handle unknown certid in SCEP GetCertInitial request

core/server/OpenXPKI/Client/Service/SCEP.pm
@@ -112,6 +112,11 @@ sub generate_pkcs7_response {
 
         my $failInfo = ($response->error == 40001) ? 'badMessageCheck' : 'badRequest';
 
+        # Special handling of invalid certid with GetCertInitial request
+        if ($self->message_type() eq 'GetCertInitial' && $response->error == 40003) {
+            $failInfo = 'badCertId';
+        }
+
         $self->logger->warn('Client error / malformed request ' . $failInfo);
 
         return $self->backend()->run_command('scep_generate_failure_response',
@@ -173,7 +178,7 @@ around 'build_params' => sub {
             $params->{_url_params} = $extra;
         }
     } elsif ($self->message_type() eq 'GetCertInitial') {
-        $params->{pkcs10} = '';
+        $params->{pkcs10} = undef;
         $params->{transaction_id} = $self->transaction_id();
         $params->{signer_cert} = $self->signer();
     } elsif ($self->message_type() =~ m{\AGet(Cert|CRL)\z}) {

commit 0add055d3e72784ce4bebc4546e2400ccb5b068d

Fix problem with GetCertInitial / cleanup service handler
Fixes problem introduced with 69a9e7d6c696ec6fafd00aa046f7b59db1123725

Add a new error code 40005 for failed pickup workflow

Move some EST/CMS specific parts out of the shared handler code

Encapsulate pickup_config assemby into dedicated method

Remove unused 'operation' parameter from handle_enrollment_request and
build_params methods.

core/server/OpenXPKI/Client/Service/Base.pm
@@ -9,6 +9,7 @@ use Crypt::PKCS10;
 use Log::Log4perl qw(:easy);
 use MIME::Base64;
 use Digest::SHA qw(sha1_hex);
+use Feature::Compat::Try;
 use OpenXPKI::Exception;
 use OpenXPKI::Client::Simple;
 use OpenXPKI::Client::Service::Response;
@@ -59,8 +60,11 @@ sub _init_backend {
 sub build_params {
 
     my $self = shift;
-    my $operation = shift || $self->operation();
 
+    # not used here
+    #my $cgi = shift;
+
+    my $operation = $self->operation();
     my $conf = $self->config()->config();
 
     my $param = {};
@@ -137,11 +141,52 @@ sub build_params {
 
 }
 
+sub build_pickup_config {
+
+    my $self = shift;
+    my $param = shift;
+    my $cgi = shift;
+
+    my $conf = $self->config()->config();
+    my $pickup_config = {(
+        workflow => 'certificate_enroll',
+        pickup => 'pkcs10',
+        pickup_attribute => 'transaction_id',
+        ),
+        %{$conf->{$self->operation()} || {}},
+    };
+
+    Log::Log4perl::MDC->put('tid', $param->{transaction_id});
+
+    # check for pickup parameter
+    my $pickup_value;
+    # namespace needs a single value
+    if ($pickup_config->{pickup_workflow}) {
+        # explicit pickup paramters are set
+        my @keys = split /\s*,\s*/, $pickup_config->{pickup};
+        foreach my $key (@keys) {
+            # take value from param hash if defined, this makes data
+            # from the environment avail to the pickup workflow
+            my $val = $param->{$key} // $cgi->param($key);
+            $pickup_value->{$key} = $val if (defined $val);
+        }
+    } else {
+        # pickup via transaction_id
+        $pickup_value = $param->{transaction_id};
+    }
+
+    return ($pickup_config, $pickup_value);
+}
+
+
 sub handle_enrollment_request {
 
     my $self = shift;
     my $cgi = shift;
-    my $operation = shift || $self->operation();
+
+    die "Passing operation to handle_enrollment_request is no longer supported." if (@_);
+
+    my $operation = $self->operation();
 
     my $config = $self->config();
     my $log = $self->logger();
@@ -150,7 +195,7 @@ sub handle_enrollment_request {
 
     # Build configuration parameters, can be overloaded by protocols
     # e.g. SCEP to inject data from the input
-    my $param = $self->build_params( $operation, $cgi );
+    my $param = $self->build_params( $cgi );
 
     if (!defined $param) {
         return OpenXPKI::Client::Service::Response->new( 50010 );
@@ -193,49 +238,26 @@ sub handle_enrollment_request {
         $param->{transaction_id} = sha1_hex($decoded->csrRequest) unless($param->{transaction_id});
     }
 
-    # fall back to simpleneroll config for simplereenroll if not set
-    if ($operation eq 'simplereenroll' && !$conf->{simplereenroll}) {
-        $operation = "simpleenroll";
-    }
-
-    my $pickup_config = {(
-        workflow => 'certificate_enroll',
-        pickup => 'pkcs10',
-        pickup_attribute => 'transaction_id',
-        ),
-        %{$conf->{$operation} || {}},
-    };
+    my ($pickup_config, $pickup_value) = $self->build_pickup_config( $param, $cgi );
     $log->trace(Dumper $pickup_config) if $log->is_trace;
 
     my $workflow;
-    eval {
 
-        Log::Log4perl::MDC->put('tid', $param->{transaction_id});
-
-        # check for pickup parameter
-        my $pickup_value;
-        # namespace needs a single value
-        if ($pickup_config->{pickup_workflow}) {
-            # explicit pickup paramters are set
-            my @keys = split /\s*,\s*/, $pickup_config->{pickup};
-            foreach my $key (@keys) {
-                # take value from param hash if defined, this makes data
-                # from the environment avail to the pickup workflow
-                my $val = $param->{$key} // $cgi->param($key);
-                $pickup_value->{$key} = $val if (defined $val);
-            }
-        } else {
-            # pickup via transaction_id
-            $pickup_value = $param->{transaction_id};
-        }
-
+    try {
         # try pickup
         $workflow = $self->pickup_workflow($pickup_config, $pickup_value);
+
+        # it was a pickup, it was not successful, we do not have a PKCS10
+        if ($pickup_value && !$workflow && !$param->{pkcs10}) {
+            $log->debug("Pickup failed and no PKCS10 given");
+            return OpenXPKI::Client::Service::Response->new( 40005 );
+        }
 
         # pickup return undef if no workflow was found - start new one
         if (!$workflow) {
             $log->debug(sprintf("Initialize %s with params %s",
                 $pickup_config->{workflow}, join(", ", keys %{$param})));
+
             $workflow = $client->handle_workflow({
                 type => $pickup_config->{workflow},
                 params => $param,
@@ -243,7 +265,10 @@ sub handle_enrollment_request {
         }
 
         $log->trace( 'Workflow info '  . Dumper $workflow ) if ($log->is_trace());
-    };
+    } catch ($error) {
+        $log->error( $error );
+        return OpenXPKI::Client::Service::Response->new( 50003 );
+    }
 
     if (!$workflow || ( $workflow->{'proc_state'} ne 'finished' && !$workflow->{id} ) || $workflow->{'proc_state'} eq 'exception') {
         my $reply = $client->last_reply() || {};
@@ -253,7 +278,7 @@ sub handle_enrollment_request {
                 return OpenXPKI::Client::Service::Response->new( 40004 );
             }
         }
-        $log->error( $EVAL_ERROR ? $EVAL_ERROR : 'Internal Server Error');
+        $log->error( 'Internal server error');
         return OpenXPKI::Client::Service::Response->new( 50003 );
     }
 
@@ -322,7 +347,7 @@ sub handle_property_request {
 
     my $conf = $config->config();
 
-    my $param = $self->build_params( $operation, $cgi );
+    my $param = $self->build_params( $cgi );
 
     if (!defined $param) {
         return OpenXPKI::Client::Service::Response->new( 50010 );

core/server/OpenXPKI/Client/Service/EST.pm
@@ -18,14 +18,13 @@ sub handle_revocation_request {
 
     my $self = shift;
     my $cgi = shift;
-    my $operation = shift || $self->operation();
 
     my $config = $self->config();
     my $log = $self->logger();
 
     my $conf = $config->config();
 
-    my $param = $self->build_params( $operation, $cgi );
+    my $param = $self->build_params( $cgi );
 
     if (!defined $param) {
         return OpenXPKI::Client::Service::Response->new( 50010 );
@@ -42,7 +41,7 @@ sub handle_revocation_request {
         return OpenXPKI::Client::Service::Response->new( 40002 );
     }
 
-    my $workflow_type = $conf->{$operation}->{workflow} ||
+    my $workflow_type = $conf->{simplerevoke}->{workflow} ||
         'certificate_revoke';
     $log->debug( 'Start workflow type ' . $workflow_type );
     $log->trace( 'Workflow Paramters '  . Dumper $param );

core/server/OpenXPKI/Client/Service/Response.pm
@@ -104,6 +104,7 @@ our %named_messages = (
     '40002' => 'Unable to parse request',
     '40003' => 'Incoming enrollment with empty body',
     '40004' => 'Missing or invalid parameters',
+    '40005' => 'No result for given pickup parameters',
     '40006' => 'Request was rejected',
     '40100' => 'Unauthorized',
     '40400' => 'Not Found',

core/server/OpenXPKI/Client/Service/SCEP.pm
@@ -110,15 +110,16 @@ sub generate_pkcs7_response {
         # but we must remove the undef value from the parameters list
         delete $params{alias} unless ($params{alias});
 
-        my $failInfo = ($response->error == 40001) ? 'badMessageCheck' : 'badRequest';
-
-        # Special handling of invalid certid with GetCertInitial request
-        if ($self->message_type() eq 'GetCertInitial' && $response->error == 40003) {
+        my $failInfo;
+        if ($response->error == 40001) {
+            $failInfo = 'badMessageCheck';
+        } elsif ($response->error == 40005) {
             $failInfo = 'badCertId';
+        } else {
+            $failInfo = 'badRequest';
         }
 
         $self->logger->warn('Client error / malformed request ' . $failInfo);
-
         return $self->backend()->run_command('scep_generate_failure_response',
             { %params, failinfo => $failInfo });
     }
@@ -161,7 +162,7 @@ around 'build_params' => sub {
         # Load url paramters if defined by config
         my $conf = $self->config()->config()->{'PKIOperation'};
         if ($conf->{param}) {
-            my $cgi = $args[1];
+            my $cgi = $args[0];
             my $extra;
             my @extra_params;
             # The legacy version - map anything
@@ -178,7 +179,7 @@ around 'build_params' => sub {
             $params->{_url_params} = $extra;
         }
     } elsif ($self->message_type() eq 'GetCertInitial') {
-        $params->{pkcs10} = undef;
+        $params->{pkcs10} = '';
         $params->{transaction_id} = $self->transaction_id();
         $params->{signer_cert} = $self->signer();
     } elsif ($self->message_type() =~ m{\AGet(Cert|CRL)\z}) {

core/server/cgi-bin/est.fcgi
@@ -47,6 +47,14 @@ while (my $cgi = CGI::Fast->new()) {
         next;
     }
 
+    # TODO - this should be removed
+    if ($operation eq 'simplereenroll' &&
+        !$config->config()->{'simplereenroll'} &&
+        $config->config()->{'simpleenroll'}) {
+        $operation eq 'simpleenroll';
+        $log->warn('Fall back to simpleenroll configuration on simplereenroll');
+    }
+
     my $client = OpenXPKI::Client::Service::EST->new(
         config => $config,
         logger => $log,
@@ -68,12 +76,23 @@ while (my $cgi = CGI::Fast->new()) {
     } elsif($operation eq 'csrattrs') {
         $mime = "application/csrattrs";
         $response = $client->handle_property_request($cgi);
+
     } elsif($operation eq 'simplerevoke') {
 
-        $response = $client->handle_revocation_request($cgi);
+        if (!$cgi->param( 'POSTDATA' )) {
+            $log->debug( 'Incoming revocation request with empty body' );
+            $response = OpenXPKI::Client::Service::Response->new( 40003 );
+        } else {
+            $response = $client->handle_revocation_request($cgi);
+        }
 
     } else {
-        $response = $client->handle_enrollment_request($cgi);
+        if (!$cgi->param( 'POSTDATA' )) {
+            $log->debug( 'Incoming enrollment with empty body' );
+            $response = OpenXPKI::Client::Service::Response->new( 40003 );
+        } else {
+            $response = $client->handle_enrollment_request($cgi);
+        }
     }
 
     $log->debug('Status: ' . $response->http_status_line());