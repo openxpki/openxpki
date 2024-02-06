openxpki/openxpki

commit 69a9e7d6c696ec6fafd00aa046f7b59db1123725

Properly handle unknown certid in SCEP GetCertInitial request

core/server/OpenXPKI/Client/Service/SCEP.pm
@@ -112,6 +112,11 @@ sub generate_pkcs7_response {
 
         my $failInfo = ($response->error == 40001) ? 'badMessageCheck' : 'badRequest';
 
+        # Special handling of invalid certid with GetCertInitial request
+        if ($self->message_type() eq 'GetCertInitial' && $response->error == 40003) {
+            $failInfo = 'badCertId';
+        }
+
         $self->logger->warn('Client error / malformed request ' . $failInfo);
 
         return $self->backend()->run_command('scep_generate_failure_response',
@@ -173,7 +178,7 @@ around 'build_params' => sub {
             $params->{_url_params} = $extra;
         }
     } elsif ($self->message_type() eq 'GetCertInitial') {
-        $params->{pkcs10} = '';
+        $params->{pkcs10} = undef;
         $params->{transaction_id} = $self->transaction_id();
         $params->{signer_cert} = $self->signer();
     } elsif ($self->message_type() =~ m{\AGet(Cert|CRL)\z}) {

commit 29299ffbdce1bba056db372c47125a4c0d40279a

Fix problem with GetCertInitial
Backport from 0add055d3e72784ce4bebc4546e2400ccb5b068d to fix problem
introduced by 69a9e7d6c696ec6fafd00aa046f7b59db1123725

core/server/OpenXPKI/Client/Service/Base.pm
@@ -9,6 +9,7 @@ use Crypt::PKCS10;
 use Log::Log4perl qw(:easy);
 use MIME::Base64;
 use Digest::SHA qw(sha1_hex);
+use Feature::Compat::Try;
 use OpenXPKI::Exception;
 use OpenXPKI::Client::Simple;
 use OpenXPKI::Client::Service::Response;
@@ -208,7 +209,7 @@ sub handle_enrollment_request {
     $log->trace(Dumper $pickup_config) if $log->is_trace;
 
     my $workflow;
-    eval {
+    try {
 
         Log::Log4perl::MDC->put('tid', $param->{transaction_id});
 
@@ -232,6 +233,12 @@ sub handle_enrollment_request {
         # try pickup
         $workflow = $self->pickup_workflow($pickup_config, $pickup_value);
 
+        # it was a pickup, it was not successful, we do not have a PKCS10
+        if ($pickup_value && !$workflow && !$param->{pkcs10}) {
+            $log->debug("Pickup failed and no PKCS10 given");
+            return OpenXPKI::Client::Service::Response->new( 40005 );
+        }
+
         # pickup return undef if no workflow was found - start new one
         if (!$workflow) {
             $log->debug(sprintf("Initialize %s with params %s",
@@ -243,7 +250,10 @@ sub handle_enrollment_request {
         }
 
         $log->trace( 'Workflow info '  . Dumper $workflow ) if ($log->is_trace());
-    };
+    } catch ($error) {
+        $log->error( $error );
+        return OpenXPKI::Client::Service::Response->new( 50003 );
+    }
 
     if (!$workflow || ( $workflow->{'proc_state'} ne 'finished' && !$workflow->{id} ) || $workflow->{'proc_state'} eq 'exception') {
         my $reply = $client->last_reply() || {};
@@ -253,7 +263,7 @@ sub handle_enrollment_request {
                 return OpenXPKI::Client::Service::Response->new( 40004 );
             }
         }
-        $log->error( $EVAL_ERROR ? $EVAL_ERROR : 'Internal Server Error');
+        $log->error( 'Internal server error');
         return OpenXPKI::Client::Service::Response->new( 50003 );
     }
 

core/server/OpenXPKI/Client/Service/Response.pm
@@ -104,6 +104,7 @@ our %named_messages = (
     '40002' => 'Unable to parse request',
     '40003' => 'Incoming enrollment with empty body',
     '40004' => 'Missing or invalid parameters',
+    '40005' => 'No result for given pickup parameters',
     '40006' => 'Request was rejected',
     '40100' => 'Unauthorized',
     '40400' => 'Not Found',

core/server/OpenXPKI/Client/Service/SCEP.pm
@@ -110,15 +110,16 @@ sub generate_pkcs7_response {
         # but we must remove the undef value from the parameters list
         delete $params{alias} unless ($params{alias});
 
-        my $failInfo = ($response->error == 40001) ? 'badMessageCheck' : 'badRequest';
-
-        # Special handling of invalid certid with GetCertInitial request
-        if ($self->message_type() eq 'GetCertInitial' && $response->error == 40003) {
+        my $failInfo;
+        if ($response->error == 40001) {
+            $failInfo = 'badMessageCheck';
+        } elsif ($response->error == 40005) {
             $failInfo = 'badCertId';
+        } else {
+            $failInfo = 'badRequest';
         }
 
         $self->logger->warn('Client error / malformed request ' . $failInfo);
-
         return $self->backend()->run_command('scep_generate_failure_response',
             { %params, failinfo => $failInfo });
     }
@@ -178,7 +179,7 @@ around 'build_params' => sub {
             $params->{_url_params} = $extra;
         }
     } elsif ($self->message_type() eq 'GetCertInitial') {
-        $params->{pkcs10} = undef;
+        $params->{pkcs10} = '';
         $params->{transaction_id} = $self->transaction_id();
         $params->{signer_cert} = $self->signer();
     } elsif ($self->message_type() =~ m{\AGet(Cert|CRL)\z}) {