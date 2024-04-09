5861cdae881b65879236605083668d31281783ef
Oliver Welter <owelter@whiterabbitsecurity.com>
2024-04-09 18:25:43 +0200

    Do not set empty redirects from Workflow UI

--- core/server/OpenXPKI/Client/UI/Workflow.pm
+++ core/server/OpenXPKI/Client/UI/Workflow.pm
@@ -1268,7 +1268,7 @@ sub __render_field_redirect {
             $self->status->message($v->{label});
             $self->status->level($v->{level}) if $v->{level};
         }
-    } else {
+    } elsif ($item->{value}) {
         $self->redirect->to($item->{value});
     }
     return -1; # do not output this field
