1075e8586645cfbef66f8cb0007afe8c901ab636
Jens Berthold <jens@jebecs.de>
2024-05-23 09:22:08 +0200

    Make creation of virtual wf IDs optional in Null persister

--- core/server/OpenXPKI/Server/Workflow/Persister/Null.pm
+++ core/server/OpenXPKI/Server/Workflow/Persister/Null.pm
@@ -13,6 +13,7 @@ sub init {
     my $self = shift;
     my $params = shift;
 
+    $self->param(enable_volatile_ids => $params->{enable_volatile_ids});
     $self->SUPER::init( $params );
 
     return; # no useful return value
@@ -23,9 +24,9 @@ sub create_workflow {
     my $workflow = shift;
     ##! 1: "create volatile workflow"
 
-    CTX('log')->workflow()->info("Created volatile workflow for type ".$workflow->type());
+    CTX('log')->workflow->info('Created volatile workflow for type '.$workflow->type);
 
-    return 'V'.OpenXPKI::Random->new()->get_random(15);
+    return ($self->param('enable_volatile_ids') ? 'V'.OpenXPKI::Random->new->get_random(15) : 0);
 }
 
 sub update_workflow {
@@ -60,12 +61,36 @@ sub fetch_history {
 1;
 __END__
 
-=head1 Name
+=head1 NAME
 
 OpenXPKI::Server::Workflow::Persister::Null
 
-=head1 Description
+=head1 DESCRIPTION
 
-This class provides a persister for "throw away" workflows. Those are not
-persisted and therefore are only useable for "one shot" operations.
+This class provides a persister for volatile ("throw away") workflows. Those
+are not persisted and therefore are only useable for "one shot" operations.
+
 Be aware that you loose all workflow related data once the workflows ends!
+
+Example:
+
+    # config node: realm.rootca.workflow.persister
+
+    Volatile:
+        class: OpenXPKI::Server::Workflow::Persister::Null
+        param:
+            enable_volatile_ids: 1
+
+=head2 Parameters
+
+=over
+
+=item enable_volatile_ids
+
+If set to C<1> the persister adds a special workflow ID to volatile workflows
+using a dedicated "namespace": volatile workflow IDs start with C<"V"> and thus
+are non-numeric.
+
+Default is to set the workflow ID to C<0>.
+
+=back
