5518a3744551a2e99bc56082ebc24096bd4ba2df
Oliver Welter <owelter@whiterabbitsecurity.com>
2025-05-08 15:59:12 +0200

    Enhance get_menu command to filter workflows and upgrade old syntax

--- core/server/OpenXPKI/Server/API2/Plugin/UI/get_menu.pm
+++ core/server/OpenXPKI/Server/API2/Plugin/UI/get_menu.pm
@@ -20,6 +20,9 @@ use OpenXPKI::Types;
 Returns the UI menu and page definitions (I<HashRef>) for the current role (or
 for pseudo role I<_default> if there is no configuration for the current role).
 
+Upgrades elements with the old syntax I<key> to I<page> and removes any items
+linked to a workflow that is not available for the given role.
+
 =cut
 command "get_menu" => {
 } => sub {
@@ -40,6 +43,25 @@ command "get_menu" => {
             message => "Error: $realm.uicontrol.'$role'.main has wrong type. Expected: ARRAY ref, got: " . ($ref ? "$ref ref" : ($menu->{main} ? 'scalar' : 'undef')),
         );
     }
+
+    my $wf_factory = CTX('workflow_factory')->get_factory;
+    my $make_item = sub {
+        my $elem = shift;
+        # auto migrate to new keyword (v3.32+)
+        ($elem->{page} = $elem->{key} && delete $elem->{key}) if (defined $elem->{key});
+
+        # check if this is a workflow item
+        my ($wf_type) = $elem->{page} =~ m{\Aworkflow!.+!wf_type!(\w+)};
+
+        # check acl for workflows, just return if wf_type is not set
+        return (!$wf_type || $wf_factory->can_create_workflow($wf_type)) ?
+            $elem : ();
+    };
+
+    my @menu = map {
+        $_->{entries} = [ map { $make_item->($_); } $_->{entries}->@* ] if ($_->{entries});
+        $make_item->($_);
+    } $menu->{main}->@*;
 
     return $menu;
 };
