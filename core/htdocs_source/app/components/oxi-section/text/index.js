8451bf571e9284b502148c01ec2ac7a9be3edef0
Jens Berthold <jens@jebecs.de>
2026-04-11 02:57:30 +0200

    UI: fix "text" section missing buttons

---
+++ core/htdocs_source/app/components/oxi-section/text/index.js
@@ -0,0 +1,15 @@
+import Component from '@glimmer/component'
+
+/**
+ * Outputs the given text.
+ *
+ * @param { hash } def - section definition
+ * ```javascript
+ * {
+ *     ... // TODO
+ * }
+ * ```
+ * @class OxiSection::Text
+ * @extends Component
+ */
+export default class OxiSectionTextComponent extends Component {}
