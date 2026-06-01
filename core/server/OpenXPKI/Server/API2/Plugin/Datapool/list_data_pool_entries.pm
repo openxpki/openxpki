28cb18ab902a4ea00aeae0b8a854c37d33cb0693
Oliver Welter <owelter@whiterabbitsecurity.com>
2026-06-01 13:19:36 +0200

    Remove checks in data_pool API methods for cross realm / sys namespace
    Remove the assert_current_pki_realm_within_workflow method from all
    API functions of the datapool and move checks for cross-realm access
    into the workflow classes.
    Remove limitation to write to the sys* namespace from a workflow class

--- core/server/OpenXPKI/Server/API2/Plugin/Datapool/list_data_pool_entries.pm
+++ core/server/OpenXPKI/Server/API2/Plugin/Datapool/list_data_pool_entries.pm
@@ -104,11 +104,6 @@ command "list_data_pool_entries" => {
 } => sub {
     my ($self, $params) = @_;
 
-    # when called from a workflow we only allow the current realm
-    # NOTE: only check direct caller. if workflow is deeper in the caller
-    # chain we assume it's ok.
-    $self->assert_current_pki_realm_within_workflow($params->pki_realm);
-
     my $sql_params = $self->_make_db_query($params, $params->metadata, $params->values);
 
     my $order_by = $params->has_order ? $params->order : [ 'datapool_key', 'namespace' ];
@@ -165,11 +160,6 @@ command "list_data_pool_entries_count" => {
     %common_params
 } => sub {
     my ($self, $params) = @_;
-
-    # when called from a workflow we only allow the current realm
-    # NOTE: only check direct caller. if workflow is deeper in the caller
-    # chain we assume it's ok.
-    $self->assert_current_pki_realm_within_workflow($params->pki_realm);
 
     my $sql_params = $self->_make_db_query($params);
 
--- core/server/OpenXPKI/Server/API2/Plugin/Datapool/set_data_pool_entry.pm
+++ core/server/OpenXPKI/Server/API2/Plugin/Datapool/set_data_pool_entry.pm
@@ -116,21 +116,6 @@ command "set_data_pool_entry" => {
         ) ;
     }
 
-    # when called from a workflow we only allow the current realm
-    # NOTE: only check direct caller. if workflow is deeper in the caller
-    # chain we assume it's ok.
-    $self->assert_current_pki_realm_within_workflow($params->pki_realm);
-
-    ##! 16: "checking if caller is workflow class that tries to access sys.* namespace"
-    my @caller = $self->rawapi->my_caller;
-    if ($caller[0] =~ m{ \A OpenXPKI::Server::Workflow }xms and $params->namespace =~ m{ \A sys\. }xms) {
-        OpenXPKI::Exception->throw(
-            message => 'Access to namespace sys.* not allowed when called from OpenXPKI::Server::Workflow::*',
-            params => { namespace => $params->namespace, },
-        );
-
-    }
-
     my $value = $params->value;
 
     # serialization
